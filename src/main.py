3396a3a8869b91a20989cdb16394ba1aae01f7b0
Andrew Edwards <Andrew.Edwards@3dsystems.com>
2018-11-26 14:50:36 +0000

    Cleanup.

--- src/main.py
+++ src/main.py
@@ -6,64 +6,20 @@ www.yetitool.com
 '''
 
 # config
-from kivy.config import Config
-from asmcnc.comms.serial_connection import SerialConnection
-from asmcnc.comms.router_machine import RouterMachine
+#import os
+#os.environ['KIVY_GL_BACKEND'] = 'sdl2'
+import time
 
+from kivy.config import Config
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 Config.set('graphics', 'width', '800')
 Config.set('graphics', 'height', '480')
 
 import kivy
-
 from kivy.app import App
-from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
-from kivy.uix.floatlayout import FloatLayout
-from kivy.factory import Factory
-from kivy.properties import ObjectProperty # @UnresolvedImport
-from kivy.uix.popup import Popup
-
-from kivy.app import App
-from kivy.clock import Clock
 from kivy.core.window import Window
-from kivy.uix.widget import Widget
-from kivy.resources import resource_find
-from kivy.graphics.transformation import Matrix
-from kivy.graphics.opengl import *
-from kivy.graphics import *
-from kivy.uix.scrollview import ScrollView
-from kivy.properties import BooleanProperty, NumericProperty, ListProperty, ObjectProperty  # @UnresolvedImport
 
-import time
-import socket
-import subprocess
-
-import os, sys
-import subprocess
-import ntpath
-from shutil import copyfile, rmtree
-from kivy.lib.osc.OSC import null
-import zipfile
-from kivy.uix.video import Video
-from os import listdir
-from os.path import isfile, join
-import pickle
-import shutil
-import math
-import ntpath
-from kivy.core.image import Image as CoreImage
-import serial
-import time
-from threading import Thread
-
-import time
-from kivy.graphics import Line
-from kivy.uix.slider import Slider
-from kivy.cache import Cache
-
-from kivy.graphics.vertex_instructions import (Rectangle, Ellipse, Line)
-from kivy.graphics.context_instructions import Color  # @UnresolvedImport (for Eclipse users)
 from asmcnc.comms import router_machine
 from asmcnc.skavaUI import screen_inital, screen_help
 from asmcnc.skavaUI import screen_home
