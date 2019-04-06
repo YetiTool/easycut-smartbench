ef55810b2a14cdd9388a9cafe0f935ec842caf4f
Ed <skinnyed@gmail.com>
2019-04-06 01:10:43 +0100

    Disable homing screen for dev option to sqr axes

--- src/asmcnc/skavaUI/widget_developer_options.py
+++ src/asmcnc/skavaUI/widget_developer_options.py
@@ -119,7 +119,7 @@ class DevOptions(Widget):
 
     def square_axes(self):
         self.m.is_squaring_XY_needed_after_homing = True
-        self.sm.current = 'homing'
+#         self.sm.current = 'homing'
         self.m.home_all()
 
     def return_to_lobby(self):
