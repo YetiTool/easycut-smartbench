2c4253705715aafff003610c43e7185c6f7b24be
LettyYeti <lettie.adkins@yetitool.com>
2019-06-15 00:13:57 +0100

    now can save settings to a file, so can close EC for flash

--- src/asmcnc/skavaUI/widget_developer_options.py
+++ src/asmcnc/skavaUI/widget_developer_options.py
@@ -230,6 +230,6 @@ class DevOptions(Widget):
         g = open('saved_grbl_settings_params.txt', 'r')
         settings_to_restore = g.read()
         print(settings_to_restore)
-#        self.m.s.start_sequential_stream(grbl_settings)   # Send any grbl specific parameters        
+        self.m.s.start_sequential_stream(settings_to_restore)   # Send any grbl specific parameters        
 
         
