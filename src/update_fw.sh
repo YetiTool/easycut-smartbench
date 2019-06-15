6796ac96bee54b2a1b8821b06956f902214842c9
LettyYeti <lettie.adkins@yetitool.com>
2019-06-15 21:57:36 +0100

    exit EC from sh script

--- src/update_fw.sh
+++ src/update_fw.sh
@@ -1,3 +1,3 @@
 pkill -f main.py
-sleep 5
-avrdude -C/home/pi/easycut-smartbench/src/avrdude_gpio.conf -patmega2560 -cwiring -P/dev/ttyS0 -b115200 -D -Uflash:w:/home/pi/easycut-smartbench/src/GRBL104.hex:i
+#sleep 5
+#avrdude -C/home/pi/easycut-smartbench/src/avrdude_gpio.conf -patmega2560 -cwiring -P/dev/ttyS0 -b115200 -D -Uflash:w:/home/pi/easycut-smartbench/src/GRBL104.hex:i
