695549e066cbac28560cd016d1ce73545372c74d
LettyYeti <lettie.adkins@yetitool.com>
2019-07-06 17:59:42 +0100

    default config file edited on machine so no longer pointing anywhere else

--- src/update_fw.sh
+++ src/update_fw.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pkill -f main.py
 sleep 5
-avrdude -C/home/pi/easycut-smartbench/src/avrdude_gpio.conf -patmega2560 -cwiring -P/dev/ttyAMA0 -b115200 -D -Uflash:w:/home/pi/easycut-smartbench/src/GRBL104.hex:i
+avrdude -patmega2560 -cwiring -P/dev/ttyAMA0 -b115200 -D -Uflash:w:/home/pi/easycut-smartbench/src/GRBL104.hex:i
