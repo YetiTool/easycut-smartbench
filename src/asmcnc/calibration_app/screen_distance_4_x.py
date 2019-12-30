44e2d2e510a46afa08546553c44a5857ebac653f
LettyYeti <lettie.adkins@yetitool.com>
2019-12-30 23:57:04 +0000

    testing - minor correction

--- src/asmcnc/calibration_app/screen_distance_4_x.py
+++ src/asmcnc/calibration_app/screen_distance_4_x.py
@@ -15,7 +15,6 @@ from kivy.properties import ObjectProperty, StringProperty, NumericProperty
 from kivy.uix.widget import Widget
 from kivy.uix.textinput import TextInput
 # from asmcnc.calibration_app import screen_measurement
-from asmcnc.calibration_app import screen_distance_1_x # this has to be here
 
 
 
@@ -239,6 +238,7 @@ class DistanceScreen4Class(Screen):
         self.sm.current = 'homing'
 
     def repeat_section(self):
+        from asmcnc.calibration_app import screen_distance_1_x # this has to be here
         distance_screen1x = screen_distance_1_x.DistanceScreenClass(name = 'distance1x', screen_manager = self.sm, machine = self.m)
         self.sm.add_widget(distance_screen1x)
         self.sm.current = 'distance1x'
