3a5f287946cc5dacc4dfcf0f930713c439d95b0c
LettyYeti <lettie.adkins@yetitool.com>
2020-01-05 14:16:07 +0000

    testing - colouring app

--- src/asmcnc/calibration_app/screen_distance_2_x.py
+++ src/asmcnc/calibration_app/screen_distance_2_x.py
@@ -220,7 +220,7 @@ class DistanceScreen2Class(Screen):
         self.user_instructions_text.text = 'Re-measure distance between guard post and end plate. \n\n' \
                         '[b]The distance should measure ' + measure_string + '[/b]'
 #         self.improve_button_label.text = 'I want to try to improve the result'
-        self.continue_button_label.text = '[color=455A64]Ok, it measures as expected.\n\n Move to the next section.[/color]'
+        self.continue_button_label.text = '[color=455A64]Ok, it measures as expected.\n Finish and move on to the next section.[/color]'
        
     def left_button(self):
         self.next_screen()
