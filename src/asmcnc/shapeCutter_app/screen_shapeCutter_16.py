cbc094c94d47239c8ea3cbd291824bdadf2319b8
LettyYeti <lettie.adkins@yetitool.com>
2020-03-04 00:38:42 +0000

    photo screens all done

--- src/asmcnc/shapeCutter_app/screen_shapeCutter_16.py
+++ src/asmcnc/shapeCutter_app/screen_shapeCutter_16.py
@@ -11,6 +11,7 @@ from kivy.metrics import MetricsBase
 from kivy.properties import StringProperty, ObjectProperty
 
 from asmcnc.shapeCutter_app import screen_shapeCutter_17
+from asmcnc.shapeCutter_app import screen_shapeCutter_18
 
 Builder.load_string("""
 
@@ -448,11 +449,10 @@ class ShapeCutter16ScreenClass(Screen):
         self.sm.current = 'sC15'
     
     def next_screen(self):
-#         if not self.sm.has_screen('sC15'):
-#             sC15_screen = screen_shapeCutter_15.ShapeCutter15ScreenClass(name = 'sC15', screen_manager = self.sm, machine = self.m)
-#             self.sm.add_widget(sC15_screen)
-#         self.sm.current = 'sC15'
-        pass
+        if not self.sm.has_screen('sC18'):
+            sC18_screen = screen_shapeCutter_18.ShapeCutter18ScreenClass(name = 'sC18', screen_manager = self.sm, machine = self.m)
+            self.sm.add_widget(sC18_screen)
+        self.sm.current = 'sC18'
     
 # Tab functions
 
--- src/asmcnc/shapeCutter_app/screen_shapeCutter_29.py
+++ src/asmcnc/shapeCutter_app/screen_shapeCutter_29.py
@@ -1,20 +1,27 @@
 '''
 Created on 20 February 2020
-Template Screen for the Shape Cutter App
+Screen 29 for the Shape Cutter App
 
 @author: Letty
 '''
 
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.tabbedpanel import TabbedPanel
 from kivy.metrics import MetricsBase
+from kivy.properties import StringProperty, ObjectProperty
+
+from asmcnc.shapeCutter_app import screen_shapeCutter_32
 
 Builder.load_string("""
 
-<ShapeCutterTemplateButtonsScreenClass>
+<ShapeCutter29ScreenClass>
+
+    info_button: info_button
 
     BoxLayout:
+        size_hint: (None,None)
+        width: dp(800)
+        height: dp(480)
         padding: 0
         spacing: 0
         orientation: "vertical"
@@ -31,6 +38,7 @@ Builder.load_string("""
                 size_hint: (None,None)
                 height: dp(90)
                 width: dp(142)
+                on_press: root.prepare()
                 BoxLayout:
                     padding: 0
                     size: self.parent.size
@@ -43,6 +51,7 @@ Builder.load_string("""
                 size_hint: (None,None)
                 height: dp(90)
                 width: dp(142)
+                on_press: root.load()
                 BoxLayout:
                     padding: 0
                     size: self.parent.size
@@ -55,6 +64,7 @@ Builder.load_string("""
                 size_hint: (None,None)
                 height: dp(90)
                 width: dp(142)
+                on_press: root.define()
                 BoxLayout:
                     padding: 0
                     size: self.parent.size
@@ -69,12 +79,13 @@ Builder.load_string("""
                 size_hint: (None,None)
                 height: dp(90)
                 width: dp(142)
+                on_press: root.position()
                 BoxLayout:
                     padding: 0
                     size: self.parent.size
                     pos: self.parent.pos
                     Image:
-                        source: "./asmcnc/shapeCutter_app/img/position_tab_blue.png"
+                        source: "./asmcnc/shapeCutter_app/img/position_tab_grey.png"
                         center_x: self.parent.center_x
                         y: self.parent.y
                         size: self.parent.width, self.parent.height
@@ -83,6 +94,7 @@ Builder.load_string("""
                 size_hint: (None,None)
                 height: dp(90)
                 width: dp(142)
+                on_press: root.check()
                 BoxLayout:
                     padding: 0
                     size: self.parent.size
@@ -97,6 +109,7 @@ Builder.load_string("""
                 size_hint: (None,None)
                 height: dp(90)
                 width: dp(90)
+                on_press: root.exit()
                 BoxLayout:
                     padding: 0
                     size: self.parent.size
@@ -109,22 +122,207 @@ Builder.load_string("""
                         allow_stretch: True                    
                     
         BoxLayout:
-            padding: 10
-            height: dp(800)
-            width: dp(480)
+            size_hint: (None,None)
+            padding: 0
+            height: dp(390)
+            width: dp(800)
             canvas:
                 Rectangle: 
                     pos: self.pos
                     size: self.size
                     source: "./asmcnc/shapeCutter_app/img/background.png"
+            
+            BoxLayout:
+                orientation: "vertical"
+                padding: 0
+                spacing: 0
+                    
+                BoxLayout: #Header
+                    size_hint: (None,None)
+                    height: dp(60)
+                    width: dp(800)
+                    padding: (20,0,0,0)
+                    orientation: "horizontal"
+                    
+                    BoxLayout: #Screen number
+                        size_hint: (None,None)
+                        padding: 0
+                        height: dp(40)
+                        width: dp(40)
+                        canvas:
+                            Rectangle: 
+                                pos: self.pos
+                                size: self.size
+                                source: "./asmcnc/shapeCutter_app/img/number_box.png"
+                        Label:
+                            text: root.screen_number
+                            valign: "middle"
+                            halign: "center"
+                            font_size: 26
+                            markup: True
+                                
+                                
+                        
+                    BoxLayout: #Title
+                        size_hint: (None,None)
+                        height: dp(60)
+                        width: dp(740)
+                        padding: (20,20,0,0)
+                        
+                        Label:
+                            text: root.title_label
+                            color: 0,0,0,1
+                            font_size: 28
+                            markup: True
+                            halign: "left"
+                            valign: "bottom"
+                            text_size: self.size
+                            size: self.parent.size
+                            pos: self.parent.pos
+                        
+                    
+                BoxLayout: #Body
+                    size_hint: (None,None)
+                    height: dp(330)
+                    width: dp(800)
+                    padding: 0,20,0,0
+                    orientation: "horizontal"
+                    
+                    BoxLayout: #text box
+                        size_hint: (None,None)
+                        height: dp(310)
+                        width: dp(675)
+                        padding: 250,0,0,20
+                        orientation: "vertical"
+
+                        BoxLayout: #image box
+                            size_hint: (None,None)
+                            height: dp(290)
+                            width: dp(300)                      
+                            Image:
+                                source: "./asmcnc/shapeCutter_app/img/photo_29_1.png"
+                                center_x: self.parent.center_x
+                                y: self.parent.y
+                                size: self.parent.width, self.parent.height
+                                allow_stretch: True
+                                        
+
+                    BoxLayout: #action box
+                        size_hint: (None,None)
+                        height: dp(310)
+                        width: dp(125)
+                        padding: 0,0,0,34
+                        spacing: 34
+                        orientation: "vertical"
+                        
+                        BoxLayout: 
+                            size_hint: (None,None)
+                            height: dp(67)
+                            width: dp(88)
+                            padding: (24,0,24,34)
+                            Button:
+                                id: info_button
+                                size_hint: (None,None)
+                                height: dp(40)
+                                width: dp(40)
+                                background_color: hex('#F4433600')
+                                opacity: 1
+                                on_press: root.get_info()
+                                BoxLayout:
+                                    padding: 0
+                                    size: self.parent.size
+                                    pos: self.parent.pos
+                                    Image:
+                                        source: "./asmcnc/shapeCutter_app/img/info_icon.png"
+                                        center_x: self.parent.center_x
+                                        y: self.parent.y
+                                        size: self.parent.width, self.parent.height
+                                        allow_stretch: True
+
+                        Button: 
+                            size_hint: (None,None)
+                            height: dp(67)
+                            width: dp(88)
+                            background_color: hex('#F4433600')
+                            on_press: root.go_back()
+                            BoxLayout:
+                                padding: 0
+                                size: self.parent.size
+                                pos: self.parent.pos
+                                Image:
+                                    source: "./asmcnc/shapeCutter_app/img/arrow_back.png"
+                                    center_x: self.parent.center_x
+                                    y: self.parent.y
+                                    size: self.parent.width, self.parent.height
+                                    allow_stretch: True
+                        Button: 
+                            size_hint: (None,None)
+                            height: dp(67)
+                            width: dp(88)
+                            background_color: hex('#F4433600')
+                            on_press: root.next_screen()
+                            BoxLayout:
+                                padding: 0
+                                size: self.parent.size
+                                pos: self.parent.pos
+                                Image:
+                                    source: "./asmcnc/shapeCutter_app/img/arrow_next.png"
+                                    center_x: self.parent.center_x
+                                    y: self.parent.y
+                                    size: self.parent.width, self.parent.height
+                                    allow_stretch: True               
 
 """)
 
-class ShapeCutterTemplateButtonsScreenClass(Screen):
+class ShapeCutter29ScreenClass(Screen):
+    
+    info_button = ObjectProperty()
+    
+    screen_number = StringProperty("[b]29[/b]")
+    title_label = StringProperty("[b]Remove dust shoe[/b]")
+    user_instructions = StringProperty("")
     
     def __init__(self, **kwargs):
-        super(ShapeCutterTemplateButtonsScreenClass, self).__init__(**kwargs)
+        super(ShapeCutter29ScreenClass, self).__init__(**kwargs)
         self.sm=kwargs['screen_manager']
         self.m=kwargs['machine']
 
+    def on_pre_enter(self):
+        self.info_button.opacity = 0
+
+# Action buttons       
+    def get_info(self):
+        pass
+    
+    def go_back(self):
+        self.sm.current = 'sC21'
+    
+    def next_screen(self):
+        if not self.sm.has_screen('sC32'):
+            sC32_screen = screen_shapeCutter_32.ShapeCutter32ScreenClass(name = 'sC32', screen_manager = self.sm, machine = self.m)
+            self.sm.add_widget(sC32_screen)
+        self.sm.current = 'sC32'
+    
+# Tab functions
+
+    def prepare(self):
+        self.sm.current = 'sC1'
+    
+    def load(self):
+        self.sm.current = 'sC11'
+    
+    def define(self):
+        #self.sm.current = 'sC16'
+        pass
+    
+    def position(self):
+        self.sm.current = 'sC26'
+        pass
+    
+    def check(self):
+        #self.sm.current = 'sC34'
+        pass
+    
+    def exit(self):
+        self.sm.current = 'lobby'
         
