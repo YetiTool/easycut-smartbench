f31a66e7a464fd3bfc86a2a1300d92f3c8b42626
LettyYeti <lettie.adkins@yetitool.com>
2020-03-04 17:40:20 +0000

    island aperture choice

--- src/asmcnc/shapeCutter_app/screen_shapeCutter_aperture_island.py
+++ src/asmcnc/shapeCutter_app/screen_shapeCutter_aperture_island.py
@@ -13,7 +13,10 @@ from kivy.properties import ObjectProperty
 Builder.load_string("""
 
 <ShapeCutterApIsScreenClass>:
-    
+
+    image_apt: image_apt
+    image_is: image_is
+        
     BoxLayout:
         height: dp(800)
         width: dp(480)
@@ -42,12 +45,12 @@ Builder.load_string("""
             BoxLayout:
                 size_hint: (None,None)
                 width: dp(800)
-                height: dp(140)
+                height: dp(115)
                 padding: 0
                 spacing: 0
                 Label:
                     size_hint: (None,None)
-                    height: dp(170)
+                    height: dp(100)
                     width: dp(800)
                     halign: "center"
                     valign: "middle"
@@ -59,8 +62,8 @@ Builder.load_string("""
             BoxLayout:
                 size_hint: (None,None)
                 width: dp(800)
-                height: dp(200)
-                padding: (150,0,150,0)
+                height: dp(240)
+                padding: (150,0,150,15)
                 spacing: 0
                 orientation: 'horizontal'
                 pos: self.parent.pos                
@@ -68,14 +71,14 @@ Builder.load_string("""
                 BoxLayout:
                     size_hint: (None,None)
                     width: dp(250)
-                    height: dp(200)
+                    height: dp(225)
                     padding: (23,0,20,0)
                     pos: self.parent.pos
                     
                     # aperture
                     Button:
                         size_hint: (None,None)
-                        height: dp(200)
+                        height: dp(225)
                         width: dp(207)
                         background_color: hex('#F4433600')
                         center: self.parent.center
@@ -86,6 +89,7 @@ Builder.load_string("""
                             size: self.parent.size
                             pos: self.parent.pos
                             Image:
+                                id: image_apt
                                 source: "./asmcnc/shapeCutter_app/img/apt_rect.png"
                                 center_x: self.parent.center_x
                                 y: self.parent.y
@@ -94,14 +98,14 @@ Builder.load_string("""
                 BoxLayout:
                     size_hint: (None,None)
                     width: dp(250)
-                    height: dp(200)
+                    height: dp(225)
                     padding: (20,0,23,0)
                     pos: self.parent.pos
                     
                     # island
                     Button:
                         size_hint: (None,None)
-                        height: dp(200)
+                        height: dp(225)
                         width: dp(207)
                         background_color: hex('#F4433600')
                         center: self.parent.center
@@ -112,6 +116,7 @@ Builder.load_string("""
                             size: self.parent.size
                             pos: self.parent.pos
                             Image:
+                                id: image_is
                                 source: "./asmcnc/shapeCutter_app/img/is_rect.png"
                                 center_x: self.parent.center_x
                                 y: self.parent.y
@@ -121,20 +126,67 @@ Builder.load_string("""
                 size_hint: (None,None)
                 width: dp(800)
                 height: dp(50)
-                padding: (740,0,0,20)
+                padding: (150,0,150,20)
                 spacing: 0
                 orientation: 'horizontal'
                 pos: self.parent.pos
+                BoxLayout:
+                    size_hint: (None,None)
+                    width: dp(250)
+                    height: dp(50)
+                    padding: (23,0,20,0)
+                    pos: self.parent.pos
+                    Label:
+                        size_hint: (None,None)
+                        height: dp(50)
+                        width: dp(207)
+                        halign: "center"
+                        valign: "middle"
+                        text: "Outer (cut an aperture)"
+                        color: 0,0,0,1
+                        font_size: 20
+                        markup: True
+                BoxLayout:
+                    size_hint: (None,None)
+                    width: dp(250)
+                    height: dp(50)
+                    padding: (20,0,23,0)
+                    pos: self.parent.pos
+                    Label:
+                        size_hint: (None,None)
+                        height: dp(50)
+                        width: dp(207)
+                        halign: "center"
+                        valign: "middle"
+                        text: "Inner (cut an island)"
+                        color: 0,0,0,1
+                        font_size: 20
+                        markup: True
 """)
 
 class ShapeCutterApIsScreenClass(Screen):
 
     info_button = ObjectProperty()   
+    shape = 'circle'
     
     def __init__(self, **kwargs):
         super(ShapeCutterApIsScreenClass, self).__init__(**kwargs)
         self.sm=kwargs['screen_manager']
         self.m=kwargs['machine']
+        
+    def on_pre_enter(self):
+        if self.shape == 'circle':
+#             self.user_instructions = ("Mark the shape\'s datum position on the material. " \
+#                                       "This is always in the centre of your circle. ")
+            self.image_apt.source = ("./asmcnc/shapeCutter_app/img/apt_circ.png")
+            self.image_is.source = ("./asmcnc/shapeCutter_app/img/is_circ.png")
+        
+        elif self.shape == 'rectangle':
+#             self.user_instructions = ("Mark the shape\'s datum position on the material. " \
+#                                       "This is always in the bottom right corner of your rectangle.")
+            self.image_apt = ("./asmcnc/shapeCutter_app/img/apt_rect.png")
+            self.image_is = ("./asmcnc/shapeCutter_app/img/is_rect.png")
+
       
     def aperture(self):
         self.next_screen()
