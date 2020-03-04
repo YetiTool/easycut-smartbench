1ffdc256c91340079c87f59fa9afa0ba8076b4aa
LettyYeti <lettie.adkins@yetitool.com>
2020-03-04 23:20:29 +0000

    strategy params

--- src/asmcnc/shapeCutter_app/screen_shapeCutter_23.py
+++ src/asmcnc/shapeCutter_app/screen_shapeCutter_23.py
@@ -520,10 +520,10 @@ class ShapeCutter23ScreenClass(Screen):
         self.sm.current = 'sC22'
     
     def next_screen(self):
-        if not self.sm.has_screen('sC21'):
-            sC21_screen = screen_shapeCutter_21.ShapeCutter21ScreenClass(name = 'sC21', screen_manager = self.sm, machine = self.m)
-            self.sm.add_widget(sC21_screen)
-        self.sm.current = 'sC21'
+        if not self.sm.has_screen('sC24'):
+            sC24_screen = screen_shapeCutter_24.ShapeCutter24ScreenClass(name = 'sC24', screen_manager = self.sm, machine = self.m)
+            self.sm.add_widget(sC24_screen)
+        self.sm.current = 'sC24'
     
 # Tab functions
 
