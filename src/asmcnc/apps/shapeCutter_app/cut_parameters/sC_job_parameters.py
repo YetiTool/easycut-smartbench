4da07873a214ef4deaab69891f7a87fa860cef86
LettyYeti <lettie.adkins@yetitool.com>
2020-03-15 23:34:28 +0000

    weird error

--- src/asmcnc/apps/shapeCutter_app/cut_parameters/sC_job_parameters.py
+++ src/asmcnc/apps/shapeCutter_app/cut_parameters/sC_job_parameters.py
@@ -22,6 +22,7 @@ class ShapeCutterJobParameters(object):
         self.m = machine
         self.shapecutter_sm = shapecutter_sm
  
+        self.m.get_grbl_settings()
         self.refresh_parameters()
 
     def refresh_parameters(self):
