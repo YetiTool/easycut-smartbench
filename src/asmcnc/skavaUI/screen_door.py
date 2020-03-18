a8d69f44b8a7e3b306a21066ff300352f8ab5a09
LettyYeti <lettie.adkins@yetitool.com>
2020-03-18 23:52:11 +0000

    door and go screen harmony

--- src/asmcnc/skavaUI/screen_door.py
+++ src/asmcnc/skavaUI/screen_door.py
@@ -180,15 +180,15 @@ class DoorScreen(Screen):
                         +'Pressing [b]Stop[/b] will cancel the current operation completely. [/color]'
 
     def on_enter(self):
-        if self.return_to_screen == 'go':
-            self.sm.get_screen('go').start_stop_button_press()
-        else: 
-            self.m.toggle_pause()
+#         if self.return_to_screen == 'go':
+#             self.sm.get_screen('go').start_stop_button_press()
+#         else: 
+        self.m.toggle_pause()
        
     def resume_stream(self):
         self.m.resume()
-        if self.return_to_screen == 'go':
-            self.sm.get_screen('go').start_stop_button_press()
+#         if self.return_to_screen == 'go':
+#             self.sm.get_screen('go').start_stop_button_press()
         self.return_to_app()
 
     def cancel_stream(self):
