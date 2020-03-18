9ea57fc202e2412fe57cf441f4f4ff35365d1aaa
LettyYeti <lettie.adkins@yetitool.com>
2020-03-18 16:04:06 +0000

    update LED

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -198,7 +198,7 @@ class RouterMachine(object):
         self.door()
     
     def resume(self):
-        self.update_led_state(0)
+        self.set_led_blue()
         self.is_machine_paused = False
         self.s.write_realtime('~', altDisplayText = 'Resume')       
         # Restore LEDs
