b2e250cf83b2baf96062d4b7809810dcbf44f4b9
LettyYeti <lettie.adkins@yetitool.com>
2020-03-19 14:28:16 +0000

    door and go screen harmony

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -200,6 +200,7 @@ class RouterMachine(object):
             self.door()
     
     def resume(self):
+        print "Resume"
         Clock.schedule_once(lambda dt: self.set_pause(False),0.1)
         self.s.write_realtime('~', altDisplayText = 'Resume')
         # Restore LEDs
