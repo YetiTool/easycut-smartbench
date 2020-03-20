9637f314e72e789e6eee1952f4f7d4a0d2bdff3d
LettyYeti <lettie.adkins@yetitool.com>
2020-03-20 19:19:33 +0000

    testing efficacy

--- src/main.py
+++ src/main.py
@@ -63,7 +63,7 @@ if sys.platform != 'win32':
     
     pc_alert = (os.popen('grep -Fx "power_cycle_alert = True" /easycut-smartbench/src/config.txt').read())
     if pc_alert.startswith('power_cycle_alert = True'):
-        os.system('sudo sed -i "s/power_cycle_alert = True/power_cycle_alert = False" /easycut-smartbench/src/config.txt') 
+        os.system('sudo sed -i "s/power_cycle_alert = True/power_cycle_alert = False/" /home/pi/easycut-smartbench/src/config.txt') 
         start_screen = 'pc_alert'
 
     case = (os.popen('grep -Fx "gpu_mem=128" /boot/config.txt').read())
