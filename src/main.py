6569d93504cfcefc8e8d67b60a615e0b63c0d0a5
LettyYeti <lettie.adkins@yetitool.com>
2020-03-23 13:30:22 +0000

    version tag v1.1.0-beta

--- src/main.py
+++ src/main.py
@@ -59,7 +59,7 @@ from asmcnc.skavaUI import screen_door
 Cmport = 'COM3'
 
 # Current version active/working on
-initial_version = 'v1.1.0'
+initial_version = 'v1.1.0-beta'
 
 # default starting screen
 start_screen = 'safety'
