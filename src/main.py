517bd1f5d5f37a3429dbd74016245162281ab109
LettyYeti <47244859+LettyYeti@users.noreply.github.com>
2020-03-31 17:44:07 +0100

    Version number update for release

--- src/main.py
+++ src/main.py
@@ -59,7 +59,7 @@ from asmcnc.skavaUI import screen_door
 Cmport = 'COM3'
 
 # Current version active/working on
-initial_version = 'v1.1.2'
+initial_version = 'v1.1.3'
 
 # default starting screen
 start_screen = 'safety'
