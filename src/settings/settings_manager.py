3372ff44bff4d3f33c39090b02bf0e1597df80cf
LettyYeti <lettie.adkins@yetitool.com>
2020-04-09 16:23:47 +0100

    testing printing system output

--- src/settings/settings_manager.py
+++ src/settings/settings_manager.py
@@ -50,10 +50,14 @@ class Settings(object):
             if self.latest_sw_version != self.sw_version:
         ##      Update SW according to latest release:
                 os.system('sudo sed -i "s/check_config=False/check_config=True/" /home/pi/easycut-smartbench/src/config.txt')
+                
                 sed_sw_version = 'sudo sed -i "s/version=' + str(self.sw_version) + '/version=' + str(self.latest_sw_version) + '/" /home/pi/easycut-smartbench/src/config.txt'
+                print "sed 1"
                 os.system(sed_sw_version)
+                print "sed 2"
                 os.system('sudo sed -i "s/power_cycle_alert=False/power_cycle_alert=True/" /home/pi/easycut-smartbench/src/config.txt')
-                output = os.system("cd /home/pi/easycut-smartbench/ && git checkout " + self.latest_sw_version)
+                print "sed 3"
+                output = os.popen("cd /home/pi/easycut-smartbench/ && git checkout " + self.latest_sw_version)
                 print output
                 self.sm.current = 'rebooting'
             else: print "Software already up to date"
