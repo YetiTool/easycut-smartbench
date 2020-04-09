613c3937385fc37c2e84525a320cbe31ddaeb60f
LettyYeti <lettie.adkins@yetitool.com>
2020-04-09 15:04:24 +0100

    still investigating with print statements

--- src/asmcnc/comms/usb_storage.py
+++ src/asmcnc/comms/usb_storage.py
@@ -102,6 +102,9 @@ class USB_storage(object):
 #                         for device in devices:
                         for char in self.alphabet_string:
                             if ('sd' + char) in devices: # sda is a file to a USB storage device. Subsequent usb's = sdb, sdc, sdd etc
+                                
+                                print ('sd' + char)
+                                
                                 self.stop_polling_for_usb() # temporarily stop polling for USB while mounting, and attempt to mount
                                 if self.IS_USB_VERBOSE: print 'Stopped polling'
                                 self.mount_event = Clock.schedule_once(lambda dt: self.mount_linux_usb('sd' + char), 1) # allow time for linux to establish filesystem after os detection of device
