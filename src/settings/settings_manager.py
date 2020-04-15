7e3f75a79d29a081a5c3d446d8932b10032cefec
LettyYeti <lettie.adkins@yetitool.com>
2020-04-15 17:02:25 +0100

    fixing backup logic

--- src/settings/settings_manager.py
+++ src/settings/settings_manager.py
@@ -101,11 +101,11 @@ class Settings(object):
                 # if not, copy from backup
                 backup_command = '\[ ! -d \"home/pi/easycut-smartbench/src/\" \] && mkdir \/home\/pi\/easycut-smartbench && cp -RT \/home\/pi\/easycut-smartbench-backup \/home\/pi\/easycut-smartbench'
                 sed_cmd = ('sudo sed -i \'/echo \\"start easycut\\"/ a ' + backup_command + '\' /home/pi/starteasycut.sh') 
-                
-                print sed_cmd
                 os.system(sed_cmd)
                 
             directory_diff = (os.popen('diff -qr /home/pi/easycut-smartbench/src/ /home/pi/easycut-smartbench-backup/src/'))
+            print directory_diff
+            
             if directory_diff == '': return True
             else: 
                 os.system('[ -d "/home/pi/easycut-smartbench-backup/" ] && sudo rm /home/pi/easycut-smartbench-backup -r')                
