d15bc584e39791640432b6c4a5b253d73a5921b3
LettyYeti <lettie.adkins@yetitool.com>
2020-04-15 16:59:03 +0100

    fixing backup logic

--- src/settings/settings_manager.py
+++ src/settings/settings_manager.py
@@ -99,7 +99,7 @@ class Settings(object):
             case = (os.popen('grep -Fx "[ ! -d " /home/pi/starteasycut.sh').read()) #current/old directory command
             if not case.startswith('[ ! -d '):
                 # if not, copy from backup
-                backup_command = '\[ ! -d \"home/pi/easycut-smartbench/src/\" \] && mkdir \/home\/pi\/easycut-smartbench && cp -RT /home/pi/easycut-smartbench-backup /home/pi/easycut-smartbench'
+                backup_command = '\[ ! -d \"home/pi/easycut-smartbench/src/\" \] && mkdir \/home\/pi\/easycut-smartbench && cp -RT \/home\/pi\/easycut-smartbench-backup \/home\/pi\/easycut-smartbench'
                 sed_cmd = ('sudo sed -i \'/echo \\"start easycut\\"/ a ' + backup_command + '\' /home/pi/starteasycut.sh') 
                 
                 print sed_cmd
