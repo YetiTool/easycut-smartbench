8544bac208e226855a3daf8aca3184370c438e78
LettyYeti <lettie.adkins@yetitool.com>
2020-04-17 12:47:22 +0100

    test possible SW update from USB

--- src/settings/settings_manager.py
+++ src/settings/settings_manager.py
@@ -124,7 +124,7 @@ class Settings(object):
             
     def get_sw_update_via_usb(self):
          
-        dir_path_name = os.popen(('find /media/usb/ -name easycut-smartbench').read()).strip('\n')
+        dir_path_name = (os.popen('find /media/usb/ -name easycut-smartbench').read()).strip('\n')
         copy_command = 'cp -RT ' + str(dir_path_name) + '/ /home/pi/easycut-smartbench-backup/'
         print copy_command
         os.system('[ -d "/home/pi/easycut-smartbench-backup/" ] && sudo rm /home/pi/easycut-smartbench-backup/ -r')
