0a4e0a7e6b5118d4b72472284815242485109f4b
LettyYeti <lettie.adkins@yetitool.com>
2020-04-21 17:45:44 +0100

    fixing formatting

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -330,7 +330,7 @@ class PopupError(Widget):
         description = warning_message
         
         img = Image(source="./asmcnc/apps/shapeCutter_app/img/error_icon.png", allow_stretch=False)
-        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[40,20], markup = True)
+        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[0,10], markup = True)
         
         ok_button = Button(text='[b]Ok[/b]', markup = True)
         ok_button.background_normal = ''
