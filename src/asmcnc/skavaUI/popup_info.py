f36799ba9430dfc8289c457733efaa4ae5a93758
LettyYeti <lettie.adkins@yetitool.com>
2020-04-23 15:35:43 +0100

    added warning pop-up to gcode monitor text input

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -370,13 +370,13 @@ class PopupWarning(Widget):
         description = warning_message
         
         img = Image(source="./asmcnc/apps/shapeCutter_app/img/error_icon.png", allow_stretch=False)
-        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[40,20], markup = True)
+        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[0,0], markup = True)
         
         ok_button = Button(text='[b]Ok[/b]', markup = True)
         ok_button.background_normal = ''
         ok_button.background_color = [230 / 255., 74 / 255., 25 / 255., 1.]
        
-        btn_layout = BoxLayout(orientation='horizontal', spacing=10, padding=[0,0,0,0])
+        btn_layout = BoxLayout(orientation='horizontal', spacing=10, padding=[20,0,20,0])
         btn_layout.add_widget(ok_button)
         
         layout_plan = BoxLayout(orientation='vertical', spacing=10, padding=[40,20,40,20])
