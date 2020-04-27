be7eac10813284a3fc28b4911684a27949ee452d
LettyYeti <lettie.adkins@yetitool.com>
2020-04-27 11:38:48 +0100

    test github error w/o wifi connection

--- src/asmcnc/apps/SWupdater_app/screen_update_SW.py
+++ src/asmcnc/apps/SWupdater_app/screen_update_SW.py
@@ -359,9 +359,6 @@ class SWUpdateScreen(Screen):
         self.check_USB_status(1)
         self.poll_USB = Clock.schedule_interval(self.check_USB_status, 0.25)
 
-        description = "Could not connect to github. Please check that your connection is stable or try again later."
-        popup_info.PopupError(self.sm, description)
-
     def on_leave(self):
         Clock.unschedule(self.poll_USB)
         Clock.unschedule(self.poll_wifi)
@@ -373,34 +370,34 @@ class SWUpdateScreen(Screen):
 
     def get_sw_update_over_wifi(self):
                
-        if self.wifi_image.source == self.wifi_on:
+#        if self.wifi_image.source == self.wifi_on:
             
-            popup_info.PopupWait(self.sm)
+        popup_info.PopupWait(self.sm)
+        
+        def do_update():
+        
+            outcome = self.set.get_sw_update_via_wifi()
             
-            def do_update():
-            
-                outcome = self.set.get_sw_update_via_wifi()
+            if outcome == False: 
+                description = "There was a problem updating your software. \n\n" \
+                "We can try to fix the problem, but you MUST have a stable internet connection and" \
+                " power supply.\n\n" \
+                "Would you like to repair your software now?"
+                popup_info.PopupSoftwareRepair(self.sm, self, description)               
+            elif outcome == "Software already up to date!": 
+                popup_info.PopupError(self.sm, outcome)
                 
-                if outcome == False: 
-                    description = "There was a problem updating your software. \n\n" \
-                    "We can try to fix the problem, but you MUST have a stable internet connection and" \
-                    " power supply.\n\n" \
-                    "Would you like to repair your software now?"
-                    popup_info.PopupSoftwareRepair(self.sm, self, description)               
-                elif outcome == "Software already up to date!": 
-                    popup_info.PopupError(self.sm, outcome)
-                    
-                elif outcome == "Could not resolve host: github.com":
-                    description = "Could not connect to github. Please check that your connection is stable or try again later"
-                    popup_info.PopupError(self.sm, outcome)
-                else: 
-                    popup_info.PopupSoftwareUpdateSuccess(self.sm, outcome)
+            elif outcome == "Could not resolve host: github.com":
+                description = "Could not connect to github. Please check that your connection is stable, or try again later"
+                popup_info.PopupError(self.sm, outcome)
+            else: 
+                popup_info.PopupSoftwareUpdateSuccess(self.sm, outcome)
+        
+        Clock.schedule_once(lambda dt: do_update(), 2)
             
-            Clock.schedule_once(lambda dt: do_update(), 2)
-            
-        else: 
-            description = "No WiFi connection!"
-            popup_info.PopupError(self.sm, description)
+#         else: 
+#             description = "No WiFi connection!"
+#             popup_info.PopupError(self.sm, description)
 
     def repair_sw_over_wifi(self):
 
