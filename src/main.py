5a414a3eaed4d2700ac9087702deff9ab851ca78
skinn <skinn@HUAN-SOLO>
2020-05-20 15:06:39 +0100

    Start on welcome screen

--- src/main.py
+++ src/main.py
@@ -193,7 +193,7 @@ class SkavaUI(App):
         # sm.current is set at the end of start_services in serial_connection 
         # This ensures kivy has fully loaded and initial kivy schedule calls are safely made before screen is presented
         sm.current = 'welcome'
-        sm.current = 'go'
+#         sm.current = 'go'
 
         log('Screen manager activated')
         return sm
