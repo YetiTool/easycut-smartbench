468ae367460ff0a3bd6f134b306f6fbe9a3ce3e2
LettyYeti <lettie.adkins@yetitool.com>
2020-06-05 12:36:24 +0100

    test bigger label

--- src/asmcnc/skavaUI/screen_homing_prepare.py
+++ src/asmcnc/skavaUI/screen_homing_prepare.py
@@ -62,7 +62,7 @@ Builder.load_string("""
 
         Label:
             id: instruction_label
-            size_hint_y: 3
+            size_hint_y: 4
             text: ''
             markup: True
             font_size: '30px' 
