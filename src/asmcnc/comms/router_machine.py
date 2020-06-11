98a0fee990126c6a49848c299b43bc4e73597803
LettyYeti <lettie.adkins@yetitool.com>
2020-06-11 22:49:47 +0100

    testing jog & set

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -456,12 +456,13 @@ class RouterMachine(object):
 
     def set_x_datum_with_laser(self):
         self.jog_spindle_to_laser_datum()
-        self.poll_for_success = Clock.schedule_interval(wait_for_movement_to_complete, 0.5)
         
         def wait_for_movement_to_complete(self, dt):
             if not self.state() == 'Jog':
                 Clock.unschedule(self.poll_for_success)
                 self.set_x_datum()
+
+        self.poll_for_success = Clock.schedule_interval(wait_for_movement_to_complete, 0.5)
 
     def set_y_datum_with_laser(self):
         self.jog_spindle_to_laser_datum()
