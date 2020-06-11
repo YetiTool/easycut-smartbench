de24bd8d37bd8431318ab17f0f3fd44c9b2e1141
LettyYeti <lettie.adkins@yetitool.com>
2020-06-11 21:48:04 +0100

    testing read write

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -103,7 +103,7 @@ class RouterMachine(object):
     def read_z_head_laser_offset_values(self):
         try:
             file = open(self.z_head_laser_offset_file_path, 'r')
-            # [self.laser_offset_x_value, self.laser_offset_y_value] = file.readlines()
+            [self.laser_offset_x_value, self.laser_offset_y_value] = file.readlines()
             file.close
         except: 
             log("Unable to read z head laser offset values")
