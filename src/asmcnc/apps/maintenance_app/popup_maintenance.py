12308bbad8542182683711eca36d2510c0a9e45c
LettyYeti <lettie.adkins@yetitool.com>
2020-07-02 18:52:03 +0100

    added reset popup to laser screen

--- src/asmcnc/apps/maintenance_app/popup_maintenance.py
+++ src/asmcnc/apps/maintenance_app/popup_maintenance.py
@@ -18,32 +18,49 @@ class PopupResetOffset(Widget):
         self.sm = screen_manager
         
         description = "You are resetting the laser datum offset. Please confirm that this is where you have made a reference mark with the spindle."
+
+        def reset_laser_datum_offset(*args):
+            self.sm.get_screen('maintenance').laser_datum_buttons_widget.reset_laser_offset()
         
-        img = Image(size_hint_y=2, source="./asmcnc/skavaUI/img/popup_stop_visual.png", allow_stretch=True)
-        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description)
-        resume_button = Button(text='Resume')
-        cancel_button = Button(text='Cancel')
-        btn_layout = BoxLayout(orientation='horizontal', spacing=20, padding=0)
-        btn_layout.add_widget(resume_button)
-        btn_layout.add_widget(cancel_button)
+        img = Image(source="./asmcnc/apps/shapeCutter_app/img/info_icon.png", allow_stretch=False)
+        label = Label(size_hint_y=1.4, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[20,20], markup = True)
         
-        layout_plan = BoxLayout(orientation='vertical', spacing=10, padding=20)
+        ok_button = Button(text='[b]Yes[/b]', markup = True)
+        ok_button.background_normal = ''
+        ok_button.background_color = [76 / 255., 175 / 255., 80 / 255., 1.]
+        back_button = Button(text='[b]No, go back[/b]', markup = True)
+        back_button.background_normal = ''
+        back_button.background_color = [230 / 255., 74 / 255., 25 / 255., 1.]
+
+       
+        btn_layout = BoxLayout(orientation='horizontal', spacing=10, padding=[0,0,0,0])
+        btn_layout.add_widget(back_button)
+        btn_layout.add_widget(ok_button)
+        
+        layout_plan = BoxLayout(orientation='vertical', spacing=10, padding=[10,20,10,20])
         layout_plan.add_widget(img)
         layout_plan.add_widget(label)
         layout_plan.add_widget(btn_layout)
         
+
         popup = Popup(title='Reset laser datum offset',
+                      title_color=[0, 0, 0, 1],
+                      title_font= 'Roboto-Bold',
+                      title_size = '20sp',
                       content=layout_plan,
                       size_hint=(None, None),
-                      size=(600, 400),
-                      auto_dismiss= False)
+                      size=(500, 420),
+                      auto_dismiss= False
+                      )
         
-        cancel_button.bind(on_press=popup.dismiss)
-        resume_button.bind(on_press=self.reset_laser_datum_offset)
-        resume_button.bind(on_press=popup.dismiss)
+        popup.separator_color = [249 / 255., 206 / 255., 29 / 255., 1.]
+        popup.separator_height = '4dp'
+        popup.background = './asmcnc/apps/shapeCutter_app/img/popup_background.png'
         
-        popup.open()
+        ok_button.bind(on_press=popup.dismiss)
+        ok_button.bind(on_press=reset_laser_datum_offset)
+        back_button.bind(on_press=popup.dismiss)       
 
-    def reset_laser_datum_offset(self, *args):
-        self.sm.get_screen('maintenance').laser_datum_buttons_widget.reset_laser_offset()
+        popup.open() 
+
 
