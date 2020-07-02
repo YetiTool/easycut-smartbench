2ff1b0788a2fbf82a009d51f5c5973203fd9ead7
LettyYeti <lettie.adkins@yetitool.com>
2020-07-02 15:49:34 +0100

    no changes

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -398,47 +398,6 @@ class PopupWarning(Widget):
 
         popup.open()
 
-class PopupWarningWithBack(Widget):   
-    def __init__(self, screen_manager, warning_message):
-        
-        self.sm = screen_manager
-        
-        description = warning_message
-        
-        img = Image(source="./asmcnc/apps/shapeCutter_app/img/error_icon.png", allow_stretch=False)
-        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[0,0], markup = True)
-        
-        ok_button = Button(text='[b]Ok[/b]', markup = True)
-        ok_button.background_normal = ''
-        ok_button.background_color = [230 / 255., 74 / 255., 25 / 255., 1.]
-       
-        btn_layout = BoxLayout(orientation='horizontal', spacing=10, padding=[20,0,20,0])
-        btn_layout.add_widget(ok_button)
-        
-        layout_plan = BoxLayout(orientation='vertical', spacing=10, padding=[40,20,40,20])
-        layout_plan.add_widget(img)
-        layout_plan.add_widget(label)
-        layout_plan.add_widget(btn_layout)
-        
-        popup = Popup(title='Warning!',
-                      title_color=[0, 0, 0, 1],
-                      title_font= 'Roboto-Bold',
-                      title_size = '20sp',
-                      content=layout_plan,
-                      size_hint=(None, None),
-                      size=(500, 400),
-                      auto_dismiss= False
-                      )
-        
-        popup.separator_color = [230 / 255., 74 / 255., 25 / 255., 1.]
-        popup.separator_height = '4dp'
-        popup.background = './asmcnc/apps/shapeCutter_app/img/popup_background.png'
-        
-        ok_button.bind(on_press=popup.dismiss)    
-
-        popup.open()
-
-
 class PopupWait(Widget):   
     def __init__(self, screen_manager):
         
