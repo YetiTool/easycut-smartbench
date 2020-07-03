053c7135caecacf0713cfa66af9e59e6b6c9b62b
LettyYeti <lettie.adkins@yetitool.com>
2020-07-03 16:47:43 +0100

    fix datum popup shapecutter

--- src/asmcnc/apps/shapeCutter_app/screens/popup_input_error.py
+++ src/asmcnc/apps/shapeCutter_app/screens/popup_input_error.py
@@ -11,11 +11,9 @@ from kivy.uix.popup import Popup
 from kivy.properties import StringProperty  # @UnresolvedImport
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
-
+from kivy.uix.checkbox import CheckBox
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.label import Label
 from kivy.uix.button import  Button
 from kivy.uix.image import Image
