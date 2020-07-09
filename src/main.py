6010f9f9596c813237cf887ab6d202451b0ae257
LettyYeti <47244859+LettyYeti@users.noreply.github.com>
2020-07-09 12:23:36 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -69,7 +69,7 @@ from asmcnc.apps.maintenance_app import screen_maintenance
 Cmport = 'COM3'
 
 # Current version active/working on
-initial_version = 'v1.3.0'
+initial_version = 'v1.3.1'
 
 # default starting screen
 start_screen = 'safety'
