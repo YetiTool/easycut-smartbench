f994a7a89f165c861716f8e86662285e01d18e00
skinn <skinn@HUAN-SOLO>
2020-07-21 10:37:07 +0100

    Split recording to localDB and remote channel

--- src/asmcnc/comms/database_storage.py
+++ src/asmcnc/comms/database_storage.py
@@ -23,15 +23,24 @@ def log(message):
 
 class DatabaseStorage(object):
 
-
-    client = None
-    hostname = "SmartBench_0006" #TODO this needs to be serialised based on unique ID of SB console
+    
     sw_branch = "flurry_poc1" #TODO this is just an example of how we could track what SW we're running which is sending the data
+    machine_id = "SmartBench_0006" #TODO this needs to be serialised based on unique ID of SB console
+    
+    # LOCAL DB
+    localDBClient = None
+    
+    # PIPE TO REMOTE DB
+    channel = None
+    remote_hostname = "flurry.yetitool.com"
     
     
     def __init__(self):
 
-        host = "localhost"  # RasPi
+        
+        ### INTIALISE LOCAL DB
+        
+        local_hostname = "localhost"  # RasPi
         port = 8086  # default port
         user = "sensor"  # the user/password created for the pi, with write access
         password = "sensor" 
@@ -41,41 +50,72 @@ class DatabaseStorage(object):
         try:
             # Ansible may not have pre-installed this
             from influxdb import InfluxDBClient # database lib
-            self.client = InfluxDBClient(host, port, user, password, dbname)
+            self.localDBClient = InfluxDBClient(local_hostname, port, user, password, dbname)
 
         except:
-            print "Unable to initialise Flurry database. Have libs been installed? Or check DatabaseStorage credentials?"
+            print "Unable to initialise local database. Have libs been installed? Or check DatabaseStorage credentials?"
 
+        
+        ### INTIALISE PIPE TO REMOTE DB
+        
+        try:
+            # Ansible may not have pre-installed this
+            import pika
+
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host=self.remote_hostname))
+            self.channel = connection.channel()
+            
+        except:
+            print "Unable to create pipe to remote database. Have libs been installed? Or check DatabaseStorage credentials?"
+
+                
 
     def spindle_on(self):
-        self.set_value("spindle_on_off", 1)
+        self.record_value("spindle_on_off", 1)
 
 
     def spindle_off(self):
-        self.set_value("spindle_on_off", 0)
+        self.record_value("spindle_on_off", 0)
 
 
-    def set_value(self, name, value):
+    def record_value(self, name, value):
 
-        if self.client != None:
-
-            # Create the JSON data structure
-            log(str(name) + ": " + str(value))
-    
-            data = [
-                {
-                    "measurement": self.hostname,
-                    "tags": {
-                        "sw_branch": self.sw_branch,
-                        "grbl": "whatever"
-                    },
-                    "time": datetime.datetime.now(),
-                    "fields": {
-                        "value": value
-                    }
-                }
-            ]
+        if self.localDBClient != None:
+            self._record_in_local_db(name, value)
             
-            # Send the JSON data to InfluxDB
-            self.client.write_points(data)      
-     
+        if self.channel != None:
+            self._send_to_remote_db(name, value)
+  
+    
+    def _record_in_local_db(self, name, value):
+        
+        # Create the JSON data structure
+        log(str(name) + ": " + str(value))
+
+        data = [
+            {
+                "measurement": self.machine_id,
+                "tags": {
+                    "sw_branch": self.sw_branch,
+                    "grbl": "whatever"
+                },
+                "time": datetime.datetime.now(),
+                "fields": {
+                    "value": value
+                }
+            }
+        ]
+        
+        # Send the JSON data to InfluxDB
+        self.localDBClient.write_points(data)    
+        
+        
+    def _send_to_remote_db(self, name, value):
+        
+        message = "time:" + datetime.datetime.now() + "|machineID:" + self.machine_id + "|" + name + ":" + str(value)
+
+        self.channel.queue_declare(queue='hello')
+        
+        self.channel.basic_publish(exchange='', routing_key='hello', body=message)
+#         connection.close()
