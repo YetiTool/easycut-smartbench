52c1269721d64b0a692ef149792fbcf04c6e3f91
LettyYeti <lettie.adkins@yetitool.com>
2020-07-22 17:05:48 +0100

    using sudo

--- src/asmcnc/skavaUI/screen_developer.py
+++ src/asmcnc/skavaUI/screen_developer.py
@@ -353,8 +353,9 @@ class DeveloperScreen(Screen):
 ## Diagnostics
 
     def send_logs(self):
-        # os.system("/home/pi/console-raspi3b-plus-platform/ansible/templates/scp-logs.sh")
-        os.system("journalctl \> smartbench_logs.txt \&\& mv smartbench_logs.txt /media/usb/")      
+        if usb_stick.is_usb_mounted_flag == True:
+            # os.system("/home/pi/console-raspi3b-plus-platform/ansible/templates/scp-logs.sh")
+            os.system("journalctl > smartbench_logs.txt && sudo mv smartbench_logs.txt /media/usb/")
         
     def email_state(self):
         os.system("/home/pi/console-raspi3b-plus-platform/ansible/templates/e-mail-state.sh")
