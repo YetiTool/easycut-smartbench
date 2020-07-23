92481ff7926baa273d8105571b604a35c77b7451
LettyYeti <lettie.adkins@yetitool.com>
2020-07-23 14:54:35 +0100

    testing lookup popup

--- src/asmcnc/apps/shapeCutter_app/screens/popup_info.py
+++ src/asmcnc/apps/shapeCutter_app/screens/popup_info.py
@@ -11,11 +11,11 @@ from kivy.uix.popup import Popup
 from kivy.properties import StringProperty  # @UnresolvedImport
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
+from kivy.metrics import MetricsBase
 
 from kivy.uix.popup import Popup
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.label import Label
 from kivy.uix.button import  Button
 from kivy.uix.image import Image
