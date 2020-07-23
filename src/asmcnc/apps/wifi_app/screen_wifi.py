1d494b2c831ceb0644ba1cea5b4ff8037712bb3b
LettyYeti <lettie.adkins@yetitool.com>
2020-07-23 16:46:41 +0100

    try reconfigure line

--- src/asmcnc/apps/wifi_app/screen_wifi.py
+++ src/asmcnc/apps/wifi_app/screen_wifi.py
@@ -398,7 +398,8 @@ class WifiScreen(Screen):
                 os.system('echo "country="' + self.country + '| sudo tee --append /etc/wpa_supplicant/wpa_supplicant-wlan0.conf')
 
         # self.sm.current = 'rebooting'        
-        os.system('sudo ifdown --force wlan0. sudo ifup wlan0')
+        os.system('sudo ifdown --force wlan0 && sudo ifup wlan0')
+        self.sm.current = 'wifi'
 
     def refresh_ip_label_value(self, dt):
 
