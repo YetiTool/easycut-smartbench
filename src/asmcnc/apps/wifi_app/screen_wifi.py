ecd6c39940c7e1f19cf9b6224756b75922b44517
LettyYeti <lettie.adkins@yetitool.com>
2020-07-27 16:11:38 +0100

    test wifi spinner

--- src/asmcnc/apps/wifi_app/screen_wifi.py
+++ src/asmcnc/apps/wifi_app/screen_wifi.py
@@ -466,7 +466,7 @@ class WifiScreen(Screen):
     
     def get_available_networks(self):
         raw_SSID_list = os.popen('sudo iw dev wlan0 scan | grep SSID').read()
-        SSID_list = raw_SSID_list.replace('\tSSID: ','').split('\n')
+        SSID_list = raw_SSID_list.replace('\tSSID: ','').strip().split('\n')
         if '' in SSID_list: SSID_list.remove('')
         return SSID_list
 
