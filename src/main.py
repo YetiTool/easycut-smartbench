28ae187117219a4b16ccc2e8ae87570165129d5e
LettyYeti <lettie.adkins@yetitool.com>
2020-07-30 15:18:21 +0100

    first test of new alarm screen with showing details

--- src/main.py
+++ src/main.py
@@ -211,7 +211,7 @@ class SkavaUI(App):
         # Setting the first screen:        
         # sm.current is set at the end of start_services in serial_connection 
         # This ensures kivy has fully loaded and initial kivy schedule calls are safely made before screen is presented
-        sm.current = 'alarmScreen'
+        sm.current = start_screen
 
         log('Screen manager activated: ' + str(sm.current))
 
