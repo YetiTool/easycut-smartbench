3253edbcf354c467953720cb94ef4333b3aed721
LettyYeti <lettie.adkins@yetitool.com>
2020-07-30 16:14:21 +0100

    increase lines shown in details

--- src/asmcnc/skavaUI/screen_alarm.py
+++ src/asmcnc/skavaUI/screen_alarm.py
@@ -204,7 +204,7 @@ class AlarmScreenClass(Screen):
             details = ('\n').join(self.sm.get_screen('home').gcode_monitor_widget.status_report_buffer[3:])
             popup_info.PopupInfo(self.sm, 600, details)
 
-        Clock.schedule_once(lambda dt: trigger_popup(), 0.45)
+        Clock.schedule_once(lambda dt: trigger_popup(), 0.4)
 
 
     def quit_to_home(self):
