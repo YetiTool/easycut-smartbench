7a7935087cb6d330e2084232ecb2183a261e0dab
LettyYeti <lettie.adkins@yetitool.com>
2020-08-25 15:27:17 +0100

    add spindle RPM label instead

--- src/asmcnc/skavaUI/widget_speed_override.py
+++ src/asmcnc/skavaUI/widget_speed_override.py
@@ -78,7 +78,7 @@ Builder.load_string("""
                     allow_stretch: True  
         Label:
             id: spindle_rpm
-            size_hiny_y: 0.2
+            size_hiny_y: 0.15
             text: '0 RPM'
             font_size: '16px' 
             valign: 'middle'
@@ -104,7 +104,7 @@ class SpeedOverride(Widget):
         self.sm=kwargs['screen_manager']     
 
     def update_spindle_speed_label(self):
-        self.spindle_rpm.text = str(self.m.spindle_speed()) + ' RPM'
+        self.spindle_rpm.text = '[color=808080]' + str(self.m.spindle_speed()) + ' RPM' + '[/color]'
 
     def speed_up(self):
         if self.speed_override_percentage < 200: self.speed_override_percentage += 5
