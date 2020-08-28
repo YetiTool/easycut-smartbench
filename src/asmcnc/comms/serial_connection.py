7e8e6f4793af37c5cfeee5a79f07094a0bc2e2c4
LettyYeti <lettie.adkins@yetitool.com>
2020-08-28 17:12:34 +0100

    buffer bug? timing bug?

--- src/asmcnc/comms/serial_connection.py
+++ src/asmcnc/comms/serial_connection.py
@@ -187,8 +187,9 @@ class SerialConnection(object):
                 
             del self.write_command_buffer[0:(command_counter)]
 
-
-            print self.write_realtime_buffer
+            print "Buffer"
+            if self.write_realtime_buffer != []:
+                print self.write_realtime_buffer
 
             realtime_counter = 0
             for realtime_command in self.write_realtime_buffer:
