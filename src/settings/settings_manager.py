b9f821e1a5229c18a73caae7cbb22463af47e20b
LettyYeti <lettie.adkins@yetitool.com>
2020-09-24 15:31:24 +0100

    test

--- src/settings/settings_manager.py
+++ src/settings/settings_manager.py
@@ -61,9 +61,9 @@ class Settings(object):
         self.pl_branch = str(os.popen("cd /home/pi/console-raspi3b-plus-platform/ && git branch | grep \*").read()).strip('\n')
 
     def refresh_latest_platform_version(self):
-        if self.sm.current_screen == 'dev':
+        if self.sm.current == 'dev':
             os.system("cd /home/pi/console-raspi3b-plus-platform/ && git fetch --tags --quiet")
-        self.latest_platform_version = str(os.popen("git describe --tags `git rev-list --tags --max-count=1`").read()).strip('\n')
+        self.latest_platform_version = str(os.popen("cd /home/pi/console-raspi3b-plus-platform/ && git describe --tags `git rev-list --tags --max-count=1`").read()).strip('\n')
 
             
 ## GET SOFTWARE UPDATES
