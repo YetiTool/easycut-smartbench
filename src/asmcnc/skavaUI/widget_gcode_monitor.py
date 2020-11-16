6e1fab346298350a0759c8336cfa3c0dbbdf340b
LettyYeti <lettie.adkins@yetitool.com>
2020-11-16 16:34:03 +0000

    undoing any test changes

--- src/asmcnc/skavaUI/widget_gcode_monitor.py
+++ src/asmcnc/skavaUI/widget_gcode_monitor.py
@@ -13,7 +13,6 @@ from kivy.base import runTouchApp
 from kivy.uix.scrollview import ScrollView
 from kivy.properties import ObjectProperty, NumericProperty, StringProperty # @UnresolvedImport
 from asmcnc.skavaUI import popup_info
-from kivy.core.window import Window
 
 Builder.load_string("""
 
