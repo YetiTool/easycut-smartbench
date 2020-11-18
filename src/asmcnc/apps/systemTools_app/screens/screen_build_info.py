d22fb369123e745c990b8ab3173657ac8478c94e
LettyYeti <lettie.adkins@yetitool.com>
2020-11-18 14:15:56 +0000

    setting up initial framework for system tools app

--- src/asmcnc/apps/systemTools_app/screens/screen_build_info.py
+++ src/asmcnc/apps/systemTools_app/screens/screen_build_info.py
@@ -18,42 +18,18 @@ Builder.load_string("""
     BoxLayout:
         height: dp(800)
         width: dp(480)
-	    canvas.before:
-	        Color: 
-	            rgba: [226 / 255., 226 / 255., 226 / 255., 1.]
-	        Rectangle: 
-	            size: self.size
-	            pos: self.pos
+        canvas.before:
+            Color: 
+                rgba: [226 / 255., 226 / 255., 226 / 255., 1.]
+            Rectangle: 
+                size: self.size
+                pos: self.pos
 
         BoxLayout:
             padding: 10
             spacing: 10
             orientation: "vertical"
-	        BoxLayout:
-	            padding: 0
-	            spacing: 0
-                canvas:
-                    Color:
-                        rgba: [1,1,1,1]
-                    RoundedRectangle:
-                        pos: self.pos
-                        size: self.size
-	            Label:
-	                size_hint: (None,None)
-	                height: dp(70)
-	                width: dp(780)
-	                text: "Build Information"
-	                color: [0,0,0,1]
-	                font_size: 30
-	                halign: "center"
-	                valign: "bottom"
-	                markup: True
-                   
-                    
             BoxLayout:
-                size_hint: (None,None)
-                width: dp(780)
-                height: dp(380)
                 padding: 0
                 spacing: 0
                 canvas:
@@ -62,6 +38,70 @@ Builder.load_string("""
                     RoundedRectangle:
                         pos: self.pos
                         size: self.size
+                Label:
+                    size_hint: (None,None)
+                    height: dp(70)
+                    width: dp(780)
+                    text: "Build Information"
+                    color: [0,0,0,1]
+                    font_size: 30
+                    halign: "center"
+                    valign: "bottom"
+                    markup: True
+                   
+                    
+            BoxLayout:
+                size_hint: (None,None)
+                width: dp(780)
+                height: dp(270)
+                padding: 0
+                spacing: 0
+                canvas:
+                    Color:
+                        rgba: [1,1,1,1]
+                    RoundedRectangle:
+                        pos: self.pos
+                        size: self.size
+
+           #      GridLayout: 
+                    # size: self.parent.size
+                    # pos: self.parent.pos
+                    # cols: 6
+                    # rows: 4
+
+            BoxLayout:
+                size_hint: (None,None)
+                width: dp(780)
+                height: dp(100)
+                padding: 0
+                spacing: 10
+                orientation: 'horizontal'
+                BoxLayout:
+                    size_hint: (None,None)
+                    width: dp(385)
+                    height: dp(100)
+                    padding: 0
+                    spacing: 0
+                canvas:
+                    Color:
+                        rgba: [1,1,1,1]
+                    RoundedRectangle:
+                        pos: self.pos
+                        size: self.size
+
+                BoxLayout:
+                    size_hint: (None,None)
+                    width: dp(385)
+                    height: dp(100)
+                    padding: 0
+                    spacing: 0
+                canvas:
+                    Color:
+                        rgba: [1,1,1,1]
+                    RoundedRectangle:
+                        pos: self.pos
+                        size: self.size
+
 
 """)
 
@@ -72,7 +112,7 @@ class BuildInfoScreen(Screen):
         self.systemtools_sm = kwargs['system_tools']
 
     def go_back(self):
-    	self.systemtools_sm.back_to_menu()
+        self.systemtools_sm.back_to_menu()
 
     def exit_app(self):
-    	self.systemtools_sm.exit_app()
+        self.systemtools_sm.exit_app()
