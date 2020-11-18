29ce15a7de280a0ba7e9d4910509895132e97fb0
LettyYeti <lettie.adkins@yetitool.com>
2020-11-18 16:47:01 +0000

    test beta testers screen

--- src/asmcnc/apps/systemTools_app/screens/screen_beta_testers.py
+++ src/asmcnc/apps/systemTools_app/screens/screen_beta_testers.py
@@ -22,6 +22,171 @@ Builder.load_string("""
                 size: self.size
                 pos: self.pos
 
+        BoxLayout:
+            padding: 10
+            spacing: 10
+            orientation: "vertical"
+            BoxLayout:
+                padding: 0
+                spacing: 0
+                canvas:
+                    Color:
+                        rgba: [1,1,1,1]
+                    RoundedRectangle:
+                        pos: self.pos
+                        size: self.size
+                Label:
+                    size_hint: (None,None)
+                    height: dp(70)
+                    width: dp(780)
+                    text: "Build Information"
+                    color: [0,0,0,1]
+                    font_size: 30
+                    halign: "center"
+                    valign: "bottom"
+                    markup: True
+                   
+                    
+            BoxLayout:
+                size_hint: (None,None)
+                width: dp(780)
+                height: dp(240)
+                padding: 0
+                spacing: 0
+                canvas:
+                    Color:
+                        rgba: [1,1,1,1]
+                    RoundedRectangle:
+                        pos: self.pos
+                        size: self.size
+
+                Label
+                    text: 'Run developer branch:'
+                    color: [0,0,0,1]
+
+                TextInput:
+                    id: user_branch
+                    text: 'branch'
+                    multiline: False
+
+                Button:
+                    text: 'Checkout and pull'
+
+
+            BoxLayout:
+                size_hint: (None,None)
+                width: dp(780)
+                height: dp(130)
+                padding: 0
+                spacing: 10
+                orientation: 'horizontal'
+
+                BoxLayout:
+                    size_hint: (None,None)
+                    width: dp(192.5)
+                    height: dp(130)
+                    padding: 0
+                    spacing: 0
+                    canvas:
+                        Color:
+                            rgba: [1,1,1,1]
+                        RoundedRectangle:
+                            pos: self.pos
+                            size: self.size
+                    BoxLayout: 
+                        size_hint: (None, None)
+                        height: dp(130)
+                        width: dp(192.5)
+                        padding: [52.25,31,52.25,31]
+                        Button:
+                            size_hint: (None,None)
+                            height: dp(68)
+                            width: dp(88)
+                            background_color: hex('#F4433600')
+                            center: self.parent.center
+                            pos: self.parent.pos
+                            on_press: root.go_back()
+                            BoxLayout:
+                                padding: 0
+                                size: self.parent.size
+                                pos: self.parent.pos
+                                Image:
+                                    source: "./asmcnc/apps/shapeCutter_app/img/arrow_back.png"
+                                    center_x: self.parent.center_x
+                                    y: self.parent.y
+                                    size: self.parent.width, self.parent.height
+                                    allow_stretch: True
+
+                BoxLayout:
+                    size_hint: (None,None)
+                    width: dp(385)
+                    height: dp(130)
+                    padding: 0
+                    spacing: 0
+                    orientation: 'vertical'
+                    canvas:
+                        Color:
+                            rgba: [1,1,1,1]
+                        RoundedRectangle:
+                            pos: self.pos
+                            size: self.size
+
+                    GridLayout:
+                        size: self.parent.size
+                        pos: self.parent.pos
+                        cols: 2
+                        rows: 0
+
+                        Label:
+                            text: 'Latest beta version:'
+                            color: [0,0,0,1]
+                        Label:
+                            id: beta_version
+                            text: 'beta_version_no'
+                            color: [0,0,0,1]
+                    Button:
+                        text: 'Update to beta'
+
+
+
+                BoxLayout:
+                    size_hint: (None,None)
+                    width: dp(192.5)
+                    height: dp(130)
+                    padding: 0
+                    spacing: 0
+                    canvas:
+                        Color:
+                            rgba: [1,1,1,1]
+                        RoundedRectangle:
+                            pos: self.pos
+                            size: self.size
+
+                    BoxLayout: 
+                        size_hint: (None, None)
+                        height: dp(130)
+                        width: dp(192.5)
+                        padding: [40.25,9,40.25,9] 
+                        Button:
+                            size_hint: (None,None)
+                            height: dp(112)
+                            width: dp(112)
+                            background_color: hex('#F4433600')
+                            center: self.parent.center
+                            pos: self.parent.pos
+                            on_press: root.exit_app()
+                            BoxLayout:
+                                padding: 0
+                                size: self.parent.size
+                                pos: self.parent.pos
+                                Image:
+                                    source: "./asmcnc/apps/wifi_app/img/quit.png"
+                                    center_x: self.parent.center_x
+                                    y: self.parent.y
+                                    size: self.parent.width, self.parent.height
+                                    allow_stretch: True
+
+
 """)
 
 class BetaTestersScreen(Screen):
