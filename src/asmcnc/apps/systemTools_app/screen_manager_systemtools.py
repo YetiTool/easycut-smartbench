68c7f94c3333e82b3a08cabd2f2fe004b28d889b
LettyYeti <lettie.adkins@yetitool.com>
2020-11-18 16:13:47 +0000

    putting in downloading logs function:

--- src/asmcnc/apps/systemTools_app/screen_manager_systemtools.py
+++ src/asmcnc/apps/systemTools_app/screen_manager_systemtools.py
@@ -36,11 +36,11 @@ class ScreenManagerSystemTools(object):
 
             if self.usb_stick.is_usb_mounted_flag == True:
                 os.system("journalctl > smartbench_logs.txt && sudo cp --no-preserve=mode,ownership smartbench_logs.txt /media/usb/ && rm smartbench_logs.txt")
-                self.usb_stick.disable()
-                
-                message = 'Logs downloaded'
-                # updated_wait_popup = popup_info.PopupWait(self.sm, description = message)
                 wait_popup.popup.dismiss()
+                self.usb_stick.disable()
+
+                # message = 'Logs downloaded'
+                # updated_wait_popup = popup_info.PopupWait(self.sm, description = message)
                 # Clock.schedule_once(lambda dt: updated_wait_popup.popup.dismiss(), 0.5)
 
             else:
