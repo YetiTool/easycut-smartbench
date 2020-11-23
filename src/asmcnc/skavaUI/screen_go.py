a49639d7a27df66aecb5876ad33670309a18c773
LettyYeti <lettie.adkins@yetitool.com>
2020-11-23 15:06:59 +0000

    refining size of text box

--- src/asmcnc/skavaUI/screen_go.py
+++ src/asmcnc/skavaUI/screen_go.py
@@ -345,7 +345,7 @@ Builder.load_string("""
 
                         Label:
                             id: spindle_voltage
-                            size_hint_y: 0.6
+                            height: 10
                             text: '0'
                             font_size: '16px' 
                             valign: 'middle'
@@ -421,11 +421,13 @@ class GoScreen(Screen):
             self.update_overload_label(self.m.s.overload_state)
             self.spindle_overload_container.size_hint_y = 0.25
             self.spindle_overload_container.opacity = 1
+            self.spindle_voltage.opacity = 1
 
         else: 
             self.spindle_overload_container.height = 0
             self.spindle_overload_container.size_hint_y = 0
             self.spindle_overload_container.opacity = 0
+            self.spindle_voltage.opacity = 0
 
         self.loop_for_job_progress = Clock.schedule_interval(self.poll_for_job_progress, 1)  # then poll repeatedly
         self.loop_for_feeds_and_speeds = Clock.schedule_interval(self.poll_for_feeds_and_speeds, 0.2)  # then poll repeatedly
