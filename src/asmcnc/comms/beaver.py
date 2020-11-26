16f181514070293ef6330c3f4f087486969cbce1
LettyYeti <lettie.adkins@yetitool.com>
2020-11-26 12:16:23 +0000

    created new beaver object to manage logging, including sending it to file

---
+++ src/asmcnc/comms/beaver.py
@@ -0,0 +1,39 @@
+'''
+Log dam
+
+BECAUSE IT'S A STACK OF LOGS C:
+
+@author Letty
+'''
+
+from datetime import datetime
+import os
+from kivy.clock import Clock
+
+dam_path = '/home/pi/smartbench_dam.txt'
+
+class Beaver(object):
+
+    dam = []
+
+    def __init__(self):
+        self.make_log("Start beaver...")
+        Clock.schedule_interval(self.sustain_dam, 10)
+
+    def make_log(self, message):
+        timestamp = datetime.now()
+        log = (timestamp.strftime('%H:%M:%S.%f' )[:12] + ' ' + str(message))
+        self.add_to_dam(log)
+
+    def add_to_dam(self, log):
+        self.dam.append(log)
+        print log
+        os.system('sed -ni \'1s/^/' + log + '\\n/;10000q;p\'' + dam_path)
+
+    def sustain_dam(self, dt):
+        if len(self.dam) > 60:
+            self.dam = self.dam[-60:]
+
+
+
+
