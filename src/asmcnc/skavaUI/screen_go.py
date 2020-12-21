77ff1b1804cb85eca3d6f429bf3266f814b143c3
LettyYeti <lettie.adkins@yetitool.com>
2020-12-21 10:15:41 +0000

    adjust size of z height container depending on mode

--- src/asmcnc/skavaUI/screen_go.py
+++ src/asmcnc/skavaUI/screen_go.py
@@ -423,12 +423,14 @@ class GoScreen(Screen):
             self.spindle_overload_container.size_hint_y = 0.25
             self.spindle_overload_container.opacity = 1
             self.spindle_overload_container.padding = [0,0,0,-10]
+            self.z_height_container.size_hint_y = 0.95
 
         else: 
             self.spindle_overload_container.height = 0
             self.spindle_overload_container.size_hint_y = 0
             self.spindle_overload_container.opacity = 0
             self.spindle_overload_container.padding = 0
+            self.z_height_container.size_hint_y = 1
 
         self.loop_for_job_progress = Clock.schedule_interval(self.poll_for_job_progress, 1)  # then poll repeatedly
         self.loop_for_feeds_and_speeds = Clock.schedule_interval(self.poll_for_feeds_and_speeds, 0.2)  # then poll repeatedly
