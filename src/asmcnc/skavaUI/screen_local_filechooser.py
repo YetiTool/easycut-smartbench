54e9570300761ebe0e5c82e67b5f6859041489b2
LettyYeti <lettie.adkins@yetitool.com>
2021-01-12 14:27:19 +0000

    colour test

--- src/asmcnc/skavaUI/screen_local_filechooser.py
+++ src/asmcnc/skavaUI/screen_local_filechooser.py
@@ -267,6 +267,7 @@ class LocalFileChooser(Screen):
             self.image_usb.source = './asmcnc/skavaUI/img/file_select_usb.png'
             self.sm.get_screen('loading').usb_status_label.opacity = 1
             self.usb_status_label.size_hint_y = 1
+            self.usb_status_label.canvas.before.Color.rgba = hex('#4CAF50FF')
 
         else:
             self.button_usb.disabled = True
