3fcbf519ef77dd4139697b734284acdce5699d57
LettyYeti <lettie.adkins@yetitool.com>
2021-01-28 12:09:56 +0000

    updated shutdown info screen for user friendlinesss

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -1029,7 +1029,7 @@ class PopupShutdown(Widget):
           os.system('sudo shutdown -h now')
         
         img = Image(source="./asmcnc/apps/shapeCutter_app/img/info_icon.png", allow_stretch=False)
-        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[40,20], markup = True)
+        label = Label(size_hint_y=1.2, text_size=(480, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=0, markup = True)
 
         ok_button = Button(text='[b]Shutdown now[/b]', markup = True)
         ok_button.background_normal = ''
