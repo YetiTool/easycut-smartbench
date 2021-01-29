a8e0856d2c4884119cd997bb8fd14b35d6cda167
LettyYeti <lettie.adkins@yetitool.com>
2021-01-29 16:25:25 +0000

    test read-in

--- src/asmcnc/comms/localization.py
+++ src/asmcnc/comms/localization.py
@@ -11,7 +11,7 @@ class Localization(object):
 
 	# use this for just getting user language, and if it's empty just assume english
 	persistent_language_path = '/home/pi/easycut-smartbench/src/sb_values/user_language.txt'
-	complete_foriegn_dictionary_path = '/home/pi/easycut-smartbench/src/asmcnc/comms/foreign_dictionary.csv'
+	complete_foreign_dictionary_path = '/home/pi/easycut-smartbench/src/asmcnc/comms/foreign_dictionary.csv'
 
 	default_lang = 'English (GB)'
 	lang = default_lang
@@ -28,11 +28,16 @@ class Localization(object):
 	def load_in_new_language(self, language):
 		self.lang = language
 
-		with open(self.complete_foriegn_dictionary_path, "r") as csv_file:
+		with open(self.complete_foreign_dictionary_path, "r") as csv_file:
 		    csv_reader = csv.DictReader(csv_file, delimiter=',')
 		    for lines in csv_reader:
 		    	self.dictionary[str(lines[self.default_lang])] = str(lines[self.lang])
 
-		print(self.dictionary['System Information'])
+	def save_active_dictionary(self):
+		with open(self.complete_foreign_dictionary_path,  'w', newline='') as csv_file:
+		    csv_reader = csv.DictReader(csv_file, delimiter=',')
+
+		    lang_writer = csv.writer(csvfile, delimiter=',')
+		    
 
 	supported_languages = ['English (GB)', 'Korean (KOR)', 'German (DE)', 'French (FR)', 'Italian (IT)']
