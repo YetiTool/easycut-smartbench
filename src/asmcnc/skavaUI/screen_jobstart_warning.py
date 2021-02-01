a636287358d56c492e96f6619af4d5cdc3fd737f
LettyYeti <lettie.adkins@yetitool.com>
2021-02-01 17:57:10 +0000

    tweaking formatting

--- src/asmcnc/skavaUI/screen_jobstart_warning.py
+++ src/asmcnc/skavaUI/screen_jobstart_warning.py
@@ -68,7 +68,7 @@ Builder.load_string("""
                 size_hint: (None,None)
                 width: dp(800)
                 height: dp(300)
-                padding: [40,20,40,20]
+                padding: [40,20,40,0]
                 spacing: 20
                 orientation: 'vertical'
              
