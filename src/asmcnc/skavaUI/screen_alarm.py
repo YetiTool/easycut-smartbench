bafb621e1067788b619804e31a064693a1596c59
LettyYeti <lettie.adkins@yetitool.com>
2021-02-09 15:41:39 +0000

    test alarm screen

--- src/asmcnc/skavaUI/screen_alarm.py
+++ src/asmcnc/skavaUI/screen_alarm.py
@@ -152,7 +152,7 @@ Builder.load_string("""
                     text_size: self.size
                     valign: "middle"
                     halign: "center"
-                    padding_x: [100, 0]
+                    padding_x: 50
 
                     # BoxLayout:
                     #     padding: 0
