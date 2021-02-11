417c443b28a787302f441eca070cf674f73ec4f9
LettyYeti <lettie.adkins@yetitool.com>
2021-02-11 11:38:26 +0000

    translate second safety screen - reformatting

--- src/asmcnc/skavaUI/screen_jobstart_warning.py
+++ src/asmcnc/skavaUI/screen_jobstart_warning.py
@@ -81,7 +81,7 @@ Builder.load_string("""
              
                 BoxLayout:
                     orientation: 'horizontal'
-                    spacing:0
+                    spacing:10
                     size_hint_y: 1.22
                     BoxLayout:
                         padding: 20
@@ -127,7 +127,7 @@ Builder.load_string("""
 
                     BoxLayout:
                         orientation: 'horizontal'
-                        spacing:0
+                        spacing:10
                         size_hint_x: 0.75
 
                         BoxLayout:
