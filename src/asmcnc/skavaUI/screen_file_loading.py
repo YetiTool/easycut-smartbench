ef45704b49250c0afacf8405a0420efafe1273a6
LettyYeti <lettie.adkins@yetitool.com>
2021-02-12 11:49:41 +0000

    sort out file loading screen

--- src/asmcnc/skavaUI/screen_file_loading.py
+++ src/asmcnc/skavaUI/screen_file_loading.py
@@ -116,7 +116,7 @@ Builder.load_string("""
                 id: warning_body_label
                 font_size: '22sp'
                 halign: 'center'
-                valign: 'top'
+                valign: 'center'
                 size_hint_y: 1.7
                 markup: True
                 size: self.texture_size
