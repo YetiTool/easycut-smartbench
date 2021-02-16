800e7867ec760797a5920d0776eb1935eb1e7075
LettyYeti <lettie.adkins@yetitool.com>
2021-02-16 14:24:25 +0000

    debugging homing

--- src/asmcnc/production/process_micrometer_read.py
+++ src/asmcnc/production/process_micrometer_read.py
@@ -430,16 +430,23 @@ class ProcessMicrometerScreen(Screen):
 
     def format_output(self):
 
-        self.HOME_abs_initial_value = self.HOME_DTI_abs_list[0]
-        self.FAR_abs_initial_value = self.FAR_DTI_abs_list[0]
 
-        self.HOME_zeroed_list = [(H - self.HOME_abs_initial_value) for H in self.HOME_DTI_abs_list]
-        self.FAR_zeroed_list = [(F - self.FAR_abs_initial_value) for F in self.FAR_DTI_abs_list]
+        try: 
+            self.HOME_abs_initial_value = self.HOME_DTI_abs_list[0]
+            self.HOME_zeroed_list = [(H - self.HOME_abs_initial_value) for H in self.HOME_DTI_abs_list]
+        except: pass
+        try: 
+            self.FAR_abs_initial_value = self.FAR_DTI_abs_list[0]
+            self.FAR_zeroed_list = [(F - self.FAR_abs_initial_value) for F in self.FAR_DTI_abs_list]
+        except: pass
+
+        
+        
 
         self.HOME_Y_pos_list_converted = self.convert_to_json(self.HOME_Y_pos_list)
         self.FAR_Y_pos_list_converted = self.convert_to_json(self.FAR_Y_pos_list)
-        self.HOME_abs_converted = self.convert_to_json(self.HOME_DTI_abs_list)
-        self.FAR_abs_converted = self.convert_to_json(self.FAR_DTI_abs_list)
+        self.HOME_DTI_abs_list_converted = self.convert_to_json(self.HOME_DTI_abs_list)
+        self.FAR_DTI_abs_list_converted = self.convert_to_json(self.FAR_DTI_abs_list)
         self.HOME_zeroed_converted = self.convert_to_json(self.HOME_zeroed_list)
         self.FAR_zeroed_converted = self.convert_to_json(self.FAR_zeroed_list)
 
@@ -495,16 +502,16 @@ class ProcessMicrometerScreen(Screen):
 
         log("Writing DTI measurements to Gsheet")
 
-        if self.HOME_DTI_abs_list != []:
+        if self.HOME_DTI_abs_list_converted != []:
             self.home_data_status = 'Sending...'
-            worksheet.update('C3:C', self.HOME_Y_pos_list)
-            worksheet.update('D3:D', self.HOME_DTI_abs_list)
+            worksheet.update('C3:C', self.HOME_Y_pos_list_converted)
+            worksheet.update('D3:D', self.HOME_DTI_abs_list_converted)
             log('Home side data sent')
 
-        if self.FAR_DTI_abs_list != []:
+        if self.FAR_DTI_abs_list_converted != []:
             self.far_data_status = 'Sending...'
-            worksheet.update('E2:E', self.FAR_Y_pos_list)
-            worksheet.update('F2:F', self.FAR_DTI_abs_list)
+            worksheet.update('E2:E', self.FAR_Y_pos_list_converted)
+            worksheet.update('F2:F', self.FAR_DTI_abs_list_converted)
             log('Home side data sent')
 
         current_utc = datetime.utcnow()
