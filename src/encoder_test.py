355bbba6d2e7e06b37b5d0573e566eacf3f9cebd
LettyYeti <lettie.adkins@yetitool.com>
2021-02-19 16:48:25 +0000

    inital testing for squareness jig

--- src/encoder_test.py
+++ src/encoder_test.py
@@ -7,7 +7,7 @@ from asmcnc.production import encoder_connection
 
 AMA0 = 'ttyAMA0' # check these when HW is installed
 AMA1 = 'ttyAMA1' # check these when HW is installed
-y_length = float(2645 - 20)
+
 encoder_resolution = 0.025 # mm (25 microns)
 
 sm = None
@@ -15,5 +15,5 @@ e_test = encoder_connection.EncoderConnection(sm, AMA0)
 
 
 while True: 
-	print(str(float(self.e_test.F_side + self.e_test.F_side)*encoder_resolution))
+	print(str(float(e_test.F_side + e_test.F_side)*encoder_resolution))
 	time.sleep(2)
