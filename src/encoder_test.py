f9b93f5716caca871ee252425292029d72a1c4ce
LettyYeti <lettie.adkins@yetitool.com>
2021-02-19 17:13:33 +0000

    inital testing for squareness jig

--- src/encoder_test.py
+++ src/encoder_test.py
@@ -5,8 +5,8 @@ Test to read in encoder & print live readings
 import time
 from asmcnc.production import encoder_connection
 
-AMA0 = 'ttyAMA0' # check these when HW is installed
-AMA1 = 'ttyAMA1' # check these when HW is installed
+AMA0 = 'ttyACM0' # check these when HW is installed
+AMA1 = 'ttyACM1' # check these when HW is installed
 
 encoder_resolution = 0.025 # mm (25 microns)
 
