4ffd416af52343c4785ed3f4d0f95042732daa2f
LettyYeti <lettie.adkins@yetitool.com>
2021-02-19 16:47:41 +0000

    inital testing for squareness jig

--- src/encoder_test.py
+++ src/encoder_test.py
@@ -10,7 +10,8 @@ AMA1 = 'ttyAMA1' # check these when HW is installed
 y_length = float(2645 - 20)
 encoder_resolution = 0.025 # mm (25 microns)
 
-e_test = encoder_connection.EncoderConnection(self.sm, AMA0)
+sm = None
+e_test = encoder_connection.EncoderConnection(sm, AMA0)
 
 
 while True: 
