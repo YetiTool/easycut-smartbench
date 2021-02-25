787e5388e0d043ddea612ecf4da7dee7dcfced91
LettyYeti <lettie.adkins@yetitool.com>
2021-02-25 12:49:43 +0000

    debugging

--- src/asmcnc/production/process_linear_encoder_read.py
+++ src/asmcnc/production/process_linear_encoder_read.py
@@ -481,13 +481,13 @@ class ProcessLinearEncoderScreen(Screen):
         # multiply everything by -1 to get a positive number, which affects graph formatting in google sheets
         if self.FORWARDS:
 
-            HOME_measured_distance = [(float(-1*self.starting_pos + ((H - self.HOME_raw_pulse_list[0])*encoder_resolution))) for H in self.HOME_raw_pulse_list]
-            FAR_measured_distance = [(float(-1*self.starting_pos + ((F - self.FAR_raw_pulse_list[0])*encoder_resolution))) for F in self.FAR_raw_pulse_list]    
+            HOME_measured_distance = [-1*(self.starting_pos + (H - self.starting_H)*encoder_resolution) for H in self.HOME_raw_pulse_list]
+            FAR_measured_distance = [-1*(self.starting_pos + (F - self.starting_F)*encoder_resolution) for F in self.FAR_raw_pulse_list]
         
         else:
 
-            HOME_measured_distance = [(float(-1*self.starting_pos - ((H - self.HOME_raw_pulse_list[0])*encoder_resolution))) for H in self.HOME_raw_pulse_list]
-            FAR_measured_distance = [(float(-1*self.starting_pos - ((F - self.FAR_raw_pulse_list[0])*encoder_resolution))) for F in self.FAR_raw_pulse_list]
+            HOME_measured_distance = [-1*(self.starting_pos - (H - self.starting_H)*encoder_resolution) for H in self.HOME_raw_pulse_list]
+            FAR_measured_distance = [-1*(self.starting_pos - (F - self.starting_F)*encoder_resolution) for F in self.FAR_raw_pulse_list]
 
         # make positive for benefits of graphing
         machine_coordinates = [-1*Y for Y in self.Y_pos_list]
@@ -520,7 +520,7 @@ class ProcessLinearEncoderScreen(Screen):
         self.aggregate_offset = self.convert_to_json(DELTA_Y)
 
         self.HOME_distance_abs = self.convert_to_json(HOME_measured_distance)
-        self.FAR_distance_abs = self.convert_to_json(FAR_measured_distance)        
+        self.FAR_distance_abs = self.convert_to_json(FAR_measured_distance)   
 
 
     def convert_to_json(self, data):
