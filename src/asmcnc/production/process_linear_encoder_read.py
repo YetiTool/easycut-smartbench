fd947d4f726cb5dd902c16dedfc58c25cd62a7f7
LettyYeti <lettie.adkins@yetitool.com>
2021-03-02 16:34:31 +0000

    5 second test

--- src/asmcnc/production/process_linear_encoder_read.py
+++ src/asmcnc/production/process_linear_encoder_read.py
@@ -123,7 +123,7 @@ Builder.load_string("""
 
                 TextInput: 
                     id: bench_id 
-                    text: "test-2-secs"
+                    text: "test-5-secs"
                     multiline: False
 
                 ToggleButton:
@@ -220,7 +220,7 @@ Builder.load_string("""
 
 class ProcessLinearEncoderScreen(Screen):
 
-    POLL_TIME = 2
+    POLL_TIME = 5
 
     # LISTS TO HOLD RAW RECORDED DATA
     Y_pos_list = []
