3d504f9bfa7d117aa47107d00b1f1e1aedc2101c
LettyYeti <lettie.adkins@yetitool.com>
2021-03-12 15:02:45 +0000

    auto-finding new test ids for serial number

--- src/asmcnc/production/process_linear_encoder_read.py
+++ src/asmcnc/production/process_linear_encoder_read.py
@@ -121,6 +121,7 @@ Builder.load_string("""
                     text: "YB"
                     multiline: False
                     font_size: '20sp'
+                    on_text_validate: root.generate_test_id()
 
                 TextInput: 
                     id: test_id
