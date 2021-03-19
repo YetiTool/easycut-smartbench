07163c4546a44839d374e9b706ee95c831cf161e
LettyYeti <lettie.adkins@yetitool.com>
2021-03-19 15:49:31 +0000

    test erronous usb stick handling

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -351,7 +351,51 @@ class PopupUSBInfo(Widget):
         ok_button.bind(on_press=self.popup.dismiss)
 #         back_button.bind(on_press=popup.dismiss)       
 
-        self.popup.open()  
+        self.popup.open()
+
+class PopupUSBError(Widget):   
+    def __init__(self, screen_manager, usb):
+        
+        self.sm = screen_manager
+        
+        description = "Problem mounting USB stick.\nPlease check that your USB stick is working properly.\n\nIf this error persists, you may need to reformat your USB stick."
+
+        def restart_polling(*args):
+          usb.start_polling_for_usb()
+        
+        img = Image(source="./asmcnc/apps/shapeCutter_app/img/error_icon.png", allow_stretch=False)
+        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[0,10], markup = True)
+        
+        ok_button = Button(text='[b]Ok[/b]', markup = True)
+        ok_button.background_normal = ''
+        ok_button.background_color = [230 / 255., 74 / 255., 25 / 255., 1.]
+       
+        btn_layout = BoxLayout(orientation='horizontal', spacing=10, padding=[0,0,0,0])
+        btn_layout.add_widget(ok_button)
+        
+        layout_plan = BoxLayout(orientation='vertical', spacing=10, padding=[40,20,40,20])
+        layout_plan.add_widget(img)
+        layout_plan.add_widget(label)
+        layout_plan.add_widget(btn_layout)
+        
+        popup = Popup(title='Error!',
+                      title_color=[0, 0, 0, 1],
+                      title_font= 'Roboto-Bold',
+                      title_size = '20sp',
+                      content=layout_plan,
+                      size_hint=(None, None),
+                      size=(500, 400),
+                      auto_dismiss= False
+                      )
+        
+        popup.separator_color = [230 / 255., 74 / 255., 25 / 255., 1.]
+        popup.separator_height = '4dp'
+        popup.background = './asmcnc/apps/shapeCutter_app/img/popup_background.png'
+        
+        ok_button.bind(on_press=popup.dismiss)
+        ok_button.bind(on_press=restart_polling)  
+
+        popup.open()
 
 class PopupInfo(Widget):
 
