57d00cd5bd9e838cec8abd5da058a3e9bb955472
LettyYeti <lettie.adkins@yetitool.com>
2021-03-23 10:56:09 +0000

    extend error message text box

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -636,7 +636,7 @@ class PopupError(Widget):
         ok_string = self.l.get_bold('Ok')
         
         img = Image(source="./asmcnc/apps/shapeCutter_app/img/error_icon.png", allow_stretch=False)
-        label = Label(size_hint_y=1, text_size=(360, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[0,10], markup = True)
+        label = Label(size_hint_y=1, text_size=(460, None), halign='center', valign='middle', text=description, color=[0,0,0,1], padding=[0,10], markup = True)
         
         ok_button = Button(text=ok_string, markup = True)
         ok_button.background_normal = ''
