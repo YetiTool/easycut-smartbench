33ff298534398983c68419251376daac8573f978
LettyYeti <lettie.adkins@yetitool.com>
2021-03-25 16:29:01 +0000

    alarm details popup

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -1117,8 +1117,8 @@ class PopupLimitSwitchInfo(Widget):
     def __init__(self, screen_manager, alarm_details):
         
         self.sm = screen_manager
-        popup_width = 720
-        label_width = popup_width - 40
+        popup_width = 740
+        label_width = popup_width - 20
 
         description = (
           "If the Z head is far from a limit, there may be a fault. Contact us at https://www.yetitool.com/support." + \
@@ -1127,14 +1127,14 @@ class PopupLimitSwitchInfo(Widget):
           )
 
         img = Image(source="./asmcnc/apps/warranty_app/img/registration-qr-code.png", allow_stretch=False)
-        label = Label(size_hint_y=2, text_size=(label_width, None), markup=True, halign='left', valign='middle', text=description, color=[0,0,0,1], padding=[10,10])
+        label = Label(size_hint_y=1.8, text_size=(label_width, None), markup=True, halign='left', valign='middle', text=description, color=[0,0,0,1], padding=[10,10])
         
         ok_button = Button(text='[b]Ok[/b]', markup = True)
         ok_button.background_normal = ''
         ok_button.background_color = [76 / 255., 175 / 255., 80 / 255., 1.]
 
         
-        btn_layout = BoxLayout(orientation='horizontal', spacing=15, padding=[150,20,150,0])
+        btn_layout = BoxLayout(orientation='horizontal', spacing=15, padding=[150,10,150,0])
         btn_layout.add_widget(ok_button)
         
         layout_plan = BoxLayout(orientation='vertical', spacing=5, padding=[10,5,10,10])
@@ -1148,7 +1148,7 @@ class PopupLimitSwitchInfo(Widget):
                       title_size = '20sp',
                       content=layout_plan,
                       size_hint=(None, None),
-                      size=(popup_width, 420),
+                      size=(popup_width, 440),
                       auto_dismiss= False
                       )
 
