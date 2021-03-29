cdbe5152cf5d2636cbda0651bf8cb1aa69b2869d
LettyYeti <lettie.adkins@yetitool.com>
2021-03-29 12:13:25 +0100

    start reformatting brush popup

--- src/asmcnc/apps/maintenance_app/popup_maintenance.py
+++ src/asmcnc/apps/maintenance_app/popup_maintenance.py
@@ -174,7 +174,7 @@ class PopupBrushInfo(Widget):
 
 
         img_full_brush = Image(source="./asmcnc/apps/maintenance_app/img/brush_long_img.png", allow_stretch=False)
-        label_full_brush_top = Label(text="NEW", size_hint_y=0.1, font_size='12sp', markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
+        label_full_brush_top = Label(text="NEW", text_size=self.size, size_hint_y=0.1, font_size='12sp', markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
         label_full_brush_length = Label(text="16mm", size_hint_y=0.1, font_size='12sp', text_size=(None, None), markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
         label_full_brush_tolerance = Label(text="(+/-0.2mm)", size_hint_y=0.1, font_size='12sp', text_size=(None, None), markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
 
@@ -185,7 +185,7 @@ class PopupBrushInfo(Widget):
         example_full_length.add_widget(label_full_brush_tolerance)
 
         img_med_brush = Image(source="./asmcnc/apps/maintenance_app/img/brush_med_img.png", allow_stretch=False)
-        label_med_brush_top = Label(text="LOW", size_hint_y=0.1, font_size='12sp', markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
+        label_med_brush_top = Label(text="LOW", text_size=self.size, size_hint_y=0.1, font_size='12sp', markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
         label_med_brush_length = Label(text="10mm", size_hint_y=0.1, font_size='12sp', text_size=(None, None), markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
         label_med_brush_tolerance = Label(text="(+/-0.2mm)", size_hint_y=0.1, font_size='12sp', text_size=(None, None), markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
 
@@ -196,7 +196,7 @@ class PopupBrushInfo(Widget):
         example_med_length.add_widget(label_med_brush_tolerance)
 
         img_short_brush = Image(source="./asmcnc/apps/maintenance_app/img/brush_short_img.png", allow_stretch=False)
-        label_short_brush_top = Label(text="SHUT-OFF", size_hint_y=0.1, font_size='12sp', markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
+        label_short_brush_top = Label(text="SHUT-OFF", text_size=self.size, size_hint_y=0.1, font_size='12sp', markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
         label_short_brush_length = Label(text="9.5mm", size_hint_y=0.1, font_size='12sp', text_size=(None, None), markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
         label_short_brush_tolerance = Label(text="(+/-0.2mm)", size_hint_y=0.1, font_size='12sp', text_size=(None, None), markup=True, halign='left', valign='middle', color=[0,0,0,1], padding=[0,0])
 
