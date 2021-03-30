f668f30ea4214f583f89af8d7d11aa32fb802370
LettyYeti <lettie.adkins@yetitool.com>
2021-03-30 18:09:56 +0100

    set up language select in warranty app

--- src/asmcnc/apps/warranty_app/screens/screen_language_select.py
+++ src/asmcnc/apps/warranty_app/screens/screen_language_select.py
@@ -141,4 +141,4 @@ class LanguageSelectScreen(Screen):
 		chosen_lang = self.language_button.text
 		self.l.load_in_new_language(chosen_lang)
 		Clock.schedule_once(lambda dt: self.next_screen(), 0.5)
-		self.loading_label.text = self.l.get_string("Loading...")
+		self.loading_label.text = self.l.get_str("Loading...")
