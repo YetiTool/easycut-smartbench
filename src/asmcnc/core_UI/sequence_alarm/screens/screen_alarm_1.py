1704bd0b17847fd85de64302d5d5708c2b2376ff
LettyYeti <lettie.adkins@yetitool.com>
2021-03-31 16:47:38 +0100

    initial linking and alarm manager tests

--- src/asmcnc/core_UI/sequence_alarm/screens/screen_alarm_1.py
+++ src/asmcnc/core_UI/sequence_alarm/screens/screen_alarm_1.py
@@ -6,19 +6,6 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.clock import Clock
 
-# ALARM_CODES = {
-
-#     "ALARM:1" : "The machine's position was likely lost. Re-homing is highly recommended.",
-#     "ALARM:2" : "The requested motion target exceeds the machine's travel.",
-#     "ALARM:3" : "Machine was reset while in motion and cannot guarantee position. Lost steps are likely. Re-homing is recommended.",
-#     "ALARM:4" : "Probe fail. Probe was not in the expected state before starting probe cycle.",
-#     "ALARM:5" : "Probe fail. Probe did not contact the workpiece within the programmed travel.",
-#     "ALARM:6" : "Homing fail. Reset during active homing cycle.",
-#     "ALARM:7" : "Homing fail. Safety switch was activated during the homing cycle.",
-#     "ALARM:8" : "Homing fail. Cycle failed to clear limit switch when pulling off.",
-#     "ALARM:9" : "Homing fail. Could not find limit switch within search distance.",
-
-# }
 
 # Kivy UI builder:
 Builder.load_string("""
@@ -138,24 +125,24 @@ Builder.load_string("""
 				height: dp(132)
 				width: dp(244.5)
 				padding: [0, 0, 184.5, 0]
-				Button:
-					size_hint: (None,None)
-					height: dp(52)
-					width: dp(60)
-					background_color: hex('#F4433600')
-					center: self.parent.center
-					pos: self.parent.pos
-					on_press: root.go_back()
-					BoxLayout:
-						padding: 0
-						size: self.parent.size
-						pos: self.parent.pos
-						Image:
-							source: "./asmcnc/apps/systemTools_app/img/back_to_menu.png"
-							center_x: self.parent.center_x
-							y: self.parent.y
-							size: self.parent.width, self.parent.height
-							allow_stretch: True
+				# Button:
+				# 	size_hint: (None,None)
+				# 	height: dp(52)
+				# 	width: dp(60)
+				# 	background_color: hex('#F4433600')
+				# 	center: self.parent.center
+				# 	pos: self.parent.pos
+				# 	on_press: root.go_back()
+				# 	BoxLayout:
+				# 		padding: 0
+				# 		size: self.parent.size
+				# 		pos: self.parent.pos
+				# 		Image:
+				# 			source: "./asmcnc/apps/systemTools_app/img/back_to_menu.png"
+				# 			center_x: self.parent.center_x
+				# 			y: self.parent.y
+				# 			size: self.parent.width, self.parent.height
+				# 			allow_stretch: True
 
 			BoxLayout: 
 				size_hint: (None, None)
@@ -182,33 +169,36 @@ Builder.load_string("""
 				height: dp(132)
 				width: dp(244.5)
 				padding: [193.5, 0, 0, 0]
-				Button:
-					size_hint: (None,None)
-					height: dp(60)
-					width: dp(51)
-					background_color: hex('#F4433600')
-					center: self.parent.center
-					pos: self.parent.pos
-					on_press: root.exit_app()
-					BoxLayout:
-						padding: 0
-						size: self.parent.size
-						pos: self.parent.pos
-						Image:
-							source: "./asmcnc/apps/systemTools_app/img/back_to_lobby.png"
-							center_x: self.parent.center_x
-							y: self.parent.y
-							size: self.parent.width, self.parent.height
-							allow_stretch: True 
+				# Button:
+				# 	size_hint: (None,None)
+				# 	height: dp(60)
+				# 	width: dp(51)
+				# 	background_color: hex('#F4433600')
+				# 	center: self.parent.center
+				# 	pos: self.parent.pos
+				# 	on_press: root.exit_app()
+				# 	BoxLayout:
+				# 		padding: 0
+				# 		size: self.parent.size
+				# 		pos: self.parent.pos
+				# 		Image:
+				# 			source: "./asmcnc/apps/systemTools_app/img/back_to_lobby.png"
+				# 			center_x: self.parent.center_x
+				# 			y: self.parent.y
+				# 			size: self.parent.width, self.parent.height
+				# 			allow_stretch: True 
 """)
 
 class AlarmScreen1(Screen):
 
-	# this is the screen's description
-	# alarm_description = StringProperty()
-
-	
 	def __init__(self, **kwargs):
 		super(AlarmScreen1, self).__init__(**kwargs)
-		self.sm=kwargs['screen_manager']
-		self.m=kwargs['machine']
+		self.a=kwargs['alarm_manager']
+
+	def next_screen(self):
+		self.a.sm.current = 'alarm_2'
+
+	def prev_screen(self):
+		self.a.sm.current = 'alarm_1'
+
+# git force
--- src/asmcnc/core_UI/sequence_alarm/screens/screen_alarm_3.py
+++ src/asmcnc/core_UI/sequence_alarm/screens/screen_alarm_3.py
@@ -59,7 +59,7 @@ Builder.load_string("""
 						background_color: hex('#F4433600')
 						center: self.parent.center
 						pos: self.parent.pos
-						on_press: root.go_back()
+						on_press: root.prev_screen()
 						BoxLayout:
 							padding: 0
 							size: self.parent.size
@@ -96,37 +96,40 @@ Builder.load_string("""
 					height: dp(132)
 					width: dp(244.5)
 					padding: [193.5, 0, 0, 0]
-					Button:
-						size_hint: (None,None)
-						height: dp(60)
-						width: dp(51)
-						background_color: hex('#F4433600')
-						center: self.parent.center
-						pos: self.parent.pos
-						on_press: root.exit_app()
-						BoxLayout:
-							padding: 0
-							size: self.parent.size
-							pos: self.parent.pos
-							Image:
-								source: "./asmcnc/apps/systemTools_app/img/back_to_lobby.png"
-								center_x: self.parent.center_x
-								y: self.parent.y
-								size: self.parent.width, self.parent.height
-								allow_stretch: True 
+					# Button:
+					# 	size_hint: (None,None)
+					# 	height: dp(60)
+					# 	width: dp(51)
+					# 	background_color: hex('#F4433600')
+					# 	center: self.parent.center
+					# 	pos: self.parent.pos
+					# 	on_press: root.exit_app()
+					# 	BoxLayout:
+					# 		padding: 0
+					# 		size: self.parent.size
+					# 		pos: self.parent.pos
+					# 		Image:
+					# 			source: "./asmcnc/apps/systemTools_app/img/back_to_lobby.png"
+					# 			center_x: self.parent.center_x
+					# 			y: self.parent.y
+					# 			size: self.parent.width, self.parent.height
+					# 			allow_stretch: True 
 """)
 
 class AlarmScreen3(Screen):
 
-	# this is the screen's description
-	# alarm_description = StringProperty()
-
-	
 	def __init__(self, **kwargs):
 		super(AlarmScreen3, self).__init__(**kwargs)
-		self.sm=kwargs['screen_manager']
-		self.m=kwargs['machine']
+		self.a=kwargs['alarm_manager']
 
-		self.status_bar_widget = widget_status_bar.StatusBar(screen_manager=self.sm, machine=self.m)
+		self.status_bar_widget = widget_status_bar.StatusBar(screen_manager=self.a.sm, machine=self.a.m)
 		self.status_container.add_widget(self.status_bar_widget)
 		self.status_bar_widget.cheeky_color = '#1976d2'
+
+	def next_screen(self):
+		self.a.sm.current = 'alarm_4'
+
+	def prev_screen(self):
+		self.a.sm.current = 'alarm_2'
+
+# git force
