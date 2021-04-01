200f98da0e9ed5229ce3dc7015b006271e16a835
LettyYeti <lettie.adkins@yetitool.com>
2021-04-01 11:39:05 +0100

    alarm report

--- src/asmcnc/core_UI/sequence_alarm/alarm_manager.py
+++ src/asmcnc/core_UI/sequence_alarm/alarm_manager.py
@@ -244,12 +244,12 @@ class AlarmSequenceManager(object):
 
 	def write_report_to_file(self):
 
-		report_file_path = "/home/pi/alarm_report_" + str(datetime.datetime.now()) + ".txt"
+		report_file_path = "/home/pi/alarm_report_" + str(datetime.date.today()) + ".txt"
 
 		# try:
-		file = open(report_file_path, 'w+')
-		file.write(self.report_string)
-		file.close()
+		with open(report_file_path, 'w+') as file:
+			file.write(self.report_string)
+
 		os.system("sudo cp --no-preserve=mode,ownership " + report_file_path + " /media/usb/ && rm " + report_file_path)
 		print("Alarm report written to file")
 		return True
