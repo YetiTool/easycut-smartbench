a6d353959cfd5f59064d65cd4456f2f770556457
LettyYeti <lettie.adkins@yetitool.com>
2021-04-08 14:24:07 +0100

    debugging m-state screen

--- src/asmcnc/apps/shapeCutter_app/screens/screen_shapeCutter_23.py
+++ src/asmcnc/apps/shapeCutter_app/screens/screen_shapeCutter_23.py
@@ -536,7 +536,7 @@ class ShapeCutter23ScreenClass(Screen):
         # "[b]Spindle Speed:[/b] Rotational speed of the tool.\n\n" \
         # "For more help please visit: https://www.yetitool.com/support/knowledge-\nbase/hardware-smartbench-feeds-speeds"
         message = ", loading feeds and speeds look-up table..."
-        popup_info.PopupWait(self.shapecutter_sm, self.l, message)
+        popup_info.PopupWait(self.shapecutter_sm, message)
         # popup_info.PopupInfo(self.shapecutter_sm, info)
         Clock.schedule_once(lambda dt: popup_info.PopupFeedsAndSpeedsLookupTable(self.shapecutter_sm), 1.5)
 
