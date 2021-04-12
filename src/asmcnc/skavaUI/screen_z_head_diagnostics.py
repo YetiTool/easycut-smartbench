5e812acfc7ac11d41d9347ae89c521f4cb552a5a
LettyYeti <lettie.adkins@yetitool.com>
2021-04-12 14:50:32 +0100

    put in infrastructure to read temp and voltage from poll

--- src/asmcnc/skavaUI/screen_z_head_diagnostics.py
+++ src/asmcnc/skavaUI/screen_z_head_diagnostics.py
@@ -579,40 +579,40 @@ class ZHeadDiagnosticsScreen(Screen):
             Clock.unschedule(self.poll_for_temps_power)
             return
 
-        if (self.m.s.motor_driver_temp > 10) and (self.m.s.motor_driver_temp < 50):
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
-        else:
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
-            Clock.unschedule(self.poll_for_temps_power)
-            return
+        # if (self.m.s.motor_driver_temp > 10) and (self.m.s.motor_driver_temp < 50):
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
+        # else:
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
+        #     Clock.unschedule(self.poll_for_temps_power)
+        #     return
 
-        if (self.m.s.microcontroller_mV > 4800) and (self.m.s.microcontroller_mV < 5200):
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
-        else:
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
-            Clock.unschedule(self.poll_for_temps_power)
-            return
+        # if (self.m.s.microcontroller_mV > 4800) and (self.m.s.microcontroller_mV < 5200):
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
+        # else:
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
+        #     Clock.unschedule(self.poll_for_temps_power)
+        #     return
 
-        if (self.m.s.LED_mV > 4800) and (self.m.s.LED_mV < 5200):
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
-        else:
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
-            Clock.unschedule(self.poll_for_temps_power)
-            return
+        # if (self.m.s.LED_mV > 4800) and (self.m.s.LED_mV < 5200):
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
+        # else:
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
+        #     Clock.unschedule(self.poll_for_temps_power)
+        #     return
 
-        if (self.m.s.PSU_mV > 22000) and (self.m.s.PSU_mV < 26000):
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
-        else:
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
-            Clock.unschedule(self.poll_for_temps_power)
-            return
+        # if (self.m.s.PSU_mV > 22000) and (self.m.s.PSU_mV < 26000):
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
+        # else:
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
+        #     Clock.unschedule(self.poll_for_temps_power)
+        #     return
 
-        if self.ac_loss == True:
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
-        else:
-            self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
-            Clock.unschedule(self.poll_for_temps_power)
-            return
+        # if self.ac_loss == True:
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
+        # else:
+        #     self.temp_voltage_power_check.source = "./asmcnc/skavaUI/img/template_cancel.png"
+        #     Clock.unschedule(self.poll_for_temps_power)
+        #     return
 
     def cycle_limit_switch(self):
         if self.m.s.limit_z:
