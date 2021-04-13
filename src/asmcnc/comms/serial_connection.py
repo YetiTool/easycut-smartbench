6242fe757ecb5039e0b612bf81d84a6aeea0d098
LettyYeti <lettie.adkins@yetitool.com>
2021-04-13 10:04:22 +0100

    use alternate port

--- src/asmcnc/comms/serial_connection.py
+++ src/asmcnc/comms/serial_connection.py
@@ -108,7 +108,7 @@ class SerialConnection(object):
                     # EITHER: USB Comms hardware
                     # if (line[:6] == 'ttyUSB' or line[:6] == 'ttyACM'): # look for prefix of known success (covers both Mega and Uno)
                     # OR: UART Comms hardware
-                    if (line[:4] == 'ttyS' or line[:6] == 'ttyAMA'): # look for...   
+                    if line[:6] == 'ttyAMA': # look for...   
                         # When platform is updated, this needs to be moved across to the AMA0 port :)
                         devicePort = line # take whole line (includes suffix address e.g. ttyACM0
                         self.s = serial.Serial('/dev/' + str(devicePort), BAUD_RATE, timeout = 6, writeTimeout = 20) # assign
