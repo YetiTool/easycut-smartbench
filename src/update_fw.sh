27a1af61547aad5640c6205ada2e4a41fec8f4bd
LettyYeti <lettie.adkins@yetitool.com>
2021-04-13 10:56:54 +0100

    trial fw check

--- src/update_fw.sh
+++ src/update_fw.sh
@@ -5,5 +5,5 @@ sleep 5
 # this uses the default bt port - needs changing in PL before can be automated. 
 # also need to create a standard location to find the hex file - currently this is just a dummy.
 # might need to rewrite this script each time for new hex file. 
-grbl_file=/media/usb/GRBL*.hex
-avrdude -patmega2560 -cwiring -P/dev/ttyAMA0 -b115200 -D -Uflash:w:$grbl_file:i
+grbl_file=(/media/usb/GRBL*.hex)
+"avrdude -patmega2560 -cwiring -P/dev/ttyAMA0 -b115200 -D -Uflash:w:" + $grbl_file + ":i"
