51cbf09d4a71d1e1917f0a4138ac338e997ec52b
LettyYeti <lettie.adkins@yetitool.com>
2021-04-14 14:48:53 +0100

    try to do flash with error return and without rebooting

--- src/asmcnc/production/screen_z_head_diagnostics.py
+++ src/asmcnc/production/screen_z_head_diagnostics.py
@@ -792,7 +792,7 @@ class ZHeadDiagnosticsScreen(Screen):
 
             if exit_code == 0: 
                 did_fw_update_succeed = "Success!"
-                Clock.schedule_once(lambda dt: self.do_reboot(), 5)
+                Clock.schedule_once(lambda dt: self.do_reboot(), 10)
 
             else: 
                 did_fw_update_succeed = "Update failed. Reboot to reconnect to Z head." + "\n" + str(stdout)
