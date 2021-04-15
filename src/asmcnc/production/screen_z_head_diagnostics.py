24452f19dfb9d0cc9057f516055c86393fa960d4
LettyYeti <lettie.adkins@yetitool.com>
2021-04-15 15:28:54 +0100

    add status bar for info

--- src/asmcnc/production/screen_z_head_diagnostics.py
+++ src/asmcnc/production/screen_z_head_diagnostics.py
@@ -534,6 +534,7 @@ class ZHeadDiagnosticsScreen(Screen):
 
     def home(self):
         self.m.is_machine_completed_the_initial_squaring_decision = True
+        self.m.is_squaring_XY_needed_after_homing = False
         self.m.request_homing_procedure('z_head_diagnostics','z_head_diagnostics', False)
 
     def resume_from_alarm(self):
