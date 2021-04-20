9a5a844f6a9b59b8d6e18bfa24d6d1318bab514b
LettyYeti <lettie.adkins@yetitool.com>
2021-04-20 16:15:34 +0100

    try to make yetitool owner of doc

--- src/asmcnc/production/process_linear_encoder_read.py
+++ src/asmcnc/production/process_linear_encoder_read.py
@@ -686,30 +686,30 @@ class ProcessLinearEncoderScreen(Screen):
         self.active_folder_id = folder.get('id')
         log('Created new folder: ' + str(folder.get('id')))
 
-        # # CHANGE FOLDER OWVER
-        # param_perm = {}
-        # param_perm['value'] = 'ed.sells@yetitool.com'
-        # # param_perm['type'] = 'user'
-        # param_perm['role'] = 'owner'
+        # CHANGE FOLDER OWVER
+        param_perm = {}
+        param_perm['value'] = 'engineering.service.accounts.group@yetitool.com'
+        # param_perm['type'] = 'user'
+        param_perm['role'] = 'owner'
 
-        # perm_id = "08371608215019286311"
+        perm_id = "08371608215019286311"
 
-        # return self.drive_service.permissions().update(fileId=self.active_folder_id,
-        #                      permissionId=perm_id,
-        #                      body=param_perm,
-        #                      transferOwnership=True).execute()
+        return self.drive_service.permissions().update(fileId=self.active_folder_id,
+                             permissionId=perm_id,
+                             body=param_perm,
+                             transferOwnership=True).execute()
 
 
-        # # Remove the API service bot's default parents, which will hopefully enable access
-        # folder = self.drive_service.files().get(fileId=self.active_folder_id,
-        #                                     fields='parents').execute()
+        # Remove the API service bot's default parents, which will hopefully enable access
+        folder = self.drive_service.files().get(fileId=self.active_folder_id,
+                                            fields='parents').execute()
 
-        # previous_parents = ",".join(folder.get('parents'))
-        # # Move the file to the new folder
-        # folder = self.drive_service.files().update(fileId=self.active_folder_id,
-        #                                     addParents=self.live_measurements_id,
-        #                                     removeParents=previous_parents,
-        #                                     fields='id, parents').execute()
+        previous_parents = ",".join(folder.get('parents'))
+        # Move the file to the new folder
+        folder = self.drive_service.files().update(fileId=self.active_folder_id,
+                                            addParents=self.live_measurements_id,
+                                            removeParents=previous_parents,
+                                            fields='id, parents').execute()
 
 
     def create_new_document(self):
