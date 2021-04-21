3d4cc31a118d37dc2f861929779345b506db38d6
LettyYeti <lettie.adkins@yetitool.com>
2021-04-21 15:41:40 +0100

    move the shutdown button

--- src/asmcnc/skavaUI/screen_lobby.py
+++ src/asmcnc/skavaUI/screen_lobby.py
@@ -47,23 +47,6 @@ Builder.load_string("""
             padding: [10, 10, 734, 0]
             orientation: 'horizontal'
 
-            # Button:
-            #     id: shutdown_button
-            #     size_hint_y: 1
-            #     background_color: hex('#FFFFFF00')
-            #     on_press: root.shutdown_console()
-
-            #     BoxLayout:
-            #         size: self.parent.size
-            #         pos: self.parent.pos
-            #         Image:
-            #             id: image_select
-            #             source: "./asmcnc/skavaUI/img/shutdown.png"
-            #             center_x: self.parent.center_x
-            #             y: self.parent.y
-            #             size: self.parent.width, self.parent.height
-            #             allow_stretch: True 
-
         Carousel:
             size_hint_y: 270
             id: carousel
@@ -313,38 +296,6 @@ Builder.load_string("""
                         font_size: '25sp'
                         text: 'System Tools'
                         markup: True
-
-                # BoxLayout:
-                #     orientation: 'vertical'
-                #     size_hint_x: 1
-                #     spacing: 20
-    
-                #     Button:
-                #         size_hint_y: 8
-                #         id: load_button
-                #         disabled: False
-                #         background_color: hex('#FFFFFF00')
-                #         on_release: 
-                #             self.background_color = hex('#FFFFFF00')
-                #         on_press:
-                #             root.developer_app()
-                #             self.background_color = hex('#FFFFFF00')
-                #         BoxLayout:
-                #             padding: 0
-                #             size: self.parent.size
-                #             pos: self.parent.pos
-                #             Image:
-                #                 id: image_select
-                #                 source: "./asmcnc/skavaUI/img/lobby_developer.png"
-                #                 center_x: self.parent.center_x
-                #                 center_y: self.parent.center_y
-                #                 size: self.parent.width, self.parent.height
-                #                 allow_stretch: True 
-                #     Label:
-                #         size_hint_y: 1
-                #         font_size: '25sp'
-                #         text: 'DUMMY'
-                #         markup: True
                        
         BoxLayout:
             size_hint_y: 6
@@ -362,8 +313,6 @@ Builder.load_string("""
                 size_hint_x: None
                 width: 720
                 height: self.parent.height
-                # size: self.parent.size
-                # pos: self.parent.pos
                 padding: [80, 40, 0, 40]
                 orientation: 'horizontal'
                 
@@ -388,26 +337,6 @@ Builder.load_string("""
                             allow_stretch: True 
                 Label:
                     size_hint_y: 0.8
-
-                # Button:
-                #     disabled: False
-                #     size_hint_y: 1
-                #     background_color: hex('#FFFFFF00')
-                #     on_release: 
-                #         self.background_color = hex('#FFFFFF00')
-                #     on_press:
-                #         root.help_popup()
-                #         self.background_color = hex('#FFFFFF00')
-                #     BoxLayout:
-                #         size: self.parent.size
-                #         pos: self.parent.pos
-                #         Image:
-                #             id: image_cancel
-                #             source: "./asmcnc/skavaUI/img/lobby_help.png"
-                #             center_x: self.parent.center_x
-                #             y: self.parent.y
-                #             size: self.parent.width, self.parent.height
-                #             allow_stretch: True 
 
                 Button:
                     id: shutdown_button
