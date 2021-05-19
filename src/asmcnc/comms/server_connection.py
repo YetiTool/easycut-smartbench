7abb22a5d9c7e8fe328fb79f2d5ba8b55c262dc5
LettyYeti <lettie.adkins@yetitool.com>
2021-05-19 15:05:45 +0100

    test server connect draft

--- src/asmcnc/comms/server_connection.py
+++ src/asmcnc/comms/server_connection.py
@@ -2,7 +2,7 @@ import socket
 import sys
 import threading
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
+HOST = ''  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 class ServerConnection(object):
