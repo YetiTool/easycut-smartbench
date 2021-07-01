5e3318d8a33fcab629f7a6288fc593e24b5b0021
LettyYeti <lettie.adkins@yetitool.com>
2021-07-01 14:20:49 +0100

    run sb values folder on mac and pc systems

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -106,9 +106,9 @@ class RouterMachine(object):
         self.s.establish_connection(win_serial_port)
 
         # initialise sb_value files if they don't already exist (to record persistent maintenance values)
-        if sys.platform != "win32" and sys.platform != "darwin":
-            self.check_presence_of_sb_values_files()
-            self.get_persistent_values()
+        # if sys.platform != "win32" and sys.platform != "darwin":
+        self.check_presence_of_sb_values_files()
+        self.get_persistent_values()
 
 # PERSISTENT MACHINE VALUES
     def check_presence_of_sb_values_files(self):
