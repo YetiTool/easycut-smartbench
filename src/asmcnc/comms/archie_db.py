12a0c8b221ef645cfce66f1afe5297afaa39ee7b
LettyYeti <lettie.adkins@yetitool.com>
2021-07-30 15:02:32 +0100

    testing branch

--- src/asmcnc/comms/archie_db.py
+++ src/asmcnc/comms/archie_db.py
@@ -28,19 +28,19 @@ class SQLRabbit:
         z_lube_limit_hrs = self.m.time_to_remind_user_to_lube_z_seconds/3600
         z_lube_used_hrs = self.m.time_since_z_head_lubricated_seconds/3600
         z_lube_hrs_left = round(z_lube_limit_hrs - z_lube_used_hrs, 2)
-        z_lube_percent_used = round((z_lube_used_hrs/z_lube_limit_hrs)*100, 2) # This was percentage left, not percentage used
+        z_lube_percent_left = round((z_lube_hrs_left/z_lube_limit_hrs)*100, 2) # This was percentage left, not percentage used
     
         # spindle brush
         spindle_brush_limit_hrs = self.m.spindle_brush_lifetime_seconds/3600
         spindle_brush_used_hrs = self.m.spindle_brush_use_seconds/3600
         spindle_brush_hrs_left = round(spindle_brush_limit_hrs - spindle_brush_used_hrs, 2)
-        spindle_brush_percent_used = round((spindle_brush_used_hrs/spindle_brush_limit_hrs)*100, 2) # This was percentage left, not percentage used
+        spindle_brush_percent_left = round((spindle_brush_hrs_left/spindle_brush_limit_hrs)*100, 2) # This was percentage left, not percentage used
     
         # calibration
         calibration_limit_hrs = self.m.time_to_remind_user_to_calibrate_seconds/3600
         calibration_used_hrs = self.m.time_since_calibration_seconds/3600
         calibration_hrs_left = round(calibration_limit_hrs - calibration_used_hrs, 2)
-        calibration_percent_used = round((calibration_used_hrs/calibration_limit_hrs)*100, 2) # This was percentage left, not percentage used
+        calibration_percent_left = round((calibration_hrs_left/calibration_limit_hrs)*100, 2) # This was percentage left, not percentage used
         
         data = [
             {
@@ -52,23 +52,22 @@ class SQLRabbit:
                 },
                 "statuses": {
                     "status": self.m.s.m_state,
-                    "file_name": self.sm.get_screen('go').job_name_only or '',
-                    "overload_peak": float(self.sm.get_screen('go').overload_peak) or '',
-    
-                    "job_time": self.sm.get_screen('go').time_taken_seconds or '',
-                    "job_percent": self.sm.get_screen('go').percent_thru_job or 0.0,
-                    "job_name": self.sm.get_screen('go').job_name_only or '',
-    
-                    "z_lube_%_thru": z_lube_percent_used,
+
+                    "z_lube_%_left": z_lube_percent_left,
                     "z_lube_hrs_before_next": z_lube_hrs_left,
     
-                    "spindle_brush_%_thru": spindle_brush_percent_used,
+                    "spindle_brush_%_left": spindle_brush_percent_left,
                     "spindle_brush_hrs_before_next": spindle_brush_hrs_left,
     
-                    "calibration_%_thru": calibration_percent_used,
+                    "calibration_%_left": calibration_percent_left,
                     "calibration_hrs_before_next": calibration_hrs_left,
-                    
-                    "gcode_line": self.m.s.g_count or 0
+
+                    "file_name": self.sm.get_screen('go').job_name_only or '',
+                    "job_time": self.sm.get_screen('go').time_taken_seconds or '',
+                    "gcode_line": self.m.s.g_count or 0,
+                    "job_percent": self.sm.get_screen('go').percent_thru_job or 0.0,
+                    "overload_peak": float(self.sm.get_screen('go').overload_peak) or ''
+
                 },
                 "events": {
                     "placeholder": ""
@@ -89,11 +88,11 @@ class SQLRabbit:
         #             },
         #             "statuses": {
         #                 "status": self.m.s.m_state,
-        #                 "z_lube_%_thru": z_lube_percent_used,
+        #                 "z_lube_%_left": z_lube_percent_left,
         #                 "z_lube_hrs_before_next": z_lube_hrs_left,
-        #                 "spindle_brush_%_thru": spindle_brush_percent_used,
+        #                 "spindle_brush_%_left": spindle_brush_percent_left,
         #                 "spindle_brush_hrs_before_next": spindle_brush_hrs_left,
-        #                 "calibration_%_thru": calibration_percent_used,
+        #                 "calibration_%_left": calibration_percent_left,
         #                 "calibration_hrs_before_next": calibration_hrs_left,
         #                 "gcode_line": self.m.s.g_count
         #             },
