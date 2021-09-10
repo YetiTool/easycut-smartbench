931dd58673bd03bbfca15b665ead5737b015d815
Hugh Harford <dev@poscoconsulting.com>
2021-09-10 14:58:38 +0100

    Plan for unit_test_boundary_walk
    Actual test to follow, of course.
    Following TDD approach:
    1. Spec and write test (that fails)
    2. Code to get test to pass

--- test/unit_test_start/unit_test_boundary_walk.py
+++ test/unit_test_start/unit_test_boundary_walk.py
@@ -12,6 +12,27 @@ NOTES:
 '''
 import unittest
 
+##########################################################
+
+#  PLAN THE TEST:
+#  
+#  >>> GIVEN:
+#    1    GIVEN a gcode file
+#    2    GIVEN a working SB & console with software loaded
+#  
+
+#  >>> WHEN:
+#    *    WHEN the boundary_walk check is undertaken (likely to be on the kivy preview screen)
+#  
+
+#  >>> THEN:
+#    *    THEN an non-zero array BOUNDARY_array is returned that provides the boundary co-ordinates
+#    *    THEN the BOUNDARY_array is a complete boundary (polygon), so the start and end point are the same
+#    *    THEN no part of the gcode file will exceed the area described by the BOUNDARY_array
+
+
+##########################################################
+
 class UnitTestBoundaryWalkIsComplete(unittest.TestCase):
     
     def testBoundaryIsAnArray(self):  ## test method names begin 'test*'
