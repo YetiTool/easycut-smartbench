5b3b4c9abd67818dc978677405f0caa160cbf668
LettyYeti <lettie.adkins@yetitool.com>
2021-09-15 12:32:59 +0100

    testing logic from release notes screen

--- src/asmcnc/core_UI/data_and_wifi/data_consent_manager.py
+++ src/asmcnc/core_UI/data_and_wifi/data_consent_manager.py
@@ -65,9 +65,11 @@ class DataConsentManager(object):
 
 	def update_seen(self):
 		user_has_seen_privacy_notice = (os.popen('grep "user_has_seen_privacy_notice" /home/pi/easycut-smartbench/src/config.txt').read())
+
+		print(user_has_seen_privacy_notice)
 		
 		if not user_has_seen_privacy_notice:
 			os.system("sudo sed -i -e '$auser_has_seen_privacy_notice=True' /home/pi/easycut-smartbench/src/config.txt")
 
-		elif user_has_seen_privacy_notice.endswith('False'):
+		elif 'False' in user_has_seen_privacy_notice:
 			os.system('sudo sed -i "s/user_has_seen_privacy_notice=False/user_has_seen_privacy_notice=True/" /home/pi/easycut-smartbench/src/config.txt')
