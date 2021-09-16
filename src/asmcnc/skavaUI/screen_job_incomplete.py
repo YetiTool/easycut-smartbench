e07cca748ba5eaa3df88ebd3c244d6e2dbcbb2e3
LettyYeti <lettie.adkins@yetitool.com>
2021-09-16 17:16:07 +0100

    test alarms and errors

--- src/asmcnc/skavaUI/screen_job_incomplete.py
+++ src/asmcnc/skavaUI/screen_job_incomplete.py
@@ -171,9 +171,9 @@ Builder.load_string("""
                             size_hint_y: None
                             height: self.parent.height
                             opacity: 1
-                            on_press: root.open_event_details_text_input()
+                            # on_press: root.open_event_details_text_input()
                             focus_next: event_details_input
-                            disabled: True
+                            # disabled: True
 
                         TextInput:
                             id: event_details_input
@@ -380,13 +380,13 @@ class JobIncompleteScreen(Screen):
             self.event_details_label.color = [25 / 255., 118 / 255., 210 / 255., 1.]
             self.job_cancelled_label.text = self.l.get_str("Job cancelled by the user.")
             self.event_details_label.text = "<" + self.l.get_str("add your reason for cancellation here") + ">"
-            self.event_details_label.disabled = False
+            self.event_details_label.on_press = self.open_event_details_text_input()
 
         else:
             self.event_details_label.color = [51 / 255., 51 / 255., 51 / 255., 1.]
             self.job_cancelled_label.text = self.l.get_str("Job cancelled due to an event.").replace(self.l.get_str("event"), self.l.get_str(self.event_type))
             lost_position_message = self.l.get_str("Recover any parts from this job before rehoming and starting a new job.")
-            self.event_details_label.disabled = True
+            self.event_details_label.on_press = pass
 
             if 'alarm' in self.event_type:
                 self.event_details_label.text = (
