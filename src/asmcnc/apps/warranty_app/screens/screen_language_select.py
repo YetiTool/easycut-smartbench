09d052567bf0717a5f5d2fb924ef7485980a9834
LettyYeti <lettie.adkins@yetitool.com>
2021-09-22 12:11:38 +0100

    reduce to approved languages

--- src/asmcnc/apps/warranty_app/screens/screen_language_select.py
+++ src/asmcnc/apps/warranty_app/screens/screen_language_select.py
@@ -42,20 +42,20 @@ Builder.load_string("""
 	row_1_col_1 : row_1_col_1
 	row_1_col_2 : row_1_col_2
 	row_1_col_3 : row_1_col_3
-	row_2_col_1 : row_2_col_1
-	row_2_col_2 : row_2_col_2
-	row_2_col_3 : row_2_col_3
-	row_3_col_1 : row_3_col_1
+	# row_2_col_1 : row_2_col_1
+	# row_2_col_2 : row_2_col_2
+	# row_2_col_3 : row_2_col_3
+	# row_3_col_1 : row_3_col_1
 	# row_3_col_2 : row_3_col_2
 	# row_3_col_3 : row_3_col_3
 
 	row_1_col_1_image : row_1_col_1_image
 	row_1_col_2_image : row_1_col_2_image
 	row_1_col_3_image : row_1_col_3_image
-	row_2_col_1_image : row_2_col_1_image
-	row_2_col_2_image : row_2_col_2_image
-	row_2_col_3_image : row_2_col_3_image
-	row_3_col_1_image : row_3_col_1_image
+	# row_2_col_1_image : row_2_col_1_image
+	# row_2_col_2_image : row_2_col_2_image
+	# row_2_col_3_image : row_2_col_3_image
+	# row_3_col_1_image : row_3_col_1_image
 	# row_3_col_2_image : row_3_col_2_image
 	# row_3_col_3_image : row_3_col_3_image
 
@@ -215,85 +215,97 @@ Builder.load_string("""
 						color: hex('#333333ff')
 
 
-					# ROW 2
+					# # ROW 2
 
-					CheckBox: 
-						group: "language_radio_buttons" 
-						on_press: root.select_language(self, row_2_col_1)
-						color: hex('#333333ff')
+					# CheckBox: 
+					# 	group: "language_radio_buttons" 
+					# 	on_press: root.select_language(self, row_2_col_1)
+					# 	color: hex('#333333ff')
 
-	                Image: 
-	                	id: row_2_col_1_image
-	                	allow_stretch: True
+	    #             Image: 
+	    #             	id: row_2_col_1_image
+	    #             	allow_stretch: True
 
-	                Label: 
-	                	id: row_2_col_1
-	                	valign: "middle"
-						font_size: '20sp'
-						text_size: self.size
-						markup: True
-						halign: "left"
-						color: hex('#333333ff')
-
-
-					CheckBox: 
-						group: "language_radio_buttons" 
-						on_press: root.select_language(self, row_2_col_2)
-						color: hex('#333333ff')
-
-	                Image: 
-	                	id: row_2_col_2_image
-	                	allow_stretch: True
-
-	                Label: 
-	                	id: row_2_col_2
-	                	valign: "middle"
-						font_size: '20sp'
-						text_size: self.size
-						markup: True
-						halign: "left"
-						color: hex('#333333ff')
+	    #             Label: 
+	    #             	id: row_2_col_1
+	    #             	valign: "middle"
+					# 	font_size: '20sp'
+					# 	text_size: self.size
+					# 	markup: True
+					# 	halign: "left"
+					# 	color: hex('#333333ff')
 
 
-					CheckBox: 
-						group: "language_radio_buttons" 
-						on_press: root.select_language(self, row_2_col_3)
-						color: hex('#333333ff')
+					# CheckBox: 
+					# 	group: "language_radio_buttons" 
+					# 	on_press: root.select_language(self, row_2_col_2)
+					# 	color: hex('#333333ff')
 
-	                Image: 
-	                	id: row_2_col_3_image
-	                	allow_stretch: True
+	    #             Image: 
+	    #             	id: row_2_col_2_image
+	    #             	allow_stretch: True
 
-	                Label: 
-	                	id: row_2_col_3
-	                	valign: "middle"
-						font_size: '20sp'
-						text_size: self.size
-						markup: True
-						halign: "left"
-						color: hex('#333333ff')
+	    #             Label: 
+	    #             	id: row_2_col_2
+	    #             	valign: "middle"
+					# 	font_size: '20sp'
+					# 	text_size: self.size
+					# 	markup: True
+					# 	halign: "left"
+					# 	color: hex('#333333ff')
 
 
-					# ROW 3
+					# CheckBox: 
+					# 	group: "language_radio_buttons" 
+					# 	on_press: root.select_language(self, row_2_col_3)
+					# 	color: hex('#333333ff')
 
-					CheckBox: 
-						group: "language_radio_buttons" 
-						on_press: root.select_language(self, row_3_col_1)
-						color: hex('#333333ff')
+	    #             Image: 
+	    #             	id: row_2_col_3_image
+	    #             	allow_stretch: True
 
-	                Image: 
-	                	id: row_3_col_1_image
-	                	allow_stretch: True
+	    #             Label: 
+	    #             	id: row_2_col_3
+	    #             	valign: "middle"
+					# 	font_size: '20sp'
+					# 	text_size: self.size
+					# 	markup: True
+					# 	halign: "left"
+					# 	color: hex('#333333ff')
 
-	                Label: 
-	                	id: row_3_col_1
-	                	valign: "middle"
-						font_size: '20sp'
-						text_size: self.size
-						markup: True
-						halign: "left"
-						color: hex('#333333ff')
 
+					# # ROW 3
+
+					# CheckBox: 
+					# 	group: "language_radio_buttons" 
+					# 	on_press: root.select_language(self, row_3_col_1)
+					# 	color: hex('#333333ff')
+
+	    #             Image: 
+	    #             	id: row_3_col_1_image
+	    #             	allow_stretch: True
+
+	    #             Label: 
+	    #             	id: row_3_col_1
+	    #             	valign: "middle"
+					# 	font_size: '20sp'
+					# 	text_size: self.size
+					# 	markup: True
+					# 	halign: "left"
+					# 	color: hex('#333333ff')
+
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
+					BoxLayout: 
 					BoxLayout: 
 					BoxLayout: 
 					BoxLayout: 
@@ -419,22 +431,22 @@ class LanguageSelectScreen(Screen):
 		self.status_bar_widget.cheeky_color = '#1976d2'
 
 		self.row_1_col_1.text = self.l.supported_languages[0]
-		self.row_1_col_2.text = self.l.supported_languages[1]
-		self.row_1_col_3.text = self.l.supported_languages[2]
-		self.row_2_col_1.text = self.l.supported_languages[3]
-		self.row_2_col_2.text = self.l.supported_languages[4]
-		self.row_2_col_3.text = self.l.supported_languages[5]
-		self.row_3_col_1.text = self.l.supported_languages[6]
+		self.row_1_col_2.text = self.l.supported_languages[3]
+		self.row_1_col_3.text = self.l.supported_languages[4]
+		# self.row_2_col_1.text = self.l.supported_languages[3]
+		# self.row_2_col_2.text = self.l.supported_languages[4]
+		# self.row_2_col_3.text = self.l.supported_languages[5]
+		# self.row_3_col_1.text = self.l.supported_languages[6]
 		# self.row_3_col_2.text = self.l.supported_languages[7]
 		# self.row_3_col_3.text = self.l.supported_languages[8]
 
 		self.row_1_col_1_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_1_col_1.text + ".png"
 		self.row_1_col_2_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_1_col_2.text + ".png"
 		self.row_1_col_3_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_1_col_3.text + ".png"
-		self.row_2_col_1_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_2_col_1.text + ".png"
-		self.row_2_col_2_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_2_col_2.text + ".png"
-		self.row_2_col_3_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_2_col_3.text + ".png"
-		self.row_3_col_1_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_3_col_1.text + ".png"
+		# self.row_2_col_1_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_2_col_1.text + ".png"
+		# self.row_2_col_2_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_2_col_2.text + ".png"
+		# self.row_2_col_3_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_2_col_3.text + ".png"
+		# self.row_3_col_1_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_3_col_1.text + ".png"
 		# self.row_3_col_2_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_3_col_2.text + ".png"
 		# self.row_3_col_3_image.source = "./asmcnc/apps/warranty_app/img/flags/" + self.row_3_col_3.text + ".png"
 
