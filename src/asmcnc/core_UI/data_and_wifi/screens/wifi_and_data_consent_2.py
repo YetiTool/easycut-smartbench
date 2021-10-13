a72ed7751be0228a2fa5ec9687602b0931dc8f9b
LettyYeti <lettie.adkins@yetitool.com>
2021-10-13 12:03:28 +0100

    test extra line on wifi screen 2

--- src/asmcnc/core_UI/data_and_wifi/screens/wifi_and_data_consent_2.py
+++ src/asmcnc/core_UI/data_and_wifi/screens/wifi_and_data_consent_2.py
@@ -157,7 +157,7 @@ class WiFiAndDataConsentScreen2(Screen):
 			"[b]•[/b] " + self.l.get_str("Remotely manage and monitor SmartBench (e.g. with SmartManager)") + \
 			"\n\n" + \
 			self.l.get_str("You can come back to this data policy at any time, and enable or disable Wi-Fi.") + \
-			"\nTest extra line here"
+			"\nTest extra line here and make it really really long so can fully see it"
 
 		)
 		self.next_button.text = self.l.get_str("Next") + "..."
