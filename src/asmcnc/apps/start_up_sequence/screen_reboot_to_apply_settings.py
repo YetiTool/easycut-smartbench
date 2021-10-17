3560650a844e19bf31cd956135137582d6b20a95
LettyYeti <lettie.adkins@yetitool.com>
2021-10-17 20:56:59 +0100

    sort out warranty app

--- src/asmcnc/apps/start_up_sequence/screen_reboot_to_apply_settings.py
+++ src/asmcnc/apps/start_up_sequence/screen_reboot_to_apply_settings.py
@@ -26,77 +26,110 @@ Builder.load_string("""
 				size: self.size
 				pos: self.pos
 		BoxLayout:
-			id: status_container 
-			size_hint_y: 0.08
-		BoxLayout:
-			size_hint_y: 0.92
-			orientation: 'vertical'
+			padding: 0
+			spacing: 0
+			orientation: "vertical"
+
+			# HEADER
 			BoxLayout:
-				orientation: 'vertical'
-				width: dp(800)
-				height: dp(200)
-				padding: [dp(20), 0]
-				size_hint: (None,None)
+				padding: 0
+				spacing: 0
+				canvas:
+					Color:
+						rgba: hex('#1976d2ff')
+					Rectangle:
+						pos: self.pos
+						size: self.size
 				Label:
-					id: success_label
-					font_size: '30sp'
-					text_size: self.size
-					valign: 'top'
-					halign: 'center'
-					markup: 'true'
-					bold: True
-					color: hex('#333333ff')
+					id: title_label
+					size_hint: (None,None)
+					height: dp(60)
+					width: dp(800)
+					text: "SmartBench Warranty Registration"
+					color: hex('#f9f9f9ff')
+					# color: hex('#333333ff') #grey
+					font_size: dp(30)
+					halign: "center"
+					valign: "bottom"
+					markup: True
+				   
+			# BODY
 			BoxLayout:
-				orientation: 'vertical'
-				width: dp(800)
-				height: dp(80)
-				padding: [dp(254.5),0,dp(254.5),0]
 				size_hint: (None,None)
-                BoxLayout: 
-                    size_hint: (None, None)
-                    height: dp(79)
-                    width: dp(291)
-                    
+				width: dp(800)
+				height: dp(298)
+				orientation: 'vertical'
+				BoxLayout:
+					orientation: 'vertical'
+					width: dp(800)
+					height: dp(200)
+					padding: [dp(20), 0]
+					size_hint: (None,None)
+					Label:
+						id: success_label
+						font_size: '30sp'
+						text_size: self.size
+						valign: 'top'
+						halign: 'center'
+						markup: 'true'
+						bold: True
+						color: hex('#333333ff')
+			# FOOTER
+			BoxLayout: 
+				padding: [10,0,10,10]
+				size_hint: (None, None)
+				height: dp(122)
+				width: dp(800)
+				orientation: 'horizontal'
+				BoxLayout: 
+					size_hint: (None, None)
+					height: dp(122)
+					width: dp(244.5)
+					padding: [0, 0, 184.5, 0]
+					Button:
+						size_hint: (None,None)
+						height: dp(52)
+						width: dp(60)
+						background_color: hex('#F4433600')
+						center: self.parent.center
+						pos: self.parent.pos
+						on_press: root.prev_screen()
+						BoxLayout:
+							padding: 0
+							size: self.parent.size
+							pos: self.parent.pos
+							Image:
+								source: "./asmcnc/apps/systemTools_app/img/back_to_menu.png"
+								center_x: self.parent.center_x
+								y: self.parent.y
+								size: self.parent.width, self.parent.height
+								allow_stretch: True
+
+				BoxLayout: 
+					size_hint: (None, None)
+					height: dp(122)
+					width: dp(291)
+					padding: [0,0,0,32]
 					Button:
 						id: next_button
-	                    background_normal: "./asmcnc/skavaUI/img/next.png"
-	                    background_down: "./asmcnc/skavaUI/img/next.png"
-	                    border: [dp(14.5)]*4
+						background_normal: "./asmcnc/skavaUI/img/next.png"
+						background_down: "./asmcnc/skavaUI/img/next.png"
+						border: [dp(14.5)]*4
 						size_hint: (None,None)
 						width: dp(291)
 						height: dp(79)
 						on_press: root.next_screen()
-						# text: 'Get started!'
+						text: 'Next...'
 						font_size: '30sp'
 						color: hex('#f9f9f9ff')
 						markup: True
-	                    center: self.parent.center
-	                    pos: self.parent.pos
-								
-			BoxLayout:
-				orientation: 'vertical'
-				padding: [10, 0, 0, 10]
-				size_hint: (None,None)
-				width: dp(70)
-				height: dp(62)
-                Button:
-                    size_hint: (None,None)
-                    height: dp(52)
-                    width: dp(60)
-                    background_color: hex('#F4433600')
-                    center: self.parent.center
-                    pos: self.parent.pos
-                    on_press: root.prev_screen()
-                    BoxLayout:
-                        padding: 0
-                        size: self.parent.size
-                        pos: self.parent.pos
-                        Image:
-                            source: "./asmcnc/apps/systemTools_app/img/back_to_menu.png"
-                            center_x: self.parent.center_x
-                            y: self.parent.y
-                            size: self.parent.width, self.parent.height
-                            allow_stretch: True
+						center: self.parent.center
+						pos: self.parent.pos
+				BoxLayout: 
+					size_hint: (None, None)
+					height: dp(122)
+					width: dp(244.5)
+					padding: [193.5, 0, 0, 0]
 """)
 
 class ApplySettingsScreen(Screen):
