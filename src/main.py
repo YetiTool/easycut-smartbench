a58e0179bda85e1bb10cd46d6b91727d7613869c
LettyYeti <lettie.adkins@yetitool.com>
2021-10-21 16:43:01 +0100

    test ignore errors flag

--- src/main.py
+++ src/main.py
@@ -123,6 +123,7 @@ def check_ansible_status():
 	ansible_from_easycut = (os.popen('grep "ansible_from_easycut=True" /home/pi/easycut-smartbench/src/config.txt').read())
 	# if this comes out empty, run ansible and reboot
 	if not ansible_from_easycut:
+		# when the playbook fails, it stops the other commands from running as well
 		os.system("/home/pi/easycut-smartbench/ansible/templates/ansible-start.sh && sudo systemctl restart ansible.service && sudo reboot")
 
 	
