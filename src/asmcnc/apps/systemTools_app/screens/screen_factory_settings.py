27351b2f423686cb66ff0cd0a02dcc66970a6537
LettyYeti <lettie.adkins@yetitool.com>
2021-10-21 12:04:46 +0100

    turn off dev mode

--- src/asmcnc/apps/systemTools_app/screens/screen_factory_settings.py
+++ src/asmcnc/apps/systemTools_app/screens/screen_factory_settings.py
@@ -397,7 +397,7 @@ class FactorySettingsScreen(Screen):
     smartbench_model_path = '/home/pi/smartbench_model_name.txt'
     machine_serial_number_filepath  = "/home/pi/smartbench_serial_number.txt"
 
-    dev_mode = True
+    dev_mode = False
 
     def __init__(self, **kwargs):
         super(FactorySettingsScreen, self).__init__(**kwargs)
