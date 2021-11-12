f8f4e7073d28c93540662ac1a999821f491a67c1
LettyYeti <lettie.adkins@yetitool.com>
2021-11-12 11:53:28 +0000

    test with no threads and no public ip get

--- src/settings/settings_manager.py
+++ src/settings/settings_manager.py
@@ -64,7 +64,7 @@ class Settings(object):
                     IPAddr=socket.gethostbyname(self.full_hostname)
                     self.ip_address = str(IPAddr)
                     self.wifi_available = True
-                    self.get_public_ip_address()
+                    # self.get_public_ip_address()
 
                 except:
                     self.ip_address = ''
@@ -79,7 +79,7 @@ class Settings(object):
                     # ping to check connection
                     # NB, if this comes out false but there's an IP it indicates connection in local network
                     self.wifi_available = self.do_ping_check()
-                    self.get_public_ip_address()
+                    # self.get_public_ip_address()
 
                 except:
                     self.ip_address = ''
@@ -96,7 +96,7 @@ class Settings(object):
 
                         # ping to check connection
                         self.wifi_available = self.do_ping_check()
-                        self.get_public_ip_address()
+                        # self.get_public_ip_address()
 
                     else:
                         self.ip_address = ''
