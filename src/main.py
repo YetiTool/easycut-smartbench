d11b7260e74daec0afe15471025770655e5e3bf4
LettyYeti <lettie.adkins@yetitool.com>
2021-11-17 16:52:01 +0000

    update tags and release notes

--- src/main.py
+++ src/main.py
@@ -79,7 +79,7 @@ from asmcnc.skavaUI import screen_restart_smartbench # @UnresolvedImport
 Cmport = 'COM3'
 
 # Current version active/working on
-initial_version = 'v2.0.2'
+initial_version = 'v2.0.3'
 
 config_flag = False
 		
