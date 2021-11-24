904fc8e76de3659513ae112f4304aaaefc657129
LettyYeti <lettie.adkins@yetitool.com>
2021-11-24 15:25:58 +0000

    initial draft for new support buttons and screen

--- src/asmcnc/apps/systemTools_app/screen_manager_systemtools.py
+++ src/asmcnc/apps/systemTools_app/screen_manager_systemtools.py
@@ -5,7 +5,7 @@ import sys, os
 from asmcnc.comms import usb_storage
 from asmcnc.skavaUI import popup_info, screen_diagnostics
 from asmcnc.apps.systemTools_app.screens import popup_system, screen_system_menu, screen_build_info, screen_beta_testing, \
-screen_grbl_settings, screen_factory_settings, screen_update_testing, screen_developer_temp, screen_final_test
+screen_grbl_settings, screen_factory_settings, screen_update_testing, screen_developer_temp, screen_final_test, screen_support_menu
 
 class ScreenManagerSystemTools(object):
 
@@ -29,6 +29,12 @@ class ScreenManagerSystemTools(object):
            build_info_screen = screen_build_info.BuildInfoScreen(name = 'build_info', machine = self.m, system_tools = self, settings = self.set, localization = self.l)
            self.sm.add_widget(build_info_screen)
        self.sm.current = 'build_info'
+
+    def open_support_menu_screen(self):
+       if not self.sm.has_screen('support_menu'):
+           support_menu_screen = screen_support_menu.SupportMenuScreen(name = 'support_menu', machine = self.m, system_tools = self, settings = self.set, localization = self.l)
+           self.sm.add_widget(support_menu_screen)
+       self.sm.current = 'support_menu'
 
     def download_logs_to_usb(self):
         self.usb_stick.enable()
@@ -164,6 +170,7 @@ class ScreenManagerSystemTools(object):
     def exit_app(self):
         self.sm.current = 'lobby'
         self.destroy_screen('build_info')
+        self.destroy_screen('support_menu')
         self.destroy_screen('system_menu')
         self.destroy_screen('beta_testing')
         self.destroy_screen('grbl_settings')
