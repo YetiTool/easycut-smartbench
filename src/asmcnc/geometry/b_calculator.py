e8ced888517c733fa646163feefa1cc07f071465
hsthYETI <hugh.harford@yetitool.com>
2022-02-01 16:55:37 +0000

    Refactoring boundary walk
    job_envelope_inc_circles especially but also b_calculator

--- src/asmcnc/geometry/b_calculator.py
+++ src/asmcnc/geometry/b_calculator.py
@@ -22,47 +22,35 @@ class BCalculator():
     Purpose: undertake geometry and return array of gcode 
     coordinates that represent a simple rectangular boundary
     '''
-    # for debugging levels / details
-    details_to_console = 11  
-    envelope_of_work = object
     gcode_file_path = ""
     gcodefile = []
-    boundary_xy = []
-        # boundary_xy will later be: 
-            # [0] = x coord
-            # [1] = y coord
     datum_x = 0.0
     datum_y = 0.0
     bound_range_x = [0,0]
     bound_range_y = [0,0]
     
     def __init__(self):
-        # re-set lists:
-        self.boundary_xy = [[0.0, 0.0]]
-        # get list of x,y gcode from gcode file 
-        self.sort_gcode_input("")
+        '''
+        Resets boundary for security
+        '''
+        self.boundary_xy = [[0.0, 0.0]]  # re-set lists
+        self.sort_gcode_input("")  # get list of x,y gcode from gcode file
     
     def sort_gcode_input(self, input_file_path_to_gcode = ""):
         '''
         sort_gcode_input: get list of x,y gcode from gcode file
-        '''
-        # test gcode_file_path...
-        self.gcode_file_path = "test/gcode_test_files/paired_gCode/Circles_and_star_1.gcode"
+        blank string parameter defaults to path set inside this function:
         # "test/gcode_test_files/paired_gCode/Circles_and_star_1.gcode"
-        # THE BELOW DOESN'T WORK - MUST PICK A FILE WITH .NC EXTENSION
-
+        '''
+        # default test gcode_file_path...
+        self.gcode_file_path = "test/gcode_test_files/paired_gCode/Circles_and_star_1.gcode"
         if (input_file_path_to_gcode != ""): # set path to file, if provided
             self.gcode_file_path = input_file_path_to_gcode
-
         # get job range
         self.envelope_of_work = job_envelope.BoundingBox()
         self.envelope_of_work.set_job_envelope(self.gcode_file_path)
 
     def get_job_envelope_from_gcode(self):
-        
-        if self.details_to_console == 11: 
-            print(">>>> get_job_envelope_from_gcode ____________ ") # + self.gcode_file_path)
-
         found_envelope = (self.envelope_of_work.range_x, 
                           self.envelope_of_work.range_y) 
         return found_envelope
--- src/asmcnc/geometry/job_envelope_inc_circles.py
+++ src/asmcnc/geometry/job_envelope_inc_circles.py
@@ -54,6 +54,11 @@ class BoundingBox():
         y_values = []
         z_values = []
         g_values = []
+        # use sample data for now to prove a point
+        sample_only_value = 1.1
+        x_values.append(sample_only_value)
+        y_values.append(sample_only_value)
+        z_values.append(sample_only_value)
 
         includes_arcs = 0
         
@@ -62,50 +67,63 @@ class BoundingBox():
             blocks = line.strip().split(" ")
             for part in blocks:
                 try:
-                    if "X0.000Y0.000" in part: #skip if 'datum' RISKY
-                        # assuming datum is actual datum and not part of cut workflow
-                        # FOR DEBUG ### 
-                        print("DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM DATUM ")
-                        continue
-                    else:
-                        if part.startswith(('X')): 
-                            x_values.append(float(part[1:]))
-                            print('APPENDING to x')
-                        if part.startswith(('Y')): 
-                            y_values.append(float(part[1:]))
-                            print('APPENDING to y')
-                        if part.startswith(('Z')): 
-                            z_values.append(float(part[1:]))
-                            print('APPENDING to z')
-                        if part.startswith(('G')):
-                            g_values.append(float(part[1:]))
-                            print('APPENDING to g')
-                        ### IDENTIFY ARCS OR CIRCLES with G2 or G3
-                        if part.startswith('G2') or part.startswith('G3'): 
-                            includes_arcs = self.check_circle_mode(line)
-                            print("includes_arcs: {}".format(includes_arcs))
+                    if part.startswith(('X')): 
+                        x_values.append(float(part[1:]))
+                        # print('APPENDING to x')
+                    if part.startswith(('Y')): 
+                        y_values.append(float(part[1:]))
+                    if part.startswith(('Z')): 
+                        z_values.append(float(part[1:]))
+
+                    ### IDENTIFY ARCS OR CIRCLES with G2 or G3
+                    if part.startswith('G2') or part.startswith('G3'): 
+                        includes_arcs = self.check_circle_mode(line)
+
+                    if includes_arcs: # WAS... part.startswith(('G')):
+                        # need to sort through G values, as multi-facted line...
+                        # the approach used for x, y, z above won't operate:
+                        check = self.sortarclinepart(part)
+                        if type(check) == list and type(check[0]) == float: 
+                            # print('going to to APPEND to g {}'.format(part))
+                            g_values.append(check)
+                        else:
+                            pass
+                    includes_arcs = 0
+                        
                 except TypeError as e:
                     print(
-                        'TypeError: {} TypeError HERE HERE HERE'.format(e))
+                        'TypeError: {} HERE '.format(e))
                 except ValueError as e:
-                    print( ' ValueError: {} Envelope calculator: skipped {}'.format(e, part))
+                    print( ' ValueError: {}: skipped {}'.format(e, part))
         # HERE IS THE (or at least one) BROKEN BIT, BROKEN BIT, BROKEN BIT
             # there should be no instances of "X0.000Y0.000" included
-        try:  
-            self.range_x[0], self.range_x[1] = min(x_values), max(x_values)
-        except ValueError as e:
-            print('xxx HERE ERROR, ERROR!! ValueError: {}'.format(e))
-        try:
-            self.range_y[0], self.range_y[1] = min(y_values), max(y_values)
-        except ValueError as e:
-            print('yyy HERE ERROR, ERROR!! ValueError: {}'.format(e))
-        try:
-            self.range_z[0], self.range_z[1] = min(z_values), max(z_values)
-        except ValueError as e:
-            print('zzz HERE ERROR, ERROR!! ValueError: {}'.format(e))
+        dothis = 0
+        if dothis == 1:
+            try:  
+                self.range_x[0], self.range_x[1] = min(x_values), max(x_values)
+            except ValueError as e:
+                print('xxx HERE ERROR, ERROR!! ValueError: {}'.format(e))
+            try:
+                self.range_y[0], self.range_y[1] = min(y_values), max(y_values)
+            except ValueError as e:
+                print('yyy HERE ERROR, ERROR!! ValueError: {}'.format(e))
+            try:
+                self.range_z[0], self.range_z[1] = min(z_values), max(z_values)
+            except ValueError as e:
+                print('zzz HERE ERROR, ERROR!! ValueError: {}'.format(e))
 
         try:
             gcode_file.close()
         except UnknownFileError:
             print(">>>>  Error: " + UnknownFileError)
-        
+
+    def sortarclinepart(self, glinepart):
+        '''
+        EXAMPLE: going through lines like this: 
+            G2X747.643Y448.226I180.000J0.000
+            G2X927.643Y268.226I0.000J-180.000F3000.0
+        and trying to sort out the ARC data and split it out. bit complex.
+        '''
+        f = [1.0, 2.0, 3.0]
+        # print('NOT ENTERING G-PART... sortarclinepart: {glinepart}')
+        return f
