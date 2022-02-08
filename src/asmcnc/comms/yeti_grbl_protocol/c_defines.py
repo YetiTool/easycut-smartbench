0fcf7e6b484c17d800e0b83a9d22d8e16e304a5f
LettyYeti <lettie.adkins@yetitool.com>
2022-02-08 14:05:25 +0000

    add GET_REGISTERS to c_defines file

--- src/asmcnc/comms/yeti_grbl_protocol/c_defines.py
+++ src/asmcnc/comms/yeti_grbl_protocol/c_defines.py
@@ -23,3 +23,14 @@ GET_DIGITAL_SPINDLE_INFO			= 10		 # Report Mafell digital spindle info: serial n
 RESET_DIGITAL_SPINDLE_BRUSH_TIME	= 11		 # Reset brush timer in Mafell digital spindle
 RESET_SEQUENCE_NUMBER				= 12		 # Reset protocol V2 sequence number to 0. Command would not generate sequence error even if expected sequence number does not match
 TMC_COMMAND					        = 50		 # TMC command, see table xx
+
+# common commands to be applied to whole system
+SET_SG_ALARM          = 100  # desired stall behaviour: if "true" then stall guard value below the limit will trigger alarm
+SET_CALIBR_MODE       = 101  # 1: reset all calibrations and prepare for new one, 2: complete calibration, compute , 3: print calibration coefficients
+RESTORE_TMC_DEFAULTS  = 104  # restore all TMC default settings from flash - safety net in case parameters are completely bollocked 
+STORE_TMC_PARAMS      = 105  # store existing (tuned) paraeters to the flash
+GET_REGISTERS         = 106 
+WDT_TMC_TEST          = 107  # value = 0x10: disable WD feed; other value: report EEPROM dump
+REPORT_STALLS         = 108  # report list of last stalls with associated freeze frame
+UPLOAD_CALIBR_VALUE   = 109  # upload calibration from host. Must be preceded by TMC_CALIBRATION_INIT_xxx 
+REPORT_RAW_SG         = 110  # 1: report raw stall guard values for calibration purposes. default: 0; non-persistent
