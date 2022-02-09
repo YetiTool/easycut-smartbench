3cf3d4a7ac5b2b3c2d4ff92b8017d997b4902dcc
Dennis <kiselev.dennis@gmail.com>
2022-02-09 17:25:03 +0000

    Lower beam QC app

---
+++ src/lower_beam_qc_app.py
@@ -0,0 +1,56 @@
+'''
+Created on 8 Feb 2022
+@author: Dennis
+'''
+
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, NoTransition
+
+from asmcnc.comms.router_machine import RouterMachine 
+from asmcnc.comms import server_connection
+from asmcnc.apps.app_manager import AppManagerClass
+from settings.settings_manager import Settings
+from asmcnc.job.job_data import JobData
+from asmcnc.comms.localization import Localization
+from kivy.clock import Clock
+
+from asmcnc.skavaUI.screen_home import HomeScreen
+from asmcnc.production.lower_beam_qc_jig.lower_beam_qc import LowerBeamQC
+
+from datetime import datetime
+
+Cmport = 'COM3'
+
+
+def log(message):
+    timestamp = datetime.now()
+    print (timestamp.strftime('%H:%M:%S.%f' )[:12] + ' ' + message)
+
+class LowerBeamQCApp(App):
+    def build(self):
+        log('Starting diagnostics')
+
+        sm = ScreenManager(transition=NoTransition())
+
+        sett = Settings(sm)
+
+        l = Localization()
+
+        jd = JobData(localization = l, settings_manager = sett)
+
+        m = RouterMachine(Cmport, sm, sett, l, jd)
+
+        if m.s.is_connected():
+            Clock.schedule_once(m.s.start_services, 4)
+
+        home_screen = HomeScreen(name='home', screen_manager = sm, machine = m, job = jd, settings = sett, localization = l)
+        sm.add_widget(home_screen)
+
+        lower_beam_qc = LowerBeamQC(name='qc', sm = sm, m = m, l=l)
+        sm.add_widget(lower_beam_qc)
+
+        sm.current = 'qc'
+        return sm
+
+if __name__ == '__main__':
+    LowerBeamQCApp().run()
