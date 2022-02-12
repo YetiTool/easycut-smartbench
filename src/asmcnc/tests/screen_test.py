031c213362bf38ca3342421a27bf312300400a34
LettyYeti <lettie.adkins@yetitool.com>
2022-02-12 13:39:03 +0000

    test sending motor commands

--- src/asmcnc/tests/screen_test.py
+++ src/asmcnc/tests/screen_test.py
@@ -75,6 +75,6 @@ class TestScreen(Screen):
 
         Clock.schedule_once(self.m.s.start_services, 4)
 
-        Clock.schedule_once(lambda dt: self.m.send_command_to_motor(motor=TMC_X1, command=SET_TOFF, value=0), 6)
+        Clock.schedule_once(lambda dt: self.m.send_command_to_motor(motor=TMC_X1, command=SET_TOFF, value=0), 30)
 
         
