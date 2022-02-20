be9f5745209f5fe2d9a943973200744c7e166695
LettyYeti <lettie.adkins@yetitool.com>
2022-02-20 16:43:48 +0000

    get statuses

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -2344,7 +2344,7 @@ class RouterMachine(object):
         log("Calibrating...")
 
         self.s.run_skeleton_buffer_stuffer(calibration_gcode)
-        self.poll_end_of_calibration_file_stream = Clock.schedule_interval(self.post_calibration_file_stream, 10)
+        self.poll_end_of_calibration_file_stream = Clock.schedule_interval(self.post_calibration_file_stream, 5)
 
     def quick_scrub(self, line):
 
