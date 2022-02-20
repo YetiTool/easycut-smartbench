c2c92eb42ad61fb8bfb2624070eb8cfcc5163940
LettyYeti <lettie.adkins@yetitool.com>
2022-02-20 10:48:30 +0000

    test tuning X other dir

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -1910,7 +1910,7 @@ class RouterMachine(object):
         # 3. Start long jogging in the axis of interest at 300mm/min for X and Y or for 30mm/min for Z
 
         if X and Z and not Y: 
-            self.s.write_command('$J = G91 X-1490 Z-149 F301.5')
+            self.s.write_command('$J = G91 X1490 Z-149 F301.5')
 
         elif Y: 
             self.jog_absolute_single_axis('Y', self.y_max_jog_abs_limit, 300)
