711177d85c1216908152c995650890fcbb293688
LettyYeti <lettie.adkins@yetitool.com>
2022-02-21 14:59:33 +0000

    testing starting services from screen

--- src/asmcnc/production/z_head_qc_jig/z_head_qc_home.py
+++ src/asmcnc/production/z_head_qc_jig/z_head_qc_home.py
@@ -79,8 +79,8 @@ class ZHeadQCHome(Screen):
 
         self.start_calibration_timer(0.5)
 
-        if m.s.is_connected():
-            Clock.schedule_once(m.s.start_services, 4)
+        if self.m.s.is_connected():
+            Clock.schedule_once(self.m.s.start_services, 4)
 
     def start_calibration_timer(self, minutes):
         self.sm.get_screen('qc3').update_time(minutes*30)
