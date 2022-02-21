ce396851d18c8365ce9dc9e1ae3e667d63ba528d
archiejarvis <archiepeterjarvis@gmail.com>
2022-02-21 13:48:58 +0000

    fix variable name

--- src/asmcnc/production/z_head_qc_jig/z_head_qc_db2.py
+++ src/asmcnc/production/z_head_qc_jig/z_head_qc_db2.py
@@ -38,7 +38,7 @@ class ZHeadQCDB2(Screen):
         except:
             self.sm.current = 'qcDB4'
 
-    def set_serial_number(self, serial_number):
+    def set_serial_no(self, serial_number):
         self.serial_number = serial_number
 
     def enter_next_screen(self, dt):
