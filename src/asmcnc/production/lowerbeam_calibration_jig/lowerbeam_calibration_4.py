a82b2054d9c4d8da0a23c345500cf6686ace53e8
archiejarvis <archiepeterjarvis@gmail.com>
2022-02-21 16:36:55 +0000

    lowerbeam send data to database

--- src/asmcnc/production/lowerbeam_calibration_jig/lowerbeam_calibration_4.py
+++ src/asmcnc/production/lowerbeam_calibration_jig/lowerbeam_calibration_4.py
@@ -1,5 +1,6 @@
 from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
+from asmcnc.comms.yeti_grbl_protocol.c_defines import *
 
 Builder.load_string("""
 <LBCalibration4>:
@@ -55,10 +56,26 @@ class LBCalibration4(Screen):
 
         self.sm = kwargs['sm']
         self.m = kwargs['m']
+        self.calibration_db = kwargs['calibration_db']
 
     def enter_prev_screen(self):
         self.sm.current = 'lbc2'
 
     def enter_next_screen(self):
-        self.sm.get_screen('lbc5').set_serial_no(self.serial_no_input.text)
-        self.sm.current = 'lbc5'
+        try:
+            self.calibration_db.send_calibration_payload(TMC_Y1)
+            self.calibration_db.send_calibration_payload(TMC_Y2)
+            self.sm.get_screen('lbc5').set_serial_no(self.serial_no_input.text)
+            self.sm.current = 'lbc5'
+        except:
+            self.sm.get_screen('lbc6').set_serial_no(self.serial_no_input.text)
+            self.sm.current = 'lbc6'
+
+    def send_calibration_payload(self, motor_index):
+        sg_coefficients = self.m.TMC_motor[motor_index].calibration_dataset_SG_values
+        cs = self.m.TMC_motor[motor_index].calibrated_at_current_setting
+        sgt = self.m.TMC_motor[motor_index].calibrated_at_sgt_setting
+        toff = self.m.TMC_motor[motor_index].calibrated_at_toff_setting
+        temperature = self.m.TMC_motor[motor_index].calibrated_at_temperature
+        
+        self.calibration_db.send_z_head_calibration(self.serial_number, motor_index, sg_coefficients, cs, sgt, toff, temperature)
