ca303f38828d434f0c9a351db8285050807ee87a
LettyYeti <lettie.adkins@yetitool.com>
2022-02-21 18:24:49 +0000

    output coefficients to test database

--- src/asmcnc/production/lowerbeam_calibration_jig/lowerbeam_calibration_1.py
+++ src/asmcnc/production/lowerbeam_calibration_jig/lowerbeam_calibration_1.py
@@ -34,7 +34,7 @@ class LBCalibration1(Screen):
         self.sm = kwargs['sm']
         self.m = kwargs['m']
 
-        self.update_time(30 * 0.67) 
+        self.update_time(30 * 0.7) 
 
     def update_time(self, time_left):
         seconds = time_left
