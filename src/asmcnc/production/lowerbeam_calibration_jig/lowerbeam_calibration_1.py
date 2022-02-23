6fae37566695025e60ed192d33ef62408a97043e
LettyYeti <lettie.adkins@yetitool.com>
2022-02-23 11:21:03 +0000

    update cover and belt instrctions, add micro-jog when timer starts

--- src/asmcnc/production/lowerbeam_calibration_jig/lowerbeam_calibration_1.py
+++ src/asmcnc/production/lowerbeam_calibration_jig/lowerbeam_calibration_1.py
@@ -7,6 +7,7 @@ import datetime
 Builder.load_string("""
 <LBCalibration1>:
     calibrate_time:calibrate_time
+    user_text : user_text
 
     BoxLayout:
         orientation: 'vertical'
@@ -17,12 +18,13 @@ Builder.load_string("""
             rows: 2
 
             Label:
-                text: 'Countdown to calibration...'
+                id: user_text
+                text: "Getting ready..."
                 font_size: dp(50)
             
             Label:
                 id: calibrate_time
-                text: '00:30:00'
+                text: ''
                 font_size: dp(50)
 
 """)
@@ -44,13 +46,24 @@ class LBCalibration1(Screen):
             self.sm.current = 'lbc2'
 
         elif not self.timer_started:
+            Clock.schedule_once(lambda dt: self.start_calibration_timer(60), 5)
+
+    def start_calibration_timer(self, minutes):
+
+        if self.state().startswith('Idle'):
             self.m.jog_absolute_xy(self.m.x_min_jog_abs_limit, self.m.y_min_jog_abs_limit, 6000)
             self.m.jog_absolute_single_axis('Z', self.m.z_max_jog_abs_limit, 750)
             self.m.jog_relative('X', 2, 6000)
             self.update_time(30 * 60) # 30 minutes
             self.timer_started = True
 
+        elif: 
+            Clock.schedule_once(lambda dt: self.start_calibration_timer(60), 1)
+
     def update_time(self, time_left):
+
+        self.user_text = 'Countdown to calibration...'
+
         seconds = time_left
 
         def count_down(seconds):
