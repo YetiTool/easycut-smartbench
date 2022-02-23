89a3cb7cb5ed7517c18a00e6bc48d56dda022e6e
LettyYeti <lettie.adkins@yetitool.com>
2022-02-23 10:43:23 +0000

    update cover and belt instrctions, add micro-jog when timer starts

--- src/asmcnc/production/z_head_qc_jig/z_head_qc_home.py
+++ src/asmcnc/production/z_head_qc_jig/z_head_qc_home.py
@@ -83,7 +83,7 @@ class ZHeadQCHome(Screen):
         self.start_calibration_timer(60)
 
     def start_calibration_timer(self, minutes):
-        self.m.jog_relative('Y', 0.1, 6000)
+        # self.m.jog_relative('Y', 0.1, 6000)
         self.sm.get_screen('qc3').update_time(minutes*30)
 
     def enter_qc(self):
