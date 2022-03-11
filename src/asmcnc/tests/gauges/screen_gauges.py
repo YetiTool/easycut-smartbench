b89f82621fadf5744d55d5a699c4aa65c494957d
archiejarvis <archiepeterjarvis@gmail.com>
2022-03-11 15:11:43 +0000

    run overnight test

--- src/asmcnc/tests/gauges/screen_gauges.py
+++ src/asmcnc/tests/gauges/screen_gauges.py
@@ -27,15 +27,34 @@ class GaugeScreen(Screen):
 
     def add_gauge(self, gauge):
         self.add_widget(gauge)
-        self.run()
+        self.start_overnight_test()
     
     # ddef jog_absolute_single_axis(self, axis, target, speed):
+    def start_overnight_test(self):
 
-    def run(self):
-        self.m.s.write_command('M5')
-        self.m.jog_absolute_single_axis('Z', '-25', '300')
-        self.m.jog_absolute_single_axis('Z', '-100', '300')
-        self.m.jog_absolute_single_axis('Z', '-25', '300')
-        self.m.jog_absolute_single_axis('Z', '-100', '300')
-        self.m.jog_absolute_single_axis('Z', '-25', '300')
-        self.m.jog_absolute_single_axis('Z', '-100', '300')
+        self.m.jog_absolute_xy(self.m.x_min_jog_abs_limit, self.m.y_min_jog_abs_limit, 6000)
+        self.m.jog_absolute_single_axis('Z', self.m.z_max_jog_abs_limit, 750)
+
+        Clock.schedule_once(self.stream_overnight_file, 5)
+
+    def stream_overnight_file(self, dt):
+
+        if self.m.state().startswith('Idle') and not self.overnight_running:
+
+            self.overnight_running = True
+
+            filename = './asmcnc/apps/systemTools_app/files/overnight_test.gc'
+            # filename = './asmcnc/apps/systemTools_app/files/mini_run.gc'
+
+            with open(filename) as f:
+                overnight_gcode_pre_scrubbed = f.readlines()
+
+            overnight_gcode = [self.m.quick_scrub(line) for line in overnight_gcode_pre_scrubbed]
+
+            print("Running overnight test...")
+
+            self.m.s.run_skeleton_buffer_stuffer(overnight_gcode)
+            self.poll_end_of_overnight_file_stream = Clock.schedule_interval(self.post_overnight_file_stream, 60)
+
+        else:
+            Clock.schedule_once(self.stream_overnight_file, 3)
