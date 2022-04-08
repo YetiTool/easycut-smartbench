e9aa0925a73a51e089e6339189f439a2a3a99988
LettyYeti <lettie.adkins@yetitool.com>
2022-04-08 13:58:03 +0100

    revert script

--- src/asmcnc/production/database/script_insert_statuses.py
+++ src/asmcnc/production/database/script_insert_statuses.py
@@ -15,24 +15,3 @@ calibration_db.set_up_connection()
 # calibration_db.insert_stage("CalibrationOT")
 # calibration_db.insert_stage("CalibrationCheckOT")
 # calibration_db.insert_stage("FullyCalibratedTest")
-
-
-def get_all_serials_by_machine_serial(self, machine_serial):
-
-	
-with calibration_db.conn.cursor() as cursor:
-    query = "SELECT + \
-            ZHeadSerialNumber, + \
-            LowerBeamSerialNumber, + \
-            UpperBeamSerialNumber, + \
-            ConsoleSerialNumber, + \
-            YBenchSerialNumber, + \
-            SpindleSerialNumber, + \
-            Squareness + \
-            FROM Machines WHERE MachineSerialNumber = '%s'" % machine_serial
-
-    cursor.execute(query)
-
-    data = cursor.fetchone()
-
-    return [data[0], data[1], data[2], data[3], data[4], data[5], data[6]]
