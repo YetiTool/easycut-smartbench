820f6a6c6b3d2d06cfea9fe1041528dc43dc5414
LettyYeti <lettie.adkins@yetitool.com>
2022-04-11 17:44:45 +0100

    debug why branch detect not working

--- src/asmcnc/apps/systemTools_app/screens/calibration/screen_serial_numbers.py
+++ src/asmcnc/apps/systemTools_app/screens/calibration/screen_serial_numbers.py
@@ -233,8 +233,13 @@ class UploadSerialNumbersScreen(Screen):
 
     def get_software_version_before_release(self):
 
-        if self.set.sw_branch == 'ft' or self.dev_mode: self.sw_version = self.set.latest_sw_version
+        if self.set.sw_branch.startswith('ft') or self.dev_mode: 
+            self.sw_version = self.set.latest_sw_version
         else: self.sw_version = self.set.sw_version
+
+        print("VERSIONS:")
+        print(self.sw_version)
+        print(self.set.sw_branch)
 
     def validate_and_download(self):
         regex_check = self.check_valid_inputs_regex()
