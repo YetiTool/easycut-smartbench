77791a9c267f8b91800223a841865272bb8b9f13
Archie <archiepeterjarvis@gmail.com>
2022-04-12 13:08:57 +0100

    Change size of gauges

--- src/asmcnc/comms/serial_connection.py
+++ src/asmcnc/comms/serial_connection.py
@@ -1045,10 +1045,6 @@ class SerialConnection(object):
                             partial(self.sm.get_screen('go').y_load_gauge.set_value, self.sg_y_axis))
                         Clock.schedule_once(
                             partial(self.sm.get_screen('go').z_load_gauge.set_value, self.sg_z_motor_axis))
-                        #
-                        # self.sm.get_screen('go').x_load_gauge.set_value(self.sg_x_motor_axis)
-                        # self.sm.get_screen('go').y_load_gauge.set_value(self.sg_y_axis)
-                        # self.sm.get_screen('go').z_load_gauge.set_value(self.sg_z_motor_axis)
                     except:
                         log('Failed to set value')
                         print(traceback.format_exc())
