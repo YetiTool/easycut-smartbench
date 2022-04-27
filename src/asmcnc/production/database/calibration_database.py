ac1f48407b7953da4a870e972c9c50141b952fe7
Archie <archiepeterjarvis@gmail.com>
2022-04-27 13:56:46 +0100

    Remove two identifying characters from serial number to create integer

--- src/asmcnc/production/database/calibration_database.py
+++ src/asmcnc/production/database/calibration_database.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 from datetime import datetime
 
+
 def log(message):
     timestamp = datetime.now()
-    print (timestamp.strftime('%H:%M:%S.%f' )[:12] + ' ' + str(message))
+    print(timestamp.strftime('%H:%M:%S.%f')[:12] + ' ' + str(message))
+
 
 try:
     import pytds
@@ -12,25 +14,24 @@ try:
 except:
     log('Pytds or influxdb not installed')
 
-class CalibrationDatabase(object):
 
+class CalibrationDatabase(object):
     # THIS WILL NEED EDITING IF DB CHANGES AS IDS WILL LIKELY CHANGE TOO!!
     stage_id_dict = {
-        "CalibrationQC" : 4,
-        "CalibrationCheckQC" : 5,
-        "UnweightedFT" : 6,
-        "WeightedFT" : 7,
-        "OvernightWearIn" : 8,
-        "CalibrationOT" : 9,
-        "CalibrationCheckOT" : 10,
-        "FullyCalibratedTest" : 11
+        "CalibrationQC": 4,
+        "CalibrationCheckQC": 5,
+        "UnweightedFT": 6,
+        "WeightedFT": 7,
+        "OvernightWearIn": 8,
+        "CalibrationOT": 9,
+        "CalibrationCheckOT": 10,
+        "FullyCalibratedTest": 11
     }
 
     def __init__(self):
         self.conn = None
 
-
-    # AVAILABLE STAGES ARE: 
+    # AVAILABLE STAGES ARE:
 
     # "CalibrationQC"
     # "CalibrationCheckQC"
@@ -41,7 +42,6 @@ class CalibrationDatabase(object):
     # "CalibrationCheckOT"
     # "FullyCalibratedTest"
 
-
     def set_up_connection(self):
 
         try:
@@ -51,22 +51,25 @@ class CalibrationDatabase(object):
             log("Can't import credentials")
 
         try:
-            self.conn = pytds.connect(credentials.server, credentials.database, credentials.username, credentials.password)
+            self.conn = pytds.connect(credentials.server, credentials.database, credentials.username,
+                                      credentials.password)
             log("Connected to database")
 
-        except: 
+        except:
             log('Unable to connect to database')
 
         try:
-            self.influx_client = InfluxDBClient(credentials.influx_server, credentials.influx_port, credentials.influx_username, credentials.influx_password, credentials.influx_database)
+            self.influx_client = InfluxDBClient(credentials.influx_server, credentials.influx_port,
+                                                credentials.influx_username, credentials.influx_password,
+                                                credentials.influx_database)
             log("Connected to InfluxDB")
 
         except:
             log("Unable to connect to InfluxDB")
-        
+
     def insert_serial_numbers(self, machine_serial, z_head_serial, lower_beam_serial, upper_beam_serial,
-                            console_serial, y_bench_serial, spindle_serial, software_version, firmware_version,
-                            squareness):
+                              console_serial, y_bench_serial, spindle_serial, software_version, firmware_version,
+                              squareness):
         date = datetime.now().strftime('%d/%m/%Y %H:%M')
 
         with self.conn.cursor() as cursor:
@@ -132,7 +135,7 @@ class CalibrationDatabase(object):
 
     def get_stage_id_by_description(self, description):
 
-        try: 
+        try:
             with self.conn.cursor() as cursor:
                 query = "SELECT Id FROM Stages WHERE Description = '%s'" % description
 
@@ -140,7 +143,7 @@ class CalibrationDatabase(object):
 
                 return cursor.fetchone()[0]
 
-        except: 
+        except:
             log("Could not get stage ID from DB!!")
             print(traceback.format_exc())
 
@@ -148,11 +151,9 @@ class CalibrationDatabase(object):
             # BUT if anything in db changes, it may be wrong!! 
             return self.stage_id_dict.get(description)
 
-
-
     def insert_final_test_stage(self, machine_serial, ft_stage_id):
 
-        try: 
+        try:
             combined_id = (machine_serial + str(ft_stage_id))[2:]
 
             if self.does_final_test_stage_already_exist(combined_id):
@@ -170,7 +171,6 @@ class CalibrationDatabase(object):
         except pytds.tds_base.IntegrityError:
             log("Final test stage already exists for this SN")
 
-
     def does_final_test_stage_already_exist(self, combined_id):
 
         with self.conn.cursor() as cursor:
@@ -182,10 +182,11 @@ class CalibrationDatabase(object):
 
         ### check whether tuple is empty
 
-
-    def insert_final_test_statistics(self, machine_serial, ft_stage_id, x_forw_avg, x_forw_peak, x_backw_avg, x_backw_peak,
+    def insert_final_test_statistics(self, machine_serial, ft_stage_id, x_forw_avg, x_forw_peak, x_backw_avg,
+                                     x_backw_peak,
                                      y_forw_avg, y_forw_peak, y_backw_avg, y_backw_peak, y1_forw_avg, y1_forw_peak,
-                                     y1_backw_avg, y1_backw_peak, y2_forw_avg, y2_forw_peak, y2_backw_avg, y2_backw_peak,
+                                     y1_backw_avg, y1_backw_peak, y2_forw_avg, y2_forw_peak, y2_backw_avg,
+                                     y2_backw_peak,
                                      z_forw_avg, z_forw_peak, z_backw_avg, z_backw_peak):
         combined_id = (machine_serial + str(ft_stage_id))[2:]
 
@@ -217,15 +218,13 @@ class CalibrationDatabase(object):
                     "%s, %s, %s, '%s', %s, %s, %s, %s)"
 
             for status in statuses:
-
                 print(status)
 
                 cursor.execute(query % (combined_id, status[0], status[1], status[2], status[3], status[4], status[5],
                                         status[6], status[7], status[8], status[9], status[10], status[11], status[12],
                                         status[13], status[14], status[15], status[16], status[17], status[18]))
-        
-                self.conn.commit()
 
+                self.conn.commit()
 
     def get_serials_by_machine_serial(self, machine_serial):
         with self.conn.cursor() as cursor:
@@ -259,23 +258,21 @@ class CalibrationDatabase(object):
                 }
             except:
                 log('Database is empty or incomplete for ' + combined_id)
-            
+
             return parameters
 
-    
     def get_ambient_temperature(self):
 
         try:
 
             query = u'SELECT "temperature" FROM "last_three_months"."environment_data" WHERE \
             ("device_ID" = \'“eDGE-2”\') AND time > now() - 2m ORDER ' \
-            u'BY DESC LIMIT 1 '
+                    u'BY DESC LIMIT 1 '
 
             return self.influx_client.query(query).raw['series'][0]['values'][0][1]
 
-        except: 
+        except:
             return None
-
 
     def get_all_serials_by_machine_serial(self, machine_serial):
         with self.conn.cursor() as cursor:
@@ -294,4 +291,3 @@ class CalibrationDatabase(object):
             data = cursor.fetchone()
 
             return [data[0], data[1], data[2], data[3], data[4], data[5], data[6]]
-
