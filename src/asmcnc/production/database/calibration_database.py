e9855427f1bdade0f99fdac72ae0b3a5b683b910
LettyYeti <lettie.adkins@yetitool.com>
2022-04-29 14:03:57 +0100

    try using FreeTDS driver instead on rpi

--- src/asmcnc/production/database/calibration_database.py
+++ src/asmcnc/production/database/calibration_database.py
@@ -29,7 +29,7 @@ class CalibrationDatabase(object):
         "FullyCalibratedTest": 8
     }
 
-    connection_string = 'DRIVER={ODBC Driver 17 for SQL Server};SERVER=%s,%s;DATABASE=%s;UID=%s;PWD=%s;'
+    connection_string = 'DRIVER={FreeTDS};SERVER=%s,%s;DATABASE=%s;UID=%s;PWD=%s;'
 
     def __init__(self):
         self.conn = None
