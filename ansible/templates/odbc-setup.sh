6e0440749f9abcc581b24a8fd1f3a3984070578c
LettyYeti <lettie.adkins@yetitool.com>
2022-04-29 12:31:33 +0100

    try adding line in case get-update causing the issue

--- ansible/templates/odbc-setup.sh
+++ ansible/templates/odbc-setup.sh
@@ -14,7 +14,7 @@ curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources
 
 # #Debian 11
 # curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
-
+apt-get install apt-transport-https
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
 # # optional: for bcp and sqlcmd
