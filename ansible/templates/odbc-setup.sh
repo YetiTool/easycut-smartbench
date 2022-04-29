8775349fdf576aecb5c65b05bdc8efeddd716801
LettyYeti <lettie.adkins@yetitool.com>
2022-04-29 12:22:19 +0100

    use correct driver install script for env

--- ansible/templates/odbc-setup.sh
+++ ansible/templates/odbc-setup.sh
@@ -1,20 +1,29 @@
-if ! [[ "16.04 18.04 20.04 21.04 21.10" == *"$(lsb_release -rs)"* ]];
-then
-    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
-    exit;
-fi
-
 sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
-curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+#Download appropriate package for the OS version
+#Choose only ONE of the following, corresponding to your OS version
+
+#Debian 8 (only supported up to driver version 17.6)
+# curl https://packages.microsoft.com/config/debian/8/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+#Debian 9
+curl https://packages.microsoft.com/config/debian/9/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+# #Debian 10
+# curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+# #Debian 11
+# curl https://packages.microsoft.com/config/debian/11/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 exit
 sudo apt-get update
 sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
-# optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+# # optional: for bcp and sqlcmd
+# sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
-# optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
+# # optional: for unixODBC development headers
+# sudo apt-get install -y unixodbc-dev
+# # optional: kerberos library for debian-slim distributions
+# sudo apt-get install -y libgssapi-krb5-2
