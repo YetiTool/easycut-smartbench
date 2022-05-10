3043b535f986382b0986011266bd47e7082b4c84
LettyYeti <lettie.adkins@yetitool.com>
2022-05-10 12:39:56 +0100

    delay didn't work, instead put in a pre-jog to try and prevent the instant crash

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -1910,8 +1910,6 @@ class RouterMachine(object):
         self.send_command_to_motor("REPORT RAW SG SET", command=REPORT_RAW_SG, value=1) # is there a way to check this has sent? 
         self.reset_tuning_flags()
 
-        time.sleep(0.5) # Is the report raw SG setting messing with jogs? Does putting in a delay fix things? 
-
         # Zero position
         log("Zero position")
         self.jog_absolute_xy(self.x_min_jog_abs_limit, self.y_min_jog_abs_limit, 6000)
