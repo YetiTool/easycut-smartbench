2df545e6544bc69ee3752d2aa81c2a5fbca4ae71
DennisKiselev <kiselev.dennis@gmail.com>
2022-07-25 14:03:04 +0100

    Updated recovery screen info popup

--- src/asmcnc/skavaUI/screen_job_recovery.py
+++ src/asmcnc/skavaUI/screen_job_recovery.py
@@ -381,13 +381,15 @@ class JobRecoveryScreen(Screen):
 
     def get_info(self):
 
-        info = ('This screen allows you to choose where to start your recovery.\n\n'
+        info = ('This screen allows you to choose where to start your recovery. '
                 'Use the arrows to navigate through the lines of the job file, and select a point to recover the job from. '
                 'The red marker indicates where the stall event happened. We suggest starting a few lines before here.\n\n'
                 'To confirm this start point, use the "GO TO XY" button. This will move the ZHead over the selected start point. '
                 'You can lower the spindle to check the tool is approximately where you expect it to be in the XY plane. '
                 'This XY position can be fine adjusted in the next screen. \n\n'
-                'Note: the Z position will be automatically calculated when the job recovery starts.'
+                'Note: the Z position will be automatically calculated when the job recovery starts.\n\n'
+                'Note: when using Job Recovery for GCode containing G2 & G3 commands, '
+                'the recovery point may be highlighted earlier in the file than actual, due to file streaming protocols.'
         )
 
         popup_info.PopupInfo(self.sm, self.l, 700, info)
