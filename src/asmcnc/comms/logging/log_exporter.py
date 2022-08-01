8da2ee9f0ce0c222347791d8d3dad014bf989c84
Archie <archiepeterjarvis@gmail.com>
2022-08-01 15:21:11 +0100

    Debug trim

--- src/asmcnc/comms/logging/log_exporter.py
+++ src/asmcnc/comms/logging/log_exporter.py
@@ -19,6 +19,8 @@ def trim_logs(log_file_path, x_lines):
 
         lines_to_remove = len(lines) - x_lines
 
+        print('Trimming ' + str(lines_to_remove) + ' lines')
+
         new_lines = lines[lines_to_remove:]
 
         untrimmed_file.truncate()
