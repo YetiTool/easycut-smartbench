7dc4626f73481c005f06d20de17a1d857c53297e
Archie <archiepeterjarvis@gmail.com>
2022-08-02 10:32:56 +0100

    Rename logs from easycut... to smartbench... and remove debug print

--- src/asmcnc/comms/logging/log_exporter.py
+++ src/asmcnc/comms/logging/log_exporter.py
@@ -13,12 +13,12 @@ def create_log_folder():
         os.mkdir(export_logs_folder)
 
 
-def generate_logs():
+def generate_logs(serial_number):
     create_log_folder()
 
     str_current_time = datetime.now().strftime("%m-%d-%Y-%H-%M-%S")
 
-    log_name = "easycut-exported-logs-" + str_current_time + ".txt"
+    log_name = "smartbench-exported-logs-" + str_current_time + "-" + str(serial_number) + ".txt"
     full_path = export_logs_folder + '/' + log_name
 
     command = "journalctl > " + full_path
@@ -35,8 +35,6 @@ def trim_logs(log_file_path, x_lines):
 
     with open(log_file_path, 'w+') as trimmed_file:
         lines_to_remove = line_count - x_lines
-
-        print('Trimming ' + str(lines_to_remove) + ' lines of ' + str(line_count) + ' lines')
         new_lines = lines[lines_to_remove:]
 
         trimmed_file.writelines(new_lines)
