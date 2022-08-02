02274e3fb1016075954880b9a7cf3b1c4b82b65e
Archie <archiepeterjarvis@gmail.com>
2022-08-02 17:22:00 +0100

    fix persistent gauge line

--- src/asmcnc/gauges/gauge_utils.py
+++ src/asmcnc/gauges/gauge_utils.py
@@ -1,15 +1,15 @@
 def get_hsl_by_percentage(percentage):
-    return (120 * (1-percentage)) / 360, 1, 1
+    return (120 * (1 - percentage)) / 360, 1, 1
 
 
 def get_gradient(value, max_value, lower_boundary=15, upper_boundary=15, inverse=False):
     abs_percentage = abs(float(value) / float(max_value)) * 100
 
     if abs_percentage > 100 - upper_boundary:
-        return (1/3, 1, 1) if inverse else (0, 1, 1)
+        return (120 / 360, 1, 1) if inverse else (0, 1, 1)
 
     if abs_percentage < lower_boundary:
-        return (0, 1, 1) if inverse else (1/3, 1, 1)
+        return (0, 1, 1) if inverse else (120 / 360, 1, 1)
 
     return get_hsl_by_percentage(abs_percentage)
 
@@ -21,6 +21,6 @@ def calculate_width(value, max_value, factor, outer_box_width):
     value = value / factor
 
     if value > max_value:
-        return max_value
+        return ((outer_box_width / max_value) * value) / 2
 
     return ((outer_box_width / max_value) * value) / 2
