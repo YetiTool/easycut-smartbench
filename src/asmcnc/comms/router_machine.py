21545234a26f64661a20439b54c04ec0963f4425
Archie <archiepeterjarvis@gmail.com>
2022-08-11 14:13:53 +0100

    test sending get_tmc_status

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -1726,7 +1726,9 @@ class RouterMachine(object):
         map(log, TMC_registers_report_string.split("\n"))
 
     def get_tmc_status(self):
-        self.send_command_to_motor('SENDING GET_TMC_STATUS', command=GET_TMC_STATUS)
+        out = self.send_command_to_motor('SENDING GET_TMC_STATUS', command=GET_TMC_STATUS)
+
+        print(out)
 
     #####################################################################
     # PROTOCOL MOTOR FUNCTIONS - USE THIS TO SEND ANY MOTOR COMMANDS
