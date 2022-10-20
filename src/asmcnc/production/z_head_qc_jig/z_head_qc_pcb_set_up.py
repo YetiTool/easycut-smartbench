8d40aae693eeeb2d7e7c400585e16b40be7ead9e
LettyYeti <lettie.adkins@yetitool.com>
2022-10-20 10:40:25 +0100

    testing FW HW read in

--- src/asmcnc/production/z_head_qc_jig/z_head_qc_pcb_set_up.py
+++ src/asmcnc/production/z_head_qc_jig/z_head_qc_pcb_set_up.py
@@ -518,6 +518,7 @@ class ZHeadPCBSetUp(Screen):
     def scrape_fw_version(self):
         fw_and_hw = str(self.m.s.fw_version).split('; HW:')
         print("OBJ FROM SC: " + str(self.m.s.fw_version))
+        print("OBJ FROM SC: " + str(self.m.s.hw_version))
         print("FW AND HW: " + str(fw_and_hw))
         return int(fw_and_hw[1]), fw_and_hw[0]
 
