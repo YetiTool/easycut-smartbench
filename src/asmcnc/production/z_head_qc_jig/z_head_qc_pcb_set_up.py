8b2cdcfc7a72ccb1daa53b5be2f2147707b53849
LettyYeti <lettie.adkins@yetitool.com>
2022-10-20 12:05:32 +0100

    syntax error

--- src/asmcnc/production/z_head_qc_jig/z_head_qc_pcb_set_up.py
+++ src/asmcnc/production/z_head_qc_jig/z_head_qc_pcb_set_up.py
@@ -677,11 +677,11 @@ class ZHeadPCBSetUp(Screen):
             self.ok_button.text = "Setting currents and coefficients..."
 
             # SET CURRENTS AND THERMAL COEFFICIENTS
-            if  self.m.set_thermal_coefficients("X", self.x_thermal_coefficient) and \
-                self.m.set_thermal_coefficients("Y", self.y_thermal_coefficient) and \
-                self.m.set_thermal_coefficients("Z", self.z_thermal_coefficient) and \
-                self.m.set_motor_current("Z", self.z_current) and \
-                self.m.set_motor_current("X", self.x_current):
+            if  self.m.set_thermal_coefficients("X", str(self.x_thermal_coefficient)) and \
+                self.m.set_thermal_coefficients("Y", str(self.y_thermal_coefficient)) and \
+                self.m.set_thermal_coefficients("Z", str(self.z_thermal_coefficient)) and \
+                self.m.set_motor_current("Z", str(self.z_current)) and \
+                self.m.set_motor_current("X", str(self.x_current)):
 
                 # STORE PARAMETERS
                 Clock.schedule_once(lambda dt: store_params_and_progress(), 1)
