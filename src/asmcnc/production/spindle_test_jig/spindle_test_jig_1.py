65ef5e4cdb259372f82979b3b8a2f73641b9e83f
Archie <archiepeterjarvis@gmail.com>
2023-01-19 13:47:21 +0000

    send m3 s0 to initialize spindle values

--- src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
+++ src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
@@ -321,8 +321,8 @@ class SpindleTestJig1(Screen):
         # print_unlock_receipt(str(unlock_code))
 
     def on_enter(self):
-        self.m.s.write_command('M3 S0')
-        Clock.schedule_once(lambda dt: self.send_get_digital_spindle_info(), 1)
+        Clock.schedule_once(lambda dt: self.m.s.write_command('M3 S0'), 1)
+        Clock.schedule_once(lambda dt: self.send_get_digital_spindle_info(), 2)
 
     def update_status_text(self, dt):
         try:
