7e01677c20217816c7c5b7dc733483a12e6aae16
Archie <archiepeterjarvis@gmail.com>
2023-01-20 11:07:42 +0000

    test full

--- src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
+++ src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
@@ -1,32 +1,8 @@
-#!/usr/bin/python
-from escpos.printer import Usb
-import sys
+from escpos import *
 
-
-def print_unlock_receipt(unlock_code):
-    try:
-        # Establish connection with printer
-        p = Usb(0x0416, 0x5011)
-        # Add spacing for stapling
-        p.text("\n\n\n")
-        # Print top logo
-        # p.image("img/logo.png")
-        # Print "PrecisionPro + Unlock code"
-        p.set("CENTER", "A", "normal", 2, 2, True, False)
-        p.text("\n\n\nPrecisionPro +")
-        p.set("CENTER", "A", "B", 2, 2, True, False)
-        p.text("\nUnlock Code:\n\n")
-        # Print Unlock code
-        p.set("CENTER", "A", "B", 2, 2, True, True)
-        p.text(" " + str(unlock_code) + " " + "\n\n\n")
-        # Print do not descard warning
-        # p.image("img/do_not_discard.png")
-        p.text("\n\n\n\n")
-        # Close connection with printer
-        p.close()
-        print("Printing complete")
-    except Exception as e:
-        print(e)
+def print_unlock_receipt(code):
+    p = printer.Serial('COM2')
+    p.text(str(code))
 
 
 
