291424bb23db91cba76dbb4edba5d58015ba304f
Archie <archiepeterjarvis@gmail.com>
2023-01-20 12:23:27 +0000

    try change fragment height

--- src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
+++ src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
@@ -11,7 +11,8 @@ def print_unlock_receipt(unlock_code):
         p.text("\n\n\n")
         p.text('WORKING')
         # Print top logo
-        p.image("asmcnc/production/spindle_test_jig/printer/img/logo.png", fragment_height=500)
+        p.ln()
+        p.image("asmcnc/production/spindle_test_jig/printer/img/logo.png")
         # Print "PrecisionPro + Unlock code"
         p.set("CENTER", "A", "normal", 2, 2, True, False)
         p.text("\n\n\nPrecisionPro +")
@@ -21,6 +22,7 @@ def print_unlock_receipt(unlock_code):
         p.set("CENTER", "A", "B", 2, 2, True, True)
         p.text(" " + str(unlock_code) + " " + "\n\n\n")
         # Print do not descard warning
+        p.ln()
         p.image("asmcnc/production/spindle_test_jig/printer/img/do_not_discard.png")
         p.text("\n\n\n\n")
         # Close connection with printer
