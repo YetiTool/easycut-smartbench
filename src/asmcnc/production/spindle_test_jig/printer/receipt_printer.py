fedfe9af896d5085d5db4c9d6f10aceb8f9bde16
Archie <archiepeterjarvis@gmail.com>
2023-01-20 10:44:11 +0000

    remove images for test

--- src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
+++ src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
@@ -4,26 +4,30 @@ import sys
 
 
 def print_unlock_receipt(unlock_code):
-    # Establish connection with printer
-    p = Usb(0x0416, 0x5011)
-    # Add spacing for stapling
-    p.text("\n\n\n")
-    # Print top logo
-    # p.image("asmcnc/production/spindle_test_jig/printer/img/logo.png")
-    # Print "PrecisionPro + Unlock code"
-    p.set("CENTER", "A", "normal", 2, 2, True, False)
-    p.text("\n\n\nPrecisionPro +")
-    p.set("CENTER", "A", "B", 2, 2, True, False)
-    p.text("\nUnlock Code:\n\n")
-    # Print Unlock code
-    p.set("CENTER", "A", "B", 2, 2, True, True)
-    p.text(" " + str(unlock_code) + " " + "\n\n\n")
-    # Print do not descard warning
-    # p.image("asmcnc/production/spindle_test_jig/printer/img/do_not_discard.png")
-    p.text("\n\n\n\n")
-    # Close connection with printer
-    p.close()
-    print("Printing complete")
+    try:
+        # Establish connection with printer
+        p = Usb(0x0416, 0x5011)
+        # Add spacing for stapling
+        p.text("\n\n\n")
+        # Print top logo
+        # p.image("asmcnc/production/spindle_test_jig/printer/img/logo.png")
+        # Print "PrecisionPro + Unlock code"
+        p.set("CENTER", "A", "normal", 2, 2, True, False)
+        p.text("\n\n\nPrecisionPro +")
+        p.set("CENTER", "A", "B", 2, 2, True, False)
+        p.text("\nUnlock Code:\n\n")
+        # Print Unlock code
+        p.set("CENTER", "A", "B", 2, 2, True, True)
+        p.text(" " + str(unlock_code) + " " + "\n\n\n")
+        # Print do not descard warning
+        # p.image("asmcnc/production/spindle_test_jig/printer/img/do_not_discard.png")
+        p.text("\n\n\n\n")
+        # Close connection with printer
+        p.close()
+        print("Printing complete")
+    except Exception as e:
+        print(e)
+
 
 
 if __name__ == '__main__':
