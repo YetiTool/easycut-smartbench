af4092a7951186fb59b3fe6805af56db102a09fb
Archie <archiepeterjarvis@gmail.com>
2023-01-20 11:59:20 +0000

    test full

--- src/settings/settings_manager.py
+++ src/settings/settings_manager.py
@@ -6,7 +6,6 @@ Module to get and store settings info
 
 import sys, os, subprocess, time, threading
 from time import sleep
-from __builtin__ import True, False
 from datetime import datetime
 from requests import get
 
@@ -410,7 +409,7 @@ class Settings(object):
 ## FIRMWARE UPDATE FUNCTIONS
     def get_fw_update(self):
         os.system("sudo pigpiod")
-        print "pigpio daemon started"
+        print ("pigpio daemon started")
         Clock.schedule_once(lambda dt: self.flash_fw(), 2)
 
     def get_hex_file(self):
