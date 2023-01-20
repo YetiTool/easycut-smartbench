52b89ba32ec283ccc300d5ef5b6468eb665121dd
Archie <archiepeterjarvis@gmail.com>
2023-01-20 10:28:26 +0000

    push receipt pritner code

--- src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
+++ src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
@@ -1,32 +1,32 @@
-# #!/usr/bin/python
-# from escpos.printer import Usb
-# import sys
-#
-#
-# def print_unlock_receipt(unlock_code):
-#     # Establish connection with printer
-#     p = Usb(0x0416, 0x5011)
-#     # Add spacing for stapling
-#     p.text("\n\n\n")
-#     # Print top logo
-#     p.image("img/logo.png")
-#     # Print "PrecisionPro + Unlock code"
-#     p.set("CENTER", "A", "normal", 2, 2, True, False)
-#     p.text("\n\n\nPrecisionPro +")
-#     p.set("CENTER", "A", "B", 2, 2, True, False)
-#     p.text("\nUnlock Code:\n\n")
-#     # Print Unlock code
-#     p.set("CENTER", "A", "B", 2, 2, True, True)
-#     p.text(" " + str(unlock_code) + " " + "\n\n\n")
-#     # Print do not descard warning
-#     p.image("img/do_not_discard.png")
-#     p.text("\n\n\n\n")
-#     # Close connection with printer
-#     p.close()
-#     print("Printing complete")
-#
-#
-# if __name__ == '__main__':
-#     code = sys.argv[1]
-#
-#     print_unlock_receipt(code)
+#!/usr/bin/python
+from escpos.printer import Usb
+import sys
+
+
+def print_unlock_receipt(unlock_code):
+    # Establish connection with printer
+    p = Usb(0x0416, 0x5011)
+    # Add spacing for stapling
+    p.text("\n\n\n")
+    # Print top logo
+    p.image("img/logo.png")
+    # Print "PrecisionPro + Unlock code"
+    p.set("CENTER", "A", "normal", 2, 2, True, False)
+    p.text("\n\n\nPrecisionPro +")
+    p.set("CENTER", "A", "B", 2, 2, True, False)
+    p.text("\nUnlock Code:\n\n")
+    # Print Unlock code
+    p.set("CENTER", "A", "B", 2, 2, True, True)
+    p.text(" " + str(unlock_code) + " " + "\n\n\n")
+    # Print do not descard warning
+    p.image("img/do_not_discard.png")
+    p.text("\n\n\n\n")
+    # Close connection with printer
+    p.close()
+    print("Printing complete")
+
+
+if __name__ == '__main__':
+    code = sys.argv[1]
+
+    print_unlock_receipt(code)
