ccb449ab4e9fb181ddbe33716b5af0a61798054a
Archie <archiepeterjarvis@gmail.com>
2023-01-20 11:00:11 +0000

    run shortened

--- src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
+++ src/asmcnc/production/spindle_test_jig/printer/receipt_printer.py
@@ -5,3 +5,10 @@ import sys
 def print_unlock_receipt(code):
     p = Usb(0x0416, 0x5011)
     p.text("Precision Pro+ Activation Code\n")
+    print("done")
+
+
+if __name__ == '__main__':
+    code = sys.argv[1]
+
+    print_unlock_receipt(code)
