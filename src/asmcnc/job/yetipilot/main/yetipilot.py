002e7223aec160b92ba64583009d2292683049fb
Archie <archiepeterjarvis@gmail.com>
2023-01-31 17:42:03 +0000

    fix time delta

--- src/asmcnc/job/yetipilot/main/yetipilot.py
+++ src/asmcnc/job/yetipilot/main/yetipilot.py
@@ -127,7 +127,7 @@ class YetiPilot:
             time_stamp = datetime.now() - self.m.s.job_start_time
 
         self.logger.add_log(
-            load, adjustment, time_stamp.strftime('%H:%M:%S:%f'), self.spindle_load_stack[:], self.spindle_load_stack[:],
+            load, adjustment, str(time_stamp), self.spindle_load_stack[:], self.spindle_load_stack[:],
             adjustment, adjustment, self.m.s.feed_override_percentage, str(self.moving_in_z), self.m.s.sg_x_motor_axis,
             self.m.s.sg_y_axis, self.m.s.sg_z_motor_axis, self.m.s.sg_x1_motor, self.m.s.sg_x2_motor, self.m.s.sg_y1_motor,
             self.m.s.sg_y2_motor, self.spindle_target_watts)
