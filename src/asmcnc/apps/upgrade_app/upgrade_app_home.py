a3f1b67b6b5a22fd7c04422ed17bf85f9d8660a4
Archie <archiepeterjarvis@gmail.com>
2023-02-01 10:44:17 +0000

    replace o with 0

--- src/asmcnc/apps/upgrade_app/upgrade_app_home.py
+++ src/asmcnc/apps/upgrade_app/upgrade_app_home.py
@@ -1,84 +1,122 @@
+from kivy.clock import Clock
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
-from kivy.clock import Clock
-from asmcnc.apps.upgrade_app.popups.help_popup import HelpPopup
 from asmcnc.apps.upgrade_app.upgrade_complete import UpgradeComplete
 
 Builder.load_string("""
 <UpgradeAppHome>:
-    unlock_code:unlock_code
     error_label:error_label
-    help_button:help_button
-    exit_button:exit_button
+    context_label_1:context_label_1
+    context_label_2:context_label_2
+    qr_code_img:qr_code_img
+    unlock_code_input:unlock_code_input
 
     BoxLayout:
         orientation: 'vertical'
-        spacing: 5
-
-        canvas:
-            Color:
-                rgba: hex('##e5e5e5')
-            Rectangle:
-                size: self.size
-                pos: self.pos
-                
+        
         BoxLayout:
             orientation: 'horizontal'
             size_hint: None, None
-            width: dp(800)
             height: dp(50)
+            width: dp(800)
             
+            canvas:
+                Color:
+                    rgba: hex('#1976d2')
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+            
+            BoxLayout:
+                size_hint: None, None
+                width: dp(50)
+                height: dp(50)
+            
+            Label:
+                text: 'Upgrading to PrecisionPro +'
+                halign: 'center'
+                valign: 'middle'
+                font_size: dp(28)
+                text_size: self.size
+                width: dp(200)
+                height: dp(50)
+                
             ClickableImage:
-                id: exit_button
-                source: "asmcnc/apps/shapeCutter_app/img/exit_cross.png"
+                source: 'asmcnc/apps/shapeCutter_app/img/exit_icon.png'
+                halign: 'right'
                 size_hint: None, None
                 width: dp(50)
                 height: dp(50)
                 on_press: root.exit_app()
-            
-            AnchorLayout:
-                anchor_x: 'right'
-                ClickableImage:
-                    id: help_button
-                    source: "./asmcnc/skavaUI/img/help_btn_orange_round.png"
-                    size_hint: None, None
-                    width: dp(50)
-                    height: dp(50)
-                    on_press: root.open_help()
-        
+                
         BoxLayout:
             orientation: 'vertical'
-            padding: [0,0,0,dp(230)]
+            padding: [0, dp(30), 0, 0]
             
-            Label:
-                text: "This app allows you to upgrade your SmartBench v1.3 [b]PrecisionPro[/b] to a [b]PrecisionPro +[/b].\\nYou will have needed to purchase an upgrade package. For more information on the upgrade,\\n please visit www.yetitool.com/PRODUCTS/upgrades"
-                halign: "center"
-                markup: True
-                color: 0,0,0,1
-                size_hint_y: None
-                font_size: dp(16)
-                height: dp(30)
-        
-            Label:
-                text: "Enter your unlock code:"
-                halign: "center"
-                markup: True
-                color: 0,0,0,1
-                size_hint_y: None
-                id: error_label
-                font_size: dp(16)
+            canvas: 
+                Color:
+                    rgba: hex('f9f9f9ff')
+                Rectangle:
+                    size: self.size
+                    pos: self.pos
+                    
+            BoxLayout:
+                orientation: 'vertical'
+                
+                Label:
+                    text: '1. Plug in your SC2 Spindle motor (both power and data cable)'
+                    color: 0, 0, 0, 1
+                    font_size: dp(16)
+                    
+                Label:
+                    text: '2. Type in your unlock code below'
+                    color: 0, 0, 0, 1
+                    font_size: dp(16)
+                    
+                Label:
+                    text: '3. Press the "Enter" key on the keyboard'
+                    color: 0, 0, 0, 1
+                    font_size: dp(16)
             
             BoxLayout:
                 padding: [dp(200), 0, 0, 0]
+                
                 TextInput:
-                    id: unlock_code
+                    id: unlock_code_input
                     multiline: False
-                    font_size: dp(30)
-                    color: 0,0,0,1  
+                    valign: 'middle'
+                    halign: 'center'
                     size_hint: None, None
+                    font_size: dp(30)
                     height: dp(50)
                     width: dp(400)
-                    on_text_validate: root.check_unlock_code()
+                    on_text_validate: root.on_input_enter()
+            
+            Label:
+                id: error_label
+                text: 'Unlock code incorrect, please check and try again.'
+                color: 1, 0, 0, 1
+                font_size: dp(16)
+                halign: 'center'
+                opacity: 0
+                
+            Label:
+                id: context_label_1
+                text: 'This app allows you to upgrade your SmartBench v1.3 PrecisionPro to a PrecisionPro +.'
+                color: 0, 0, 0, 1
+                halign: 'center'
+                font_size: dp(16)
+                
+            Label:
+                id: context_label_2
+                text: 'You will have needed to purchase an upgrade package.\\nFor more information on the upgrade, please visit www.yetitool.com'
+                color: 0, 0, 0, 1
+                halign: 'center'
+                font_size: dp(16)
+            
+            Image:
+                id: qr_code_img
+                source: 'asmcnc/apps/upgrade_app/img/qr_code.png'
 """)
 
 from kivy.uix.behaviors.button import ButtonBehavior
@@ -90,9 +128,14 @@ class ClickableImage(ButtonBehavior, Image):
         pass
 
 
+def get_correct_unlock_code(serial):
+    try:
+        return str(hex((serial + 42) * 10000))[2:]
+    except TypeError:
+        return 1
+
+
 class UpgradeAppHome(Screen):
-    serial = None
-    valid_unlock_code = None
     activated = False
 
     def __init__(self, **kwargs):
@@ -101,64 +144,84 @@ class UpgradeAppHome(Screen):
         self.sm = kwargs['screen_manager']
         self.m = kwargs['machine']
         self.l = kwargs['localization']
-        self.localize()
-
-    def localize(self):
-        pass
-
-    def open_help(self):
-        HelpPopup()
 
     def exit_app(self):
         self.sm.current = 'lobby'
 
     def on_enter(self):
         self.m.send_any_gcode_command('$51=1')
+        self.unlock_code_input.text = get_correct_unlock_code(1)
 
-    def on_leave(self, *args):
+    def on_leave(self):
         if not self.activated:
             self.m.send_any_gcode_command('$51=0')
 
-    def get_serial_and_calculate_unlock_code(self):
-        self.serial = self.m.s.spindle_serial_number
+    def show_verifying(self):
+        self.error_label.opacity = 0
+        self.context_label_1.text = 'Verifying...'
+        self.context_label_1.font_size = '30dp'
+        self.context_label_2.opacity = 0
+        self.qr_code_img.opacity = 0
 
-        if self.serial is None:
+    def check_unlock_code(self):
+        serial = self.m.s.spindle_serial_number or 1
+
+        if serial is None:
+            self.show_failed_to_get_spindle()
             return
 
-        self.valid_unlock_code = str(hex((self.serial + 42) * 10000))[2:]
+        correct_unlock_code = get_correct_unlock_code(serial)
+        entered_unlock_code = self.unlock_code_input.text.lower().replace('o', '0')
+
+        if correct_unlock_code is None:
+            self.show_failed_to_get_spindle()
+            return
+
+        if correct_unlock_code == entered_unlock_code:
+            self.activate_pro_plus()
+        else:
+            self.show_wrong_code_input()
+
+    def show_wrong_code_input(self):
+        self.error_label.text = 'Unlock code incorrect, please check and try again.'
+        self.error_label.opacity = 1
+        self.context_label_2.opacity = 0
+        self.qr_code_img.opacity = 1
+        self.unlock_code_input.color = [1, 0, 0, 1]
+        self.context_label_1.font_size = '16dp'
+        self.context_label_1.text = "If the problem persists, please submit a support ticket: www.yetitool.com/submit-a-ticket\nSC2 detected. Quote spindle serial number: '%s'" % str(self.m.s.spindle_serial_number)
+
+    def show_failed_to_get_spindle(self):
+        self.error_label.text = 'No SC2 detected. Check your spindle and your connections.'
+        self.error_label.opacity = 1
+        self.context_label_2.opacity = 0
+        self.qr_code_img.opacity = 1
+        self.unlock_code_input.color = [1, 0, 0, 1]
+        self.context_label_1.font_size = '16dp'
+        self.context_label_1.text = "If the problem persists, please submit a support ticket: www.yetitool.com/submit-a-ticket\nSC2 detected. Quote error 'No SC2 detected'."
 
     def create_pro_plus_file(self):
         with open('../../proplus.txt', 'w+'):
             pass
 
-    def go_to_complete_screen(self):
-        if self.sm.has_screen('upgrade_complete'):
-            self.sm.current = 'upgrade_complete'
-            return
-
-        complete_screen = UpgradeComplete(name='upgrade_complete', screen_manager=self.sm,
-                                          machine=self.m, localization=self.l)
-        self.sm.add_widget(complete_screen)
-        self.sm.current = 'upgrade_complete'
+    def add_upgrade_complete_screen(self):
+        if not self.sm.has_screen('upgrade_complete'):
+            upgrade_complete = UpgradeComplete(name='upgrade_complete', screen_manager=self.sm, machine=self.m,
+                                               localization=self.l)
+            self.sm.add_widget(upgrade_complete)
 
     def activate_pro_plus(self):
-        self.m.send_any_gcode_command('$51=1')
+        self.add_upgrade_complete_screen()
+        self.sm.current = 'upgrade_complete'
         self.create_pro_plus_file()
-        self.activated = True
-        self.go_to_complete_screen()
 
-    def verify(self):
-        if self.unlock_code.text.lower() == str(self.valid_unlock_code) and self.valid_unlock_code is not None:
-            self.activate_pro_plus()
-            return
-
-        self.error_label.text = "[color=FF0000]Unlock code incorrect, please check and try again.[/color]\n If the " \
-                                "problem persists, please submit a support ticket: www.yetitool.com/submit-a-ticket " \
-                                "\nquoting spindle serial number: " + str(self.serial)
-
-    def check_unlock_code(self):
+    def get_spindle_serial(self):
         self.m.s.write_command('M3 S0')
-        Clock.schedule_once(lambda dt: self.m.s.write_protocol(self.m.p.GetDigitalSpindleInfo(), "GET DIGITAL SPINDLE INFO"), 0.5)
-        Clock.schedule_once(lambda dt: self.get_serial_and_calculate_unlock_code(), 1)
-        Clock.schedule_once(lambda dt: self.m.s.write_command('M5'), 1.5)
-        Clock.schedule_once(lambda dt: self.verify(), 1.5)
+        Clock.schedule_once(
+            lambda dt: self.m.s.write_protocol(self.m.p.GetDigitalSpindleInfo(), "GET DIGITAL SPINDLE INFO"), 0.2)
+        Clock.schedule_once(lambda dt: self.m.s.write_command('M5'), 0.25)
+        Clock.schedule_once(lambda dt: self.check_unlock_code(), 0.5)
+
+    def on_input_enter(self):
+        self.show_verifying()
+        self.get_spindle_serial()
