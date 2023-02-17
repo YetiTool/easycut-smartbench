e03e5905b869ab5ae9edfc4239ec65ce97dab891
LettyYeti <lettie.adkins@yetitool.com>
2023-02-17 11:04:14 +0000

    fix erronous function call and add true flag to force test

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -2876,8 +2876,8 @@ class RouterMachine(object):
 
         if not self.run_calibration: return
 
-        if not self.state().startswith('Idle') or self.s.write_protocol_buffer:
-            Clock.schedule_once(lambda dt: self.prep_triple_axes_calibration(axis), 0.1)
+        if not self.state().startswith('Idle') or self.s.write_protocol_buffer or True:
+            Clock.schedule_once(lambda dt: self.prep_triple_axes_calibration(), 0.1)
             return
 
         self.calibration_tuning_fail_info = ''
