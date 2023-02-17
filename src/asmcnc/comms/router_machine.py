bb218e5a3af3b6f16a4abbdb5e518843060e1309
LettyYeti <lettie.adkins@yetitool.com>
2023-02-17 11:15:05 +0000

    take out test code

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -2876,8 +2876,7 @@ class RouterMachine(object):
 
         if not self.run_calibration: return
 
-        if not self.state().startswith('Idle') or self.s.write_protocol_buffer or True:
-            log("Resched prep triple axes cal")
+        if not self.state().startswith('Idle') or self.s.write_protocol_buffer:
             Clock.schedule_once(lambda dt: self.prep_triple_axes_calibration(), 0.1)
             return
 
