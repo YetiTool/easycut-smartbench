c5594ae108989a38adad2eaf4cf6f8725e1ac9f1
LettyYeti <lettie.adkins@yetitool.com>
2023-02-24 13:40:09 +0000

    create YP placeholder class, to start adding functions

---
+++ src/asmcnc/job/yetipilot/yetipilot.py
@@ -0,0 +1,32 @@
+import json
+
+class YetiPilot(object):
+
+    ## COMMENTED OUT BC POSSIBLY SUBJECT TO CHANGE
+    # # Algorithm Variables
+    # digital_spindle_mains_voltage = None
+    # bias_for_feed_decrease = 2.0
+    # bias_for_feed_increase = 1.0
+    # m_coefficient = 1.0
+    # c_coefficient = 35.0
+    # cap_for_feed_increase = 20
+    # cap_for_feed_decrease = -40
+    # cap_for_feed_increase_during_z_movement = 0
+    # moving_in_z = False
+
+    # # System Variables
+    # enabled = False
+    # logger = None
+    # counter = 0
+    # statuses_per_adjustment = 2
+    # spindle_load_stack_size = 2
+    # spindle_target_load_watts = 880
+    # digital_spindle_load_stack = []
+    # override_commands_per_adjustment = 2
+    # override_command_delay = 0.06
+    # tolerance_for_acceleration_detection = 50
+
+    def __init__(self, **kwargs):
+        self.m = kwargs['machine']
+        self.sm = kwargs['screen_manager']
+        self.jd = kwargs['job_data']
