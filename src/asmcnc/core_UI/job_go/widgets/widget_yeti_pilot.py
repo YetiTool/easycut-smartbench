c58d2b43e49ae268208ac6a26ea7127fc1014ca5
Archie <archiepeterjarvis@gmail.com>
2023-03-05 17:31:52 +0000

    merge

--- src/asmcnc/core_UI/job_go/widgets/widget_yeti_pilot.py
+++ src/asmcnc/core_UI/job_go/widgets/widget_yeti_pilot.py
@@ -156,7 +156,7 @@ class YetiPilotWidget(Widget):
         PopupYetiPilotSettings(self.sm, self.l, self.m, self.db, self.yp, version=not self.yp.using_advanced_profile, closing_func=self.update_profile_selection)
 
     def update_profile_selection(self, *args):
-        if self.yp.standard_profiles:
+        if self.yp.using_basic_profile:
             self.profile_selection.text = self.yp.material + "; " + self.yp.diameter + " " + self.yp.tool
         else:
             self.profile_selection.text = self.l.get_str("Advanced profile") + ": " + str(self.yp.target_ld) + " W"
