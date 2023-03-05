a0556ed6abe1f4e15951b0d6cccbb4fd87ef2cc5
LettyYeti <lettie.adkins@yetitool.com>
2023-03-05 17:33:37 +0000

    try moving listen for pauses

--- src/asmcnc/skavaUI/screen_go.py
+++ src/asmcnc/skavaUI/screen_go.py
@@ -482,7 +482,6 @@ class GoScreen(Screen):
 
         self.loop_for_job_progress = Clock.schedule_interval(self.poll_for_job_progress, 1)  # then poll repeatedly
         self.loop_for_feeds_and_speeds = Clock.schedule_interval(self.poll_for_feeds_and_speeds, 0.2)  # then poll repeatedly
-        self.listen_for_pauses = Clock.schedule_interval(lambda dt: self.raise_pause_screens_if_paused(), self.POLL_FOR_PAUSE_SCREENS)
         self.yp_widget.switch_reflects_yp()
 
         if self.is_job_started_already:
@@ -667,6 +666,8 @@ class GoScreen(Screen):
         else:
             self._start_running_job()
 
+        self.listen_for_pauses = Clock.schedule_interval(lambda dt: self.raise_pause_screens_if_paused(), self.POLL_FOR_PAUSE_SCREENS)
+
     # Pausing
 
     def _pause_job(self):
@@ -683,7 +684,9 @@ class GoScreen(Screen):
         # For now, this is enough:
 
         if self.m.s.is_job_streaming and self.m.is_machine_paused and self.m.reason_for_machine_pause and self.start_or_pause_button_image.source == "./asmcnc/skavaUI/img/pause.png":
-            if self.listen_for_pauses != None: self.listen_for_pauses.cancel()
+            if self.listen_for_pauses != None: 
+                self.listen_for_pauses.cancel()
+                self.listen_for_pauses = None
             log("RAISE PAUSE SCREEN")
             self.sm.get_screen('spindle_shutdown').reason_for_pause = self.m.reason_for_machine_pause
             self.sm.get_screen('spindle_shutdown').return_screen = "go"
@@ -769,7 +772,9 @@ class GoScreen(Screen):
 
         if self.loop_for_job_progress != None: self.loop_for_job_progress.cancel()
         if self.loop_for_feeds_and_speeds != None: self.loop_for_feeds_and_speeds.cancel()
-        if self.listen_for_pauses != None: self.listen_for_pauses.cancel()
+        if self.listen_for_pauses != None: 
+            self.listen_for_pauses.cancel()
+            self.listen_for_pauses = None
 
     ### SCREEN UPDATES
 
