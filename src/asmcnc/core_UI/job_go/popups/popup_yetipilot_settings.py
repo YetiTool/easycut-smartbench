87ed90fe20c8ad40986bb1d42bff5d6770d4dee1
Archie <archiepeterjarvis@gmail.com>
2023-03-05 11:01:02 +0000

    remove unnecessary imports

--- src/asmcnc/core_UI/job_go/popups/popup_yetipilot_settings.py
+++ src/asmcnc/core_UI/job_go/popups/popup_yetipilot_settings.py
@@ -4,24 +4,22 @@
 Popup for user to choose YetiPilot profiles
 '''
 
-import kivy
-from kivy.graphics import *
+from kivy.clock import Clock
 from kivy.lang import Builder
-from kivy.uix.popup import Popup
+from kivy.metrics import dp
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.floatlayout import FloatLayout
-from kivy.uix.widget import Widget
-from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.image import Image
-from kivy.metrics import dp
-from kivy.uix.spinner import Spinner
-from kivy.clock import Clock
 from kivy.uix.checkbox import CheckBox
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.popup import Popup
+from kivy.uix.spinner import Spinner
+from kivy.uix.widget import Widget
+
 from asmcnc.core_UI.job_go.widgets.widget_load_slider import LoadSliderWidget
-from asmcnc.core_UI.job_go.widgets.widget_settings_version import YPSettingsTypeWidget
 from asmcnc.skavaUI import widget_speed_override
 
 Builder.load_string("""
