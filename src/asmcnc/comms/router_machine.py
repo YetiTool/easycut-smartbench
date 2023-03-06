0aca01543d55cf4b528106a66058d78f2cdecbcc
LettyYeti <lettie.adkins@yetitool.com>
2023-03-06 11:06:25 +0000

    remove pause reason tracking

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -161,11 +161,6 @@ class RouterMachine(object):
         self.TMC_motor[TMC_Y2] = motors.motor_class(TMC_Y2)
         self.TMC_motor[TMC_Z] = motors.motor_class(TMC_Z)
 
-        Clock.schedule_interval(lambda dt: self.print_reason_for_pause(), 0.5)
-
-    def print_reason_for_pause(self):
-        print("REASON FOR PAUSE: " + str(self.reason_for_machine_pause))
-
     # CREATE/DESTROY SERIAL CONNECTION (for cycle app)
     def reconnect_serial_connection(self):
         self.starting_serial_connection = True
