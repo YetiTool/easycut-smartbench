60a5c3f10947e543fe9feef629b0efe799e2fd5f
DennisKiselev <kiselev.dennis@gmail.com>
2023-03-09 21:32:48 +0000

    Add instruction label

--- src/asmcnc/apps/upgrade_app/screen_upgrade.py
+++ src/asmcnc/apps/upgrade_app/screen_upgrade.py
@@ -4,6 +4,8 @@ from kivy.uix.screenmanager import Screen
 Builder.load_string("""
 <UpgradeScreen>:
 
+    instruction_label:instruction_label
+
     BoxLayout:
         orientation: 'vertical'
 
@@ -24,7 +26,7 @@ Builder.load_string("""
                     text: 'Upgrade SB V1.3 to PrecisionPro +'
                     halign: 'center'
                     valign: 'middle'
-                    font_size: dp(24)
+                    font_size: dp(30)
                     text_size: self.size
 
             BoxLayout:
@@ -61,6 +63,22 @@ Builder.load_string("""
                     size: self.size
                     pos: self.pos
 
+            BoxLayout:
+                orientation: 'vertical'
+
+                Label:
+                    id: instruction_label
+                    size_hint_y: 2
+                    font_size: dp(20)
+                    color: 0,0,0,1
+                    halign: 'center'
+                    valign: 'middle'
+                    text_size: self.size
+
+                TextInput
+
+            BoxLayout
+
 """)
 
 class UpgradeScreen(Screen):
@@ -72,5 +90,15 @@ class UpgradeScreen(Screen):
         self.m = kwargs['machine']
         self.l = kwargs['localization']
 
+    def on_pre_enter(self):
+        self.update_strings()
+
     def quit_to_lobby(self):
         self.sm.current = 'lobby'
+
+    def update_strings(self):
+        self.instruction_label.text = (
+            '1. ' + self.l.get_str('Plug in your SC2 Spindle motor (both power and data cable)') + '\n' + \
+            '2. ' + self.l.get_str('Type in your upgrade code below') + '\n' + \
+            '3. ' + self.l.get_str('Press "Enter" on the keyboard')
+        )
