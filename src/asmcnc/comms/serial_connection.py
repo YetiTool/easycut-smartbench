e530fccb6ebb9cbed4af7e9c7261278b6a142d4f
LettyYeti <lettie.adkins@yetitool.com>
2023-03-09 14:39:34 +0000

    plan to add add_to_stack func to serial comms, but this is not finished or tested yet

--- src/asmcnc/comms/serial_connection.py
+++ src/asmcnc/comms/serial_connection.py
@@ -380,7 +380,16 @@ class SerialConnection(object):
     
                 # Job streaming: stuff buffer
                 if (self.is_job_streaming and not self.m.is_machine_paused and not "Alarm" in self.m.state()):
-                    if self.yp.use_yp: self.yp.add_to_stack()
+                    if self.yp.use_yp: 
+
+                        if  self.digital_spindle_ld_qdA is not None \
+                            and self.feed_override_percentage is not None \
+                            and self.feed_rate is not None \
+                            and self.grbl_ln is not None:
+
+                            self.yp.add_to_stack(self.digital_spindle_ld_qdA, self.feed_override_percentage,
+                                                 self.feed_rate, self.grbl_ln, self.digital_spindle_mains_voltage)
+
                     if self.is_stream_lines_remaining:
                         self.stuff_buffer()
                     else: 
