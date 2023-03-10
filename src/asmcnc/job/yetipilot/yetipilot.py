162a8f052570cbe77efd557f0b03031a5a1a2abc
Archie <archiepeterjarvis@gmail.com>
2023-03-10 17:47:17 +0000

    use grbl mode tracker

--- src/asmcnc/job/yetipilot/yetipilot.py
+++ src/asmcnc/job/yetipilot/yetipilot.py
@@ -159,8 +159,8 @@ class YetiPilot(object):
                 self.digital_spindle_load_stack[-self.spindle_load_stack_size:]) / self.spindle_load_stack_size
 
             constant_feed, gcode_feed = self.m.get_is_constant_feed_rate(
-                self.jd.scrape_last_feed_command(self.jd.job_gcode_running, current_line_number),
-                feed_override_percentage, feed_rate, self.tolerance_for_acceleration_detection)
+                self.jd.grbl_mode_tracker[0][1], feed_override_percentage, feed_rate,
+                self.tolerance_for_acceleration_detection)
 
             adjustment, raw_multiplier, capped_multiplier = self.calculate_adjustment(average_digital_spindle_load,
                                                                                       constant_feed)
