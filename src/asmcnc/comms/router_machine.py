9864fcb4e0b5c0fc3aefa4774b646739cba864a7
Archie <archiepeterjarvis@gmail.com>
2023-03-24 13:18:49 +0000

    add fake screens

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -3548,6 +3548,7 @@ class RouterMachine(object):
         self.s.spindle_health_check_data[:] = []
 
         shc_screen = TestScreen(name='shc', sm=self.sm)
+        self.sm.add_widget(shc_screen)
         shc_screen.set_return(self.sm.current)
         shc_screen.set_text("Spindle Health Check In Progress")
 
@@ -3558,8 +3559,10 @@ class RouterMachine(object):
             if average_load_w > 400:
                 log("Load too high for spindle health check: " + str(average_load_w) + "W")
                 shc_failed_screen = TestScreen(name='shc_failed', sm=self.sm)
+                self.sm.add_widget(shc_failed_screen)
                 shc_failed_screen.set_return(self.sm.current)
                 shc_failed_screen.set_text("Spindle Health Check Failed: " + str(average_load_w) + "W")
+                self.sm.current = 'shc_failed'
                 return
 
             if self.sm.has_screen('go'):
