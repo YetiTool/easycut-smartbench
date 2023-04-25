79e00f4caa11b09ce66f72b02d86adf710478ed1
Archie <archiepeterjarvis@gmail.com>
2023-04-25 16:08:11 +0100

    correct for 110v

--- src/asmcnc/production/z_head_qc_jig/z_head_qc_2.py
+++ src/asmcnc/production/z_head_qc_jig/z_head_qc_2.py
@@ -294,7 +294,7 @@ class ZHeadQC2(Screen):
             if self.m.spindle_voltage == 110:
                 spindle_rpm = self.m.convert_from_110_to_230(spindle_rpm)
 
-            if not(8000 <= spindle_rpm <= 12000):
+            if spindle_rpm < 8000 or spindle_rpm > 12000:
                 fail_report.append("Spindle RPM was " + str(self.m.spindle_speed()) + ". Should be 8000-12000")
 
             self.m.s.write_command('M5')
