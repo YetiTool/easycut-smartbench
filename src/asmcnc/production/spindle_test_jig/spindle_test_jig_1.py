00ccd0f044d5dd07fe1c807ae39f4f463c07570f
Benji Pestell <benji.pestell@yetitool.com>
2023-05-22 16:13:36 +0100

    add $$

--- src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
+++ src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
@@ -334,13 +334,16 @@ class SpindleTestJig1(Screen):
 
     def switch_spindle_type(self):
         self.spindle_type_button.text = "Configuring GRBL... "
+
         self.setting_51 = self.get_spindle_type()
         if self.setting_51 == "SC1":
             Clock.schedule_once(lambda dt: self.m.s.write_command('$51 = 1'), 1)
         else:
-            Clock.schedule_once(lambda dt: self.m.s.write_command('$51 = 0'), 1)        
+            Clock.schedule_once(lambda dt: self.m.s.write_command('$51 = 0'), 1)
 
-        Clock.schedule_once(lambda dt: self.update_spindle_type_text(), 2)
+        Clock.schedule_once(lambda dt: self.s.m.write_command('$$'), 1.5)        
+
+        Clock.schedule_once(lambda dt: self.update_spindle_type_text(), 3)
 
     def update_spindle_type_text(self):
         self.spindle_type_button.text = "Spindle type: " + self.get_spindle_type()
