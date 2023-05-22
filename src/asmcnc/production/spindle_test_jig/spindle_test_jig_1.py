5e086cb8ebb012a2fd235b0ad8c8b95537540698
Benji Pestell <benji.pestell@yetitool.com>
2023-05-22 13:55:25 +0100

    stop test on spindle change

--- src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
+++ src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
@@ -333,6 +333,7 @@ class SpindleTestJig1(Screen):
         self.test.run()
 
     def switch_spindle_type(self):
+        self.stop()
         self.spindle_type_button.text = "Configuring GRBL... "
         setting_51 = int(self.m.get_dollar_setting(51))
 
