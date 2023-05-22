e43437f74809c874a1e9ff8bad38faa073000582
Benji Pestell <benji.pestell@yetitool.com>
2023-05-22 11:22:41 +0100

    Fix soft reset

--- src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
+++ src/asmcnc/production/spindle_test_jig/spindle_test_jig_1.py
@@ -338,7 +338,7 @@ class SpindleTestJig1(Screen):
         value_to_set = int(not setting_51)
 
         Clock.schedule_once(lambda dt: self.m.s.write_command('$51 = ' + str(value_to_set)), 1)
-        Clock.schedule_once(lambda dt: self.m.s._grbl_soft_reset(), 1)
+        Clock.schedule_once(lambda dt: self.m.s.write_realtime("\x18", altDisplayText = 'Soft reset'), 1)
 
         print(setting_51, value_to_set)
 
