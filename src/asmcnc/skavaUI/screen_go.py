39adab34cbbc7c1b0e838cbf7ec7c7812f227097
Archie <archiepeterjarvis@gmail.com>
2023-05-24 12:08:14 +0100

    switch conditions

--- src/asmcnc/skavaUI/screen_go.py
+++ src/asmcnc/skavaUI/screen_go.py
@@ -701,10 +701,10 @@ class GoScreen(Screen):
                 self._start_running_job()
 
                 if not self.m.has_spindle_health_check_passed():
-                    if not self.m.is_spindle_health_check_active():
-                        self.disabled_yp_widget.set_version(DisabledYPCase.DISABLED)
-                    else:
+                    if self.m.is_spindle_health_check_active():
                         self.disabled_yp_widget.set_version(DisabledYPCase.FAILED)
+                    else:
+                        self.disabled_yp_widget.set_version(DisabledYPCase.DISABLED)
 
         self.listen_for_pauses = Clock.schedule_interval(lambda dt: self.raise_pause_screens_if_paused(), self.POLL_FOR_PAUSE_SCREENS)
 
