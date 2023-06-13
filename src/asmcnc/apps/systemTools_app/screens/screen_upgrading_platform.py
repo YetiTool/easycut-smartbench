50661649b536f86fea7410114af1eb916ae21194
Archie <archiepeterjarvis@gmail.com>
2023-06-13 15:13:08 +0100

    Add popup

--- src/asmcnc/apps/systemTools_app/screens/screen_upgrading_platform.py
+++ src/asmcnc/apps/systemTools_app/screens/screen_upgrading_platform.py
@@ -22,21 +22,15 @@ Builder.load_string("""
         
         Label:
             text: "Upgrading Console Platform"
-            font_size: 30
+            font_size: dp(30)
             color: 0, 0, 0, 1
             bold: True
             
         Label:
             text: "DO NOT POWER OFF YOUR CONSOLE"
             color: 1, 0, 0, 1
-            font_size: 30
+            font_size: dp(30)
             bold: True
-            
-        Label:
-            text: "Once the upgrade is complete, your console will automatically restart."
-            color: 0, 0, 0, 1
-            font_size: 26
-        
 """)
 
 
@@ -45,7 +39,6 @@ Builder.load_string("""
 
 class ScreenUpgradingPlatform(Screen):
     upgrade_in_progress = False
-    reboot_required = True
 
     def __init__(self, **kwargs):
         super(ScreenUpgradingPlatform, self).__init__(**kwargs)
@@ -62,9 +55,6 @@ class ScreenUpgradingPlatform(Screen):
     def set_upgrade_in_progress(self, value):
         self.upgrade_in_progress = value
 
-    def reboot(self):
-        subprocess.call('sudo reboot', shell=True)
-
     def start_upgrade(self):
         self.set_upgrade_in_progress(True)
         self.clean_up()
@@ -75,10 +65,74 @@ class ScreenUpgradingPlatform(Screen):
 
         process.wait()
 
-        if process.returncode == 0:
-            if self.reboot_required:
-                Clock.schedule_once(lambda dt: self.reboot(), 5)
-        else:
-            pass
-            # TODO: Implement error handling
 
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
+
+
+class UpgradePlatformPopup(Popup):
+    return_code = None
+
+    def __init__(self, return_code, **kwargs):
+        super(UpgradePlatformPopup, self).__init__(**kwargs)
+        self.systemtools_sm = kwargs['system_tools']
+        self.l = kwargs['localization']
+        self.return_code = return_code
+
+        description = self.l.get_str(
+            "The platform upgrade has completed successfully installed. Your console will automatically reboot in 30 seconds.") \
+            if self.return_code == 1 else self.l.get_str(
+            "The platform upgrade has failed. Please check your WiFi connection and try again later.")
+        title_string = self.l.get_str('Platform Upgrade')
+        ok_string = self.l.get_bold('Reboot Now') if self.return_code == 1 else self.l.get_bold('Ok')
+
+        label = Label(size_hint_y=2, text_size=(320, None), halign='center', valign='middle', text=description,
+                      color=[0, 0, 0, 1], padding=[0, 0], markup=True)
+
+        ok_button = Button(text=ok_string, markup=True)
+        ok_button.background_normal = ''
+        ok_button.background_color = [76 / 255., 175 / 255., 80 / 255., 1.]
+
+        btn_layout = BoxLayout(orientation='horizontal', spacing=15, padding=[0, 5, 0, 0])
+        btn_layout.add_widget(ok_button)
+
+        layout_plan = BoxLayout(orientation='vertical', spacing=10, padding=[30, 20, 30, 0])
+        layout_plan.add_widget(label)
+        layout_plan.add_widget(btn_layout)
+
+        popup = Popup(title=title_string,
+                      title_color=[0, 0, 0, 1],
+                      title_font='Roboto-Bold',
+                      title_size='20sp',
+                      content=layout_plan,
+                      size_hint=(None, None),
+                      size=(360, 360),
+                      auto_dismiss=False
+                      )
+
+        popup.separator_color = [230 / 255., 74 / 255., 25 / 255., 1.]
+        popup.separator_height = '4dp'
+        popup.background = './asmcnc/apps/shapeCutter_app/img/popup_background.png'
+
+        if self.return_code == 1:
+            ok_button.bind(on_press=self.reboot)
+        else:
+            ok_button.bind(on_press=self.dismiss)
+
+        if self.return_code == 1:
+            Clock.schedule_once(lambda dt: self.reboot(), 30)
+
+        popup.open()
+
+    def reboot(self):
+        subprocess.call('sudo reboot', shell=True)
+
+    def on_open(self):
+        if self.return_code == 1:
+            Clock.schedule_once(lambda dt: self.reboot(), 30)
+
+    def dismiss(self):
+        self.systemtools_sm.current = 'screen_system_tools'
+        super(UpgradePlatformPopup, self).dismiss()
