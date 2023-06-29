3f7d670212521c0ba1667ff40704f85e553b6db3
Benji Pestell <benji.pestell@yetitool.com>
2023-06-29 12:09:09 +0100

    debug

--- src/asmcnc/comms/serial_connection.py
+++ src/asmcnc/comms/serial_connection.py
@@ -1149,9 +1149,9 @@ class SerialConnection(object):
                             return
 
                         self.digital_spindle_ld_qdA = int(digital_spindle_feedback[0])
-                        try:
+                        if self.digital_spindle_ld_qdA > 0:
                             self.digital_spindle_ld_W = math.sqrt(self.digital_spindle_ld_W)*self.digital_spindle_mains_voltage*0.1
-                        except:                        
+                        else:                        
                             self.digital_spindle_ld_W = None
                         print(self.digital_spindle_ld_W)
                         self.digital_spindle_temperature = int(digital_spindle_feedback[1])
