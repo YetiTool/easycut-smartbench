84ee802da127c51d712bbd31e90937cf6bf53b39
LettyYeti <lettie.adkins@yetitool.com>
2023-07-19 11:01:09 +0100

    mod start easycut script

--- ansible/templates/starteasycut.sh
+++ ansible/templates/starteasycut.sh
@@ -1,64 +1,68 @@
 #!/bin/bash
-echo "start easycut"
+echo "kivy to svg test app"
 
-# start the pigpio daemon
-sudo pigpiod
-
-# If easycut-smartbench directory doesn't exist, copy from backup (if available)
-if [ ! -d "/home/pi/easycut-smartbench/" -a -d  "/home/pi/easycut-smartbench-backup" ]
-then
-        echo "no easycut folder found! copying files from backup folder"
-        mkdir /home/pi/easycut-smartbench && cp -RT /home/pi/easycut-smartbench-backup /home/pi/easycut-smartbench
-fi
-
-# Run all apps from src folder
 cd /home/pi/easycut-smartbench/src/
+exec python kivy_to_svg_test.py
 
-if compgen -G "/home/pi/YETI_*_PROD_JIG.txt" > /dev/null; then
 
-        if [ -f /home/pi/YETI_ZHEADQC_PROD_JIG.txt ]
-        then
-                echo "Running Z Head QC app"
-                exec python z_head_qc_app.py
+# # start the pigpio daemon
+# sudo pigpiod
 
-        elif [ -f /home/pi/YETI_ZHEADUPDOWN_PROD_JIG.txt ]
-        then
-                echo "Running Z Head Up Down app"
-                exec python z_head_mechanics_app.py
+# # If easycut-smartbench directory doesn't exist, copy from backup (if available)
+# if [ ! -d "/home/pi/easycut-smartbench/" -a -d  "/home/pi/easycut-smartbench-backup" ]
+# then
+#         echo "no easycut folder found! copying files from backup folder"
+#         mkdir /home/pi/easycut-smartbench && cp -RT /home/pi/easycut-smartbench-backup /home/pi/easycut-smartbench
+# fi
 
-        elif [ -f /home/pi/YETI_LBQC_PROD_JIG.txt ]
-        then
-                echo "Running LB QC app"
-                exec python lower_beam_qc_app.py
+# # Run all apps from src folder
+# cd /home/pi/easycut-smartbench/src/
 
-        elif [ -f /home/pi/YETI_LBCAL_PROD_JIG.txt ]
-        then
-                echo "Running LB Calibration app"
-                exec python lb_calibration_app.py
+# if compgen -G "/home/pi/YETI_*_PROD_JIG.txt" > /dev/null; then
 
-        fi
+#         if [ -f /home/pi/YETI_ZHEADQC_PROD_JIG.txt ]
+#         then
+#                 echo "Running Z Head QC app"
+#                 exec python z_head_qc_app.py
 
-else
+#         elif [ -f /home/pi/YETI_ZHEADUPDOWN_PROD_JIG.txt ]
+#         then
+#                 echo "Running Z Head Up Down app"
+#                 exec python z_head_mechanics_app.py
+
+#         elif [ -f /home/pi/YETI_LBQC_PROD_JIG.txt ]
+#         then
+#                 echo "Running LB QC app"
+#                 exec python lower_beam_qc_app.py
+
+#         elif [ -f /home/pi/YETI_LBCAL_PROD_JIG.txt ]
+#         then
+#                 echo "Running LB Calibration app"
+#                 exec python lb_calibration_app.py
+
+#         fi
+
+# else
         
-        # execute python
-        exec python main.py
-        sleep 5
+#         # execute python
+#         exec python main.py
+#         sleep 5
 
-        # check if running
-        echo "check main.py running"
-        if ! ps ax | grep "[p]ython main.py"
-        then
-                echo "no main instance found - trying git reset"
-                git reset --hard
-                exec python main.py
-                if ! ps ax | grep "[p]ython main.py"
-                then
-                        if [ -d  "/home/pi/easycut-smartbench-backup" ]
-                        then
-                                echo "force copy of easycut-smartbench-backup back into easycut-smartbench"
-                                cp -RTf /home/pi/easycut-smartbench-backup /home/pi/easycut-smartbench
-                                exec python main.py
-                        fi
-                fi
-        fi
-fi
+#         # check if running
+#         echo "check main.py running"
+#         if ! ps ax | grep "[p]ython main.py"
+#         then
+#                 echo "no main instance found - trying git reset"
+#                 git reset --hard
+#                 exec python main.py
+#                 if ! ps ax | grep "[p]ython main.py"
+#                 then
+#                         if [ -d  "/home/pi/easycut-smartbench-backup" ]
+#                         then
+#                                 echo "force copy of easycut-smartbench-backup back into easycut-smartbench"
+#                                 cp -RTf /home/pi/easycut-smartbench-backup /home/pi/easycut-smartbench
+#                                 exec python main.py
+#                         fi
+#                 fi
+#         fi
+# fi
