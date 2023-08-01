a3cad1688001775d9b297dd851aa863b9ac6e203
Archie <archiepeterjarvis@gmail.com>
2023-08-01 15:51:08 +0100

    cast to int

--- src/asmcnc/skavaUI/screen_go.py
+++ src/asmcnc/skavaUI/screen_go.py
@@ -808,14 +808,14 @@ class GoScreen(Screen):
         if abs(self.speedOverride.speed_override - 100) > abs(
             self.spindle_speed_max_percentage - 100):
             self.spindle_speed_max_percentage = (self.speedOverride.speed_override)
-        if (self.speedOverride.spindle_rpm.text > self.
+        if (int(self.speedOverride.spindle_rpm.text) > self.
             spindle_speed_max_absolute):
             self.spindle_speed_max_absolute = (self.speedOverride.
                 spindle_rpm.text)
         if abs(self.feedOverride.feed_override - 100) > abs(
             self.feed_rate_max_percentage - 100):
             self.feed_rate_max_percentage = (self.feedOverride.feed_override)
-        if self.feedOverride.feed_absolute.text > self.feed_rate_max_absolute:
+        if int(self.feedOverride.feed_absolute.text) > self.feed_rate_max_absolute:
             self.feed_rate_max_absolute = self.feedOverride.feed_absolute.text
 
     def update_overload_label(self, state):
