a8e36af4a0e8064c7ca3284f8f640b028610b070
DennisKiselev <kiselev.dennis@gmail.com>
2023-09-13 16:43:43 +0100

    Add python libraries to geberit setup

--- src/geberit_setup.sh
+++ src/geberit_setup.sh
@@ -1,7 +1,12 @@
+sudo pip install svgwrite
+sudo apt-get install python-scipy
+sudo pip install svgpathtools
+
 git clone https://github.com/sameer/svg2gcode
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 . "$HOME/.cargo/env"
 sudo ln -s /home/pi/.cargo/bin/cargo /usr/bin/cargo
+
 cd svg2gcode
 cargo add svg2gcode
 cargo run --release -- examples/Vanderbilt_Commodores_logo.svg --off M3 --on M5 -o out.gcode
