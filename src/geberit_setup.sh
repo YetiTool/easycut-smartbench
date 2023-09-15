54cdc71147fb2f267c1535eff68fb50540c1e3d6
DennisKiselev <kiselev.dennis@gmail.com>
2023-09-15 17:03:13 +0100

    Rig testing

--- src/geberit_setup.sh
+++ src/geberit_setup.sh
@@ -1,5 +1,6 @@
 git clone https://github.com/sameer/svg2gcode
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+export PATH="$HOME/.cargo/bin:$PATH"
 . "$HOME/.cargo/env"
 sudo ln -s /home/pi/.cargo/bin/cargo /usr/bin/cargo
 cd svg2gcode
