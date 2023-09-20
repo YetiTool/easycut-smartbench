6f150a0f0a6a5108dd8e68910614d963ac6a1618
Archie <archiepeterjarvis@gmail.com>
2023-09-20 15:00:30 +0100

    if last_modal_feed_rate is None, not constant speed

--- src/asmcnc/comms/router_machine.py
+++ src/asmcnc/comms/router_machine.py
@@ -1533,6 +1533,9 @@ class RouterMachine(object):
     def feed_rate(self): return int(self.s.feed_rate)
 
     def get_is_constant_feed_rate(self, last_modal_feed_rate, feed_override_percentage, current_feed_rate, tolerance_for_acceleration_detection):
+        if last_modal_feed_rate is None:
+            return False, None
+
         constant_feed_target = last_modal_feed_rate * feed_override_percentage / 100
         return abs(constant_feed_target - current_feed_rate) <= tolerance_for_acceleration_detection, last_modal_feed_rate
 
