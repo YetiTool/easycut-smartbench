adf5bfd7f9df84c2c5d783849fe8fc326386f11b
DennisKiselev <kiselev.dennis@gmail.com>
2023-09-20 13:36:33 +0100

    Rig testing

--- src/geberit_setup.sh
+++ src/geberit_setup.sh
@@ -4,6 +4,7 @@ git clone https://github.com/sameer/svg2gcode
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
 sudo ln -s /home/pi/.cargo/bin/cargo /usr/bin/cargo
+rustup default stable
 cd svg2gcode
 cargo add svg2gcode
 cargo run --release -- examples/Vanderbilt_Commodores_logo.svg --off M3 --on M5 -o out.gcode
