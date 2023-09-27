0b9b474d1730ec45743e58e77b2915f995037122
VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>
2023-09-27 15:29:42 +0100

    Updated message

--- src/asmcnc/apps/wifi_app/screen_wifi.py
+++ src/asmcnc/apps/wifi_app/screen_wifi.py
@@ -620,7 +620,7 @@ class WifiScreen(Screen):
                     Clock.unschedule(self.wifi_error_timeout_event)
                 try:
                     wait_popup.popup.dismiss()
-                    message = self.l.get_str("WiFi connected successfully!")
+                    message = self.l.get_str("Wifi connected succesfully!")
                     popup_info.PopupMiniInfo(self.sm, self.l, message)
                 except:
                     pass
