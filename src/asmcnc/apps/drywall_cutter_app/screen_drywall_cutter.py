2a751e70993b5827cf11508006330b38a32edeb4
DennisKiselev <kiselev.dennis@gmail.com>
2023-10-18 22:04:24 +0100

    Add general layout to screen

--- src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
+++ src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
@@ -6,9 +6,101 @@ from kivy.uix.screenmanager import Screen
 Builder.load_string("""
 <DrywallCutterScreen>:
 
-    Button:
-        on_press: root.quit_to_lobby()
-        text: 'Quit'
+    BoxLayout:
+        orientation: 'vertical'
+
+        BoxLayout:
+            orientation: 'horizontal'
+            padding: dp(5)
+            spacing: dp(10)
+
+            Button:
+                size_hint_x: 7
+                text: 'Home'
+
+            Button:
+                size_hint_x: 7
+                text: 'File'
+
+            Button:
+                size_hint_x: 7
+                text: 'Tool'
+
+            Button:
+                size_hint_x: 7
+                text: 'Shape'
+
+            Button:
+                size_hint_x: 7
+                text: 'Rotate'
+
+            Button:
+                size_hint_x: 7
+                text: 'Cut on line'
+                text_size: self.size
+                halign: 'center'
+                valign: 'middle'
+
+            Button:
+                size_hint_x: 7
+                text: 'Material setup'
+                text_size: self.size
+                halign: 'center'
+                valign: 'middle'
+
+            Button:
+                size_hint_x: 15
+                text: 'STOP'
+
+            Button:
+                size_hint_x: 7
+                on_press: root.quit_to_lobby()
+                text: 'Quit'
+
+        BoxLayout:
+            size_hint_y: 5
+            orientation: 'horizontal'
+            padding: dp(5)
+            spacing: dp(10)
+
+            BoxLayout:
+                size_hint_x: 55
+
+                canvas:
+                    Color:
+                        rgba: hex('#E5E5E5FF')
+                    Rectangle:
+                        size: self.size
+                        pos: self.pos
+
+            BoxLayout:
+                size_hint_x: 23
+                orientation: 'vertical'
+                spacing: dp(10)
+
+                BoxLayout:
+                    size_hint_y: 31
+
+                    canvas:
+                        Color:
+                            rgba: hex('#E5E5E5FF')
+                        Rectangle:
+                            size: self.size
+                            pos: self.pos
+
+                BoxLayout:
+                    size_hint_y: 7
+                    orientation: 'horizontal'
+                    spacing: dp(10)
+
+                    Button:
+                        text: 'Simulate'
+
+                    Button:
+                        text: 'Save'
+
+                    Button:
+                        text: 'Run'
 
 """)
 
