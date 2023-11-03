c4e5c2babdbc50d11ef32b9d6b5e2f811068c8f1
Benji Pestell <benji.pestell@yetitool.com>
2023-11-03 16:01:03 +0000

    typo

--- src/asmcnc/apps/drywall_cutter_app/engine.py
+++ src/asmcnc/apps/drywall_cutter_app/engine.py
@@ -22,9 +22,9 @@ from io import open
 import os
 import re
 
-from config import config_loader
-
 config = config_loader.DWTConfig()
+
+from config import config_loader
 
 active_config = config.active_config
 active_cutter = config.active_cutter
