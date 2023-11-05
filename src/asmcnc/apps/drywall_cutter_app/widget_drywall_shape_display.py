872a7a3eada9588933913a763c8db8521b4340e2
DennisKiselev <kiselev.dennis@gmail.com>
2023-11-05 23:33:20 +0000

    Add x and y inputs

--- src/asmcnc/apps/drywall_cutter_app/widget_drywall_shape_display.py
+++ src/asmcnc/apps/drywall_cutter_app/widget_drywall_shape_display.py
@@ -10,6 +10,8 @@ Builder.load_string("""
     d_input:d_input
     l_input:l_input
     r_input:r_input
+    x_input:x_input
+    y_input:y_input
 
     BoxLayout:
         size: self.parent.size
@@ -99,6 +101,48 @@ Builder.load_string("""
                     pos: self.parent.pos
                     background_color: (0,0,0,0)
 
+            BoxLayout:
+                size: dp(70), dp(40)
+                size_hint: (None, None)
+
+                canvas.before:
+                    Color:
+                        rgba: hex('#E5E5E5FF')
+                    Rectangle:
+                        pos: self.x + 5, self.y + 5
+                        size: self.width - 10, self.height - 10
+
+                TextInput:
+                    id: x_input
+                    font_size: dp(25)
+                    halign: 'center'
+                    input_filter: 'int'
+                    multiline: False
+                    size: self.parent.size
+                    pos: self.parent.pos
+                    background_color: (0,0,0,0)
+
+            BoxLayout:
+                size: dp(70), dp(40)
+                size_hint: (None, None)
+
+                canvas.before:
+                    Color:
+                        rgba: hex('#E5E5E5FF')
+                    Rectangle:
+                        pos: self.x + 5, self.y + 5
+                        size: self.width - 10, self.height - 10
+
+                TextInput:
+                    id: y_input
+                    font_size: dp(25)
+                    halign: 'center'
+                    input_filter: 'int'
+                    multiline: False
+                    size: self.parent.size
+                    pos: self.parent.pos
+                    background_color: (0,0,0,0)
+
 """)
 
 
@@ -114,6 +158,8 @@ class DrywallShapeDisplay(Widget):
         self.d_input.bind(text=self.d_input_change)
         self.l_input.bind(text=self.l_input_change)
         self.r_input.bind(text=self.r_input_change)
+        self.x_input.bind(text=self.x_input_change)
+        self.y_input.bind(text=self.y_input_change)
 
     def select_shape(self, shape, rotation):
         image_source = self.image_filepath + shape
@@ -133,22 +179,39 @@ class DrywallShapeDisplay(Widget):
             self.d_input.opacity = 0
             self.d_input.parent.opacity = 0
 
-
         if shape in ['square', 'rectangle']:
             self.r_input.disabled = False
             self.r_input.opacity = 1
             self.r_input.parent.opacity = 1
+            self.x_input.disabled = False
+            self.x_input.opacity = 1
+            self.x_input.parent.opacity = 1
+            self.y_input.disabled = False
+            self.y_input.opacity = 1
+            self.y_input.parent.opacity = 1
             if shape == 'square':
                 self.r_input.parent.pos = (421, 311)
+                self.x_input.parent.pos = (86, 175)
+                self.y_input.parent.pos = (248, 327)
             else:
                 if rotation == 'horizontal':
                     self.r_input.parent.pos = (463, 311)
+                    self.x_input.parent.pos = (43, 175)
+                    self.y_input.parent.pos = (248, 327)
                 else:
                     self.r_input.parent.pos = (419, 333)
+                    self.x_input.parent.pos = (98, 155)
+                    self.y_input.parent.pos = (248, 331)
         else:
             self.r_input.disabled = True
             self.r_input.opacity = 0
             self.r_input.parent.opacity = 0
+            self.x_input.disabled = True
+            self.x_input.opacity = 0
+            self.x_input.parent.opacity = 0
+            self.y_input.disabled = True
+            self.y_input.opacity = 0
+            self.y_input.parent.opacity = 0
 
         if shape == 'line':
             self.l_input.disabled = False
@@ -184,3 +247,9 @@ class DrywallShapeDisplay(Widget):
 
     def r_input_change(self, instance, value):
         self.dwt_config.on_parameter_change('canvas_shape_dims.r', float(value or 0))
+
+    def x_input_change(self, instance, value):
+        self.dwt_config.on_parameter_change('canvas_shape_dims.x', float(value or 0))
+
+    def y_input_change(self, instance, value):
+        self.dwt_config.on_parameter_change('canvas_shape_dims.y', float(value or 0))
