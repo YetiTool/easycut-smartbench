58a6c9d6e886c95b4600d5ad4a0b86489570cd16
Benji Pestell <benji.pestell@yetitool.com>
2023-11-15 12:01:26 +0000

    attempt fix

--- src/asmcnc/apps/drywall_cutter_app/engine.py
+++ src/asmcnc/apps/drywall_cutter_app/engine.py
@@ -745,7 +745,7 @@ class GCodeEngine():
             raise Exception("Shape type: '%s' not supported" % self.config.active_config.shape_type)
         
         if simulate:
-            self.m.s.run_skeleton_buffer_stuffer(["G90"].append(cutting_lines[1:]))
+            self.m.s.run_skeleton_buffer_stuffer(cutting_lines.insert(0, "G90"))
         else:
             file_structure_1_shapes = ["rectangle", "square", "circle", "line"]
            
