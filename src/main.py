4e3ee8ca0abc0d753b9f1eb4779b1e800569ee8f
archiepeterjarvis <archiepeterjarvis@gmail.com>
2023-11-15 10:42:43 +0000

    add black bars

--- src/main.py
+++ src/main.py
@@ -25,6 +25,8 @@ from os import path
 
 from kivy.config import Config
 from kivy.clock import Clock
+from kivy.uix.boxlayout import BoxLayout
+
 Config.set('kivy', 'keyboard_mode', 'systemanddock')
 
 if sys.platform.startswith("linux"):
@@ -152,7 +154,7 @@ class SkavaUI(App):
     test_no = 0
 
     width = Window.width
-    height = Window.height
+    height = Window.height if Window.height == 480 else Window.height - 32
 
     def build(self):
 
@@ -304,6 +306,23 @@ class SkavaUI(App):
         # Clock.schedule_once(start_loop, 10)
 
         ## -----------------------------------------------------------------------------------
+
+        if self.height == 768:
+            root = BoxLayout(orientation='vertical')
+
+            top_bar = BoxLayout(size_hint=(1, None), height=16, background_color=(0, 0, 0, 1))
+            root.add_widget(top_bar)
+
+            main_content = BoxLayout(orientation='vertical')
+            main_content.add_widget(sm)
+            root.add_widget(main_content)
+
+            bottom_bar = BoxLayout(size_hint=(1, None), height=16, background_color=(0, 0, 0, 1))
+            root.add_widget(bottom_bar)
+
+            self.root = root
+            return root
+
         return sm
 
 if __name__ == '__main__':
