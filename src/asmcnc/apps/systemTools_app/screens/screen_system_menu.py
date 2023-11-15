28838eab0e43dddcf457e5604a182bc5d0477f50
archiepeterjarvis <archiepeterjarvis@gmail.com>
2023-11-15 14:38:53 +0000

    run script

--- src/asmcnc/apps/systemTools_app/screens/screen_system_menu.py
+++ src/asmcnc/apps/systemTools_app/screens/screen_system_menu.py
@@ -1,21 +1,18 @@
-# -*- coding: utf-8 -*-
-'''
+"""
 Created on 18 November 2020
 Menu screen for system tools app
 
 @author: Letty
-'''
-
+"""
 from kivy.lang import Builder
 from kivy.factory import Factory
 from kivy.uix.screenmanager import ScreenManager, Screen
 import sys
 from kivy.clock import Clock
-
 from asmcnc.skavaUI import popup_info
 from asmcnc.apps.systemTools_app.screens import popup_system
-
-Builder.load_string("""
+Builder.load_string(
+    """
 
 <SystemMenuScreen>
 
@@ -41,8 +38,8 @@ Builder.load_string("""
     GridLayout:
         size: self.parent.size
         pos: self.parent.pos
-        padding: [dp(8.33), dp(60)]
-        spacing: [dp(8.33), dp(60)]
+        padding:[dp(0.0104125)*app.width, dp(0.125)*app.height]
+        spacing:[dp(0.0104125)*app.width, dp(0.125)*app.height]
         cols: 5
         rows: 2
 
@@ -202,10 +199,11 @@ Builder.load_string("""
             border: [dp(25)]*4
             padding_y: 5
 
-""")
+"""
+    )
+
 
 class SystemMenuScreen(Screen):
-
     default_font_size = 16
 
     def __init__(self, **kwargs):
@@ -213,21 +211,11 @@ class SystemMenuScreen(Screen):
         self.systemtools_sm = kwargs['system_tools']
         self.l = kwargs['localization']
         self.kb = kwargs['keyboard']
-
-        self.id_list = [
-        self.button_system_info,
-        self.button_support_menu,
-        self.button_reboot,
-        self.button_exit_software,
-        self.button_usb_first_aid,
-        self.button_beta_testing,
-        self.button_grbl_settings,
-        self.button_factory,
-        # self.button_update_testing,
-        self.button_developer,
-        self.button_go_back
-        ]
-
+        self.id_list = [self.button_system_info, self.button_support_menu,
+            self.button_reboot, self.button_exit_software, self.
+            button_usb_first_aid, self.button_beta_testing, self.
+            button_grbl_settings, self.button_factory, self.
+            button_developer, self.button_go_back]
         self.update_strings()
 
     def go_back(self):
@@ -252,16 +240,16 @@ class SystemMenuScreen(Screen):
         popup_system.PopupBetaTesting(self.systemtools_sm, self.l)
 
     def grbl_settings(self):
-        popup_system.PopupGRBLSettingsPassword(self.systemtools_sm, self.l, self.kb)
+        popup_system.PopupGRBLSettingsPassword(self.systemtools_sm, self.l,
+            self.kb)
 
     def factory_settings(self):
-        popup_system.PopupFactorySettingsPassword(self.systemtools_sm, self.l, self.kb)
-
-    # def update_testing(self):
-    #     popup_system.PopupUpdateTestingPassword(self.systemtools_sm, self.l, self.kb)
+        popup_system.PopupFactorySettingsPassword(self.systemtools_sm, self
+            .l, self.kb)
 
     def developer(self):
-        popup_system.PopupDeveloperPassword(self.systemtools_sm, self.l, self.kb)
+        popup_system.PopupDeveloperPassword(self.systemtools_sm, self.l,
+            self.kb)
 
     def update_strings(self):
         self.button_system_info.text = self.l.get_str('System Info')
@@ -272,23 +260,20 @@ class SystemMenuScreen(Screen):
         self.button_beta_testing.text = self.l.get_str('Beta Testing')
         self.button_grbl_settings.text = self.l.get_str('GRBL Settings')
         self.button_factory.text = self.l.get_str('Factory')
-        # self.button_update_testing.text = self.l.get_str('Update Testing')
         self.button_developer.text = self.l.get_str('Developer')
         self.button_go_back.text = self.l.get_str('Go Back')
-
         for id_object in self.id_list:
             self.update_font_size(id_object)
 
     def update_font_size(self, value):
         text_length = self.l.get_text_length(value.text)
-
         if text_length < 16:
             value.font_size = self.default_font_size
-        elif text_length > 15: 
+        elif text_length > 15:
             value.font_size = self.default_font_size - 2
-        if text_length > 19: 
+        if text_length > 19:
             value.font_size = self.default_font_size - 4
-        if text_length > 22: 
+        if text_length > 22:
             value.font_size = self.default_font_size - 5
-        if text_length > 25: 
+        if text_length > 25:
             value.font_size = self.default_font_size - 6
