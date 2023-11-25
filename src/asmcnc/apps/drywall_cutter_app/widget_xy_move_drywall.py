0459c7c76a56b88bc047b72d641577823522ba90
DennisKiselev <kiselev.dennis@gmail.com>
2023-11-25 15:44:46 +0000

    Round to 1dp when checking if overlay should flash

--- src/asmcnc/apps/drywall_cutter_app/widget_xy_move_drywall.py
+++ src/asmcnc/apps/drywall_cutter_app/widget_xy_move_drywall.py
@@ -328,7 +328,8 @@ class XYMoveDrywall(Widget):
 
     def check_zh_at_datum(self, dt):
         # wpos == 0,0 when zh is at datum
-        if not (round(self.m.wpos_x(), 2) == 0 and round(self.m.wpos_y(), 2) == 0):
+        # Round to 1dp instead of 2dp to make up for grbl error
+        if not (round(self.m.wpos_x(), 1) == 0 and round(self.m.wpos_y(), 1) == 0):
             # Pulse overlay by smoothly alternating between 0 and 1 opacity
             # Hacky way to track pulsing on or off without a variable by storing that information in the opacity value
             if self.go_to_datum_button_overlay.opacity <= 0:
