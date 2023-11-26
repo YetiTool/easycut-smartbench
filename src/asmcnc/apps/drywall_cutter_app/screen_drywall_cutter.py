9e6713e2ff045764845b0b4e8d8a7fc87ef273b2
DennisKiselev <kiselev.dennis@gmail.com>
2023-11-26 15:39:43 +0000

    Set datum when loading config

--- src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
+++ src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
@@ -227,6 +227,9 @@ class DrywallCutterScreen(Screen):
 
         # set the label on the screen to the name of the config file below
 
+        # Set datum when loading a new config
+        self.m.set_datum(x=self.dwt_config.active_config.datum_position.x, y=self.dwt_config.active_config.datum_position.y, relative=True)
+
         self.apply_active_config()
 
     def apply_active_config(self):
