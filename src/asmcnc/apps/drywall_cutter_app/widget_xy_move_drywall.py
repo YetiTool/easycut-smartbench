1d7a0259b678408823c3620fd84eeba460dbb35d
Benji Pestell <benji.pestell@yetitool.com>
2023-11-29 12:21:46 +0000

    update go_to_datum flashing logic

--- src/asmcnc/apps/drywall_cutter_app/widget_xy_move_drywall.py
+++ src/asmcnc/apps/drywall_cutter_app/widget_xy_move_drywall.py
@@ -331,22 +331,29 @@ class XYMoveDrywall(Widget):
                 self.m.go_xy_datum()
 
     def check_zh_at_datum(self, dt):
-        # wpos == 0,0 when zh is at datum
-        # Round to 1dp instead of 2dp to make up for grbl error
-        if not (round(self.m.wpos_x(), 1) == 0 and round(self.m.wpos_y(), 1) == 0):
-            # Pulse overlay by smoothly alternating between 0 and 1 opacity
-            # Hacky way to track pulsing on or off without a variable by storing that information in the opacity value
+        def check_position(x, y, laser_offset=False):
+            return round(x + (self.m.laser_offset_x_value if laser_offset else 0), 1) == 0 and round(y + (self.m.laser_offset_y_value if laser_offset else 0), 1) == 0
+
+        def pulse_overlay():
             if self.go_to_datum_button_overlay.opacity <= 0:
                 self.go_to_datum_button_overlay.opacity = 0.01
             elif self.go_to_datum_button_overlay.opacity >= 1:
                 self.go_to_datum_button_overlay.opacity = 0.98
-            # Check if second decimal place is even or odd
             elif int(("%.2f" % self.go_to_datum_button_overlay.opacity)[-1]) % 2 == 1:
                 self.go_to_datum_button_overlay.opacity += 0.1
             else:
                 self.go_to_datum_button_overlay.opacity -= 0.1
+
+        if self.m.is_laser_enabled:
+            if not check_position(self.m.wpos_x(), self.m.wpos_y(), laser_offset=True):
+                pulse_overlay()
+            else:
+                self.go_to_datum_button_overlay.opacity = 0
         else:
-            self.go_to_datum_button_overlay.opacity = 0
+            if not check_position(self.m.wpos_x(), self.m.wpos_y()):
+                pulse_overlay()
+            else:
+                self.go_to_datum_button_overlay.opacity = 0
 
     def set_datum(self):
         warning = self.format_command(
