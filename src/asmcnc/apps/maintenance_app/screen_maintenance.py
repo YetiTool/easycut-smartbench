e6241a57d57d4b698a2d360f65882de37adb929f
archiepeterjarvis <archiepeterjarvis@gmail.com>
2023-12-05 13:40:53 +0000

    scale unscaled values

--- src/asmcnc/apps/maintenance_app/screen_maintenance.py
+++ src/asmcnc/apps/maintenance_app/screen_maintenance.py
@@ -409,7 +409,7 @@ Builder.load_string(
 
     BoxLayout: 
         size_hint: (None,None)
-        pos: (dp(710), dp(390))
+        pos: (dp(800.0/800)*app.width - dp(90), dp(480.0/480)*app.height - dp(90))
         Button:
             font_size: str(0.01875 * app.width) + 'sp'
             size_hint: (None,None)
