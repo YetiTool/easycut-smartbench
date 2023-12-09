7374873f0b849779b764925338278f807db1adbd
DennisKiselev <kiselev.dennis@gmail.com>
2023-12-09 23:04:12 +0000

    Checkout file from scaled_black

--- src/asmcnc/apps/start_up_sequence/screens/screen_release_notes.py
+++ src/asmcnc/apps/start_up_sequence/screens/screen_release_notes.py
@@ -1,25 +1,23 @@
-# -*- coding: utf-8 -*-
-'''
+"""
 Created on 6 Aug 2021
 @author: Dennis
 Screen shown after update to display new release notes
-'''
-
+"""
 import kivy, os
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
 from kivy.uix.scrollview import ScrollView
 from kivy.properties import StringProperty, DictProperty
-
 from datetime import datetime
 
-Builder.load_string("""
+Builder.load_string(
+    """
 
 <ScrollReleaseNotes>:
 
     release_notes: release_notes
     BoxLayout:
-        padding: [dp(0), dp(-15), dp(0), dp(0)]
+        padding:[0, dp(0.03125)*app.height, 0, 0]
         RstDocument:
             id: release_notes
             base_font_size: 30
@@ -43,7 +41,7 @@ Builder.load_string("""
 
     BoxLayout:
         orientation: 'vertical'
-        padding: [dp(0), dp(5), dp(0), dp(0)]
+        padding:[0, dp(0.0104166666667)*app.height, 0, 0]
 
         BoxLayout:
             size_hint_y: 0.4
@@ -51,15 +49,15 @@ Builder.load_string("""
             BoxLayout:
                 size_hint_y: 0.6
                 orientation: 'horizontal'
-                padding: [dp(15), dp(0)]
-                spacing: dp(10)
+                padding:[dp(0.01875)*app.width, 0]
+                spacing:dp(0.0125)*app.width
                 Image:
                     size_hint_x: 0.06
                     source: "./asmcnc/skavaUI/img/green_tick.png"
                     allow_stretch: True
                 Label:
                     id: version_number_label
-                    font_size: '30sp'
+                    font_size: str(0.0375*app.width) + 'sp'
                     color: hex('#333333')
                     text_size: self.size
                     size: self.texture_size
@@ -68,15 +66,15 @@ Builder.load_string("""
             Label:
                 id: please_read_label
                 size_hint_y: 0.4
-                padding: [15, 0]
+                padding:[dp(0.01875)*app.width, 0]
                 color: hex('#333333')
-                font_size: '18sp'
+                font_size: str(0.0225*app.width) + 'sp'
                 text_size: self.size
                 size: self.texture_size
 
         BoxLayout:
-            padding: [dp(15), dp(2), dp(20), dp(0)]
-            spacing: dp(15)
+            padding:[dp(0.01875)*app.width, dp(0.00416666666667)*app.height, dp(0.025)*app.width, 0]
+            spacing:dp(0.01875)*app.width
 
             ScrollReleaseNotes:
                 id: scroll_release_notes
@@ -91,7 +89,7 @@ Builder.load_string("""
                     id: url_label
                     size_hint_y: 0.6
                     color: hex('#333333')
-                    font_size: '13sp'
+                    font_size: str(0.01625*app.width) + 'sp'
                     height: self.texture_size[1]
                     text_size: self.size
                     markup: True
@@ -99,22 +97,25 @@ Builder.load_string("""
                     halign: "left"
 
         BoxLayout:
-            padding: [dp(250), dp(2), dp(250), dp(10)]
+            padding:[dp(0.3125)*app.width, dp(0.00416666666667)*app.height, dp(0.3125)*app.width, dp(0.0208333333333)*app.height]
             size_hint_y: 0.25
 
             Button:
                 id: next_button
                 # text: 'Next...'
-                font_size: '30sp'
+                font_size: str(0.0375*app.width) + 'sp'
                 background_normal: "./asmcnc/skavaUI/img/next.png"
                 on_press: root.next_screen()
                 color: hex('f9f9f9ff')
 
-""")
+"""
+)
+
 
 def log(message):
     timestamp = datetime.now()
-    print (timestamp.strftime('%H:%M:%S.%f' )[:12] + ' ' + str(message))
+    print(timestamp.strftime("%H:%M:%S.%f")[:12] + " " + str(message))
+
 
 def filter_version_to_filename(character):
     try:
@@ -123,46 +124,55 @@ def filter_version_to_filename(character):
     except:
         return False
 
-class ScrollReleaseNotes(ScrollView):
-    text = StringProperty('')
 
-    color_dict = DictProperty({
-                    'background': 'e5e5e5ff',
-                    'link': '1976d2ff',
-                    'paragraph': '333333ff',
-                    'title': '333333ff',
-                    'bullet': '333333ff'})
+class ScrollReleaseNotes(ScrollView):
+    text = StringProperty("")
+    color_dict = DictProperty(
+        {
+            "background": "e5e5e5ff",
+            "link": "1976d2ff",
+            "paragraph": "333333ff",
+            "title": "333333ff",
+            "bullet": "333333ff",
+        }
+    )
+
 
 class ReleaseNotesScreen(Screen):
-
     data_consent_app = None
     user_has_confirmed = False
 
     def __init__(self, **kwargs):
         super(ReleaseNotesScreen, self).__init__(**kwargs)
-        self.start_seq=kwargs['start_sequence']
-        self.sm = kwargs['screen_manager']
-        self.version = kwargs['version']
-        self.l=kwargs['localization']
-
-
-        # Filename consists of just the version digits followed by .txt, so can be found by filtering out non integers from version name
-        # Two dots before filename mean parent directory, as file is at the top of the filetree, not in src
-        self.release_notes_filename = '../' + (self.version).replace(".","") + '.txt'
+        self.start_seq = kwargs["start_sequence"]
+        self.sm = kwargs["screen_manager"]
+        self.version = kwargs["version"]
+        self.l = kwargs["localization"]
+        self.release_notes_filename = "../" + self.version.replace(".", "") + ".txt"
         self.scroll_release_notes.release_notes.source = self.release_notes_filename
-
         self.update_strings()
 
     def update_strings(self):
-
-        self.version_number_label.text = (self.l.get_str("Software updated successfully to version")).replace(self.l.get_str('version'), self.version)
-        self.please_read_label.text = self.l.get_str("These release notes contain critical information about how SmartBench has changed (in English).")
-        self.url_label.text = self.l.get_str("For full release notes, go to:") + \
-        "\n" + \
-        "https://www.yetitool.com\n/SUPPORT\n/KNOWLEDGE-BASE\n/smartbench1-console-\noperations-software-\nupdates-release-notes"
+        self.version_number_label.text = self.l.get_str(
+            "Software updated successfully to version"
+        ).replace(self.l.get_str("version"), self.version)
+        self.please_read_label.text = self.l.get_str(
+            "These release notes contain critical information about how SmartBench has changed (in English)."
+        )
+        self.url_label.text = (
+            self.l.get_str("For full release notes, go to:")
+            + "\n"
+            + """https://www.yetitool.com
+/SUPPORT
+/KNOWLEDGE-BASE
+/smartbench1-console-
+operations-software-
+updates-release-notes"""
+        )
         self.next_button.text = self.l.get_str("Next") + "..."
 
-    
     def next_screen(self):
-        os.system('sudo sed -i "s/power_cycle_alert=True/power_cycle_alert=False/" /home/pi/easycut-smartbench/src/config.txt')
+        os.system(
+            'sudo sed -i "s/power_cycle_alert=True/power_cycle_alert=False/" /home/pi/easycut-smartbench/src/config.txt'
+        )
         self.start_seq.next_in_sequence()
