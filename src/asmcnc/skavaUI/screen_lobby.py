90efb11b6fe6cae3c70c956032cb86ae4f8fdbb0
archiepeterjarvis <archiepeterjarvis@gmail.com>
2023-12-12 20:35:51 +0000

    undo testing changes

--- src/asmcnc/skavaUI/screen_lobby.py
+++ src/asmcnc/skavaUI/screen_lobby.py
@@ -527,8 +527,6 @@ class LobbyScreen(Screen):
         if not sys.platform == "win32":
             self.m.set_led_colour('GREEN')
 
-        self.sm.current = 'errorScreen'
-
         # Tell user to update if update is available
         if self.trigger_update_popup:
             popup_info.PopupInfo(self.sm, self.l, 450, self.update_message)
