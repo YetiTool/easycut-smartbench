c66c22ae16ec7b5fe1bb698c114256a346d5b3b4
VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>
2023-12-15 13:33:06 +0000

    Fixed auto formatting issues

--- src/asmcnc/skavaUI/screen_lobby.py
+++ src/asmcnc/skavaUI/screen_lobby.py
@@ -1,19 +1,26 @@
+# -*- coding: utf-8 -*-
 """
 Created on 19 Aug 2017
 
 @author: Ed
 """
+# config
+
 import kivy
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition, SlideTransition
 from kivy.uix.floatlayout import FloatLayout
-from kivy.properties import ObjectProperty, ListProperty, NumericProperty
+from kivy.properties import ObjectProperty, ListProperty, NumericProperty  # @UnresolvedImport
 from kivy.uix.widget import Widget
 from kivy.clock import Clock
+
 import sys, os, textwrap
 from os.path import expanduser
 from shutil import copy
+
 from asmcnc.skavaUI import popup_info
+from asmcnc.core_UI.popups import BasicPopup, PopupType
+from kivy.core.window import Window
 
 Builder.load_string(
     """
@@ -496,58 +503,87 @@ Builder.load_string(
                 
 """
 )
-job_cache_dir = "./jobCache/"
-job_q_dir = "./jobQ/"
-ftp_file_dir = "/home/sysop/router_ftp"
+job_cache_dir = "./jobCache/"  # where job files are cached for selection (for last used history/easy access)
+job_q_dir = "./jobQ/"  # where file is copied if to be used next in job
+ftp_file_dir = "/home/sysop/router_ftp"  # Linux location where incoming files are FTP'd to
 
 
 class LobbyScreen(Screen):
-    no_preview_found_img_path = "./asmcnc/skavaUI/img/image_preview_inverted_large.png"
+    no_preview_found_img_path = './asmcnc/skavaUI/img/image_preview_inverted_large.png'
     trigger_update_popup = False
-    welcome_popup_description = ""
-    update_message = ""
+    welcome_popup_description = ''
+    update_message = ''
     upgrade_app_hidden = False
 
     def __init__(self, **kwargs):
         super(LobbyScreen, self).__init__(**kwargs)
-        self.sm = kwargs["screen_manager"]
-        self.m = kwargs["machine"]
-        self.am = kwargs["app_manager"]
-        self.l = kwargs["localization"]
+        self.sm = kwargs['screen_manager']
+        self.m = kwargs['machine']
+        self.am = kwargs['app_manager']
+        self.l = kwargs['localization']
+
         self.update_strings()
+
+        # If it's a SmartCNC machine, then show the drywalltec app instead of shapecutter
         if "DRYWALLTEC" in self.m.smartbench_model():
             self.shapecutter_container.parent.remove_widget(self.shapecutter_container)
         else:
             self.drywall_app_container.parent.remove_widget(self.drywall_app_container)
 
     def on_pre_enter(self):
+        # Hide upgrade app if older than V1.3, and only if it has not been hidden already
         if not "V1.3" in self.m.smartbench_model() and not self.upgrade_app_hidden:
             self.upgrade_app_container.parent.remove_widget(self.upgrade_app_container)
             self.upgrade_app_hidden = True
         elif self.upgrade_app_hidden and "V1.3" in self.m.smartbench_model():
-            pass
+            pass  # reinstate upgrade_app_container, tbc - this is placeholder for now
 
     def on_enter(self):
         if not sys.platform == "win32":
-            self.m.set_led_colour("GREEN")
+            self.m.set_led_colour('GREEN')
+
+        # Tell user to update if update is available
         if self.trigger_update_popup:
-            popup_info.PopupInfo(self.sm, self.l, 450, self.update_message)
-        if self.m.trigger_setup:
-            self.help_popup()
+            popup_info.PopupInfo(self.sm, self.l, 0.5625*Window.width , self.update_message)
+
+        # Trigger welcome popup is machine is being used for the first time
+        if self.m.trigger_setup: self.help_popup()
+
+    def set_trigger_to_false(self, *args):
+        self.m.write_set_up_options(False)
+        self.sm.get_screen('lobby').carousel.load_next(mode='next')
+
+    def set_trigger_to_true(self, *args):
+        self.m.write_set_up_options(True)
 
     def help_popup(self):
-        popup_info.PopupWelcome(self.sm, self.m, self.l, self.welcome_popup_description)
+        # popup_info.PopupWelcome(self.sm, self.m, self.l, self.welcome_popup_description)
+        welcome_popup = BasicPopup(sm=self.sm, m=self.m, l=self.l,
+                                   title=self.l.get_str('Welcome to SmartBench'),
+                                   main_string=self.welcome_popup_description,
+                                   popup_type=PopupType.INFO,
+                                   popup_width=500, popup_height=440, main_label_size_delta=80,
+                                   main_label_padding=(0, 0), main_layout_padding=(10, 10, 10, 10),
+                                   main_layout_spacing=10, button_layout_padding=(20, 10, 20, 0),
+                                   button_layout_spacing=15,
+                                   button_two_background_color=(76 / 255., 175 / 255., 80 / 255., 1.),
+                                   button_one_background_color=(230 / 255., 74 / 255., 25 / 255., 1.),
+                                   button_one_text="Remind me", button_two_text="Ok",
+                                   button_one_callback=self.set_trigger_to_true,
+                                   button_two_callback=self.set_trigger_to_false)
+
+        welcome_popup.open()
 
     def pro_app(self):
         self.am.start_pro_app()
-        self.sm.current = "home"
+        self.sm.current = 'home'
 
     def shapecutter_app(self):
         self.m.run_led_rainbow_ending_green()
         self.am.start_shapecutter_app()
 
     def calibrate_smartbench(self):
-        self.am.start_calibration_app("lobby")
+        self.am.start_calibration_app('lobby')
 
     def wifi_app(self):
         self.am.start_wifi_app()
@@ -556,66 +592,59 @@ class LobbyScreen(Screen):
         self.am.start_update_app()
 
     def developer_app(self):
+        # popup_info.PopupDeveloper(self.sm)
         self.am.start_systemtools_app()
 
     def maintenance_app(self):
-        self.am.start_maintenance_app("laser_tab")
+        self.am.start_maintenance_app('laser_tab')
 
     def upgrade_app(self):
+        # Need to set $51 on entry, requires idle
         if self.m.state().startswith("Idle"):
             self.am.start_upgrade_app()
         else:
-            popup_info.PopupError(
-                self.sm,
-                self.l,
-                self.l.get_str("Please ensure machine is idle before continuing."),
-            )
+            popup_info.PopupError(self.sm, self.l, self.l.get_str("Please ensure machine is idle before continuing."))
 
     def drywall_cutter_app(self):
         self.am.start_drywall_cutter_app()
 
     def shutdown_console(self):
-        if sys.platform != "win32" and sys.platform != "darwin":
-            os.system("sudo shutdown -h")
+        if sys.platform != 'win32' and sys.platform != 'darwin':
+            os.system('sudo shutdown -h')
         popup_info.PopupShutdown(self.sm, self.l)
 
     def update_strings(self):
-        self.pro_app_label.text = self.l.get_str("CAD / CAM")
-        self.shapecutter_app_label.text = self.l.get_str("Shape Cutter")
-        self.wifi_app_label.text = self.l.get_str("Wifi")
-        self.calibrate_app_label.text = self.l.get_str("Calibrate")
-        self.update_app_label.text = self.l.get_str("Update")
-        self.maintenance_app_label.text = self.l.get_str("Maintenance")
-        self.system_tools_app_label.text = self.l.get_str("System Tools")
-        self.upgrade_app_label.text = self.l.get_str("Upgrade")
+        self.pro_app_label.text = self.l.get_str('CAD / CAM')
+        self.shapecutter_app_label.text = self.l.get_str('Shape Cutter')
+        self.wifi_app_label.text = self.l.get_str('Wifi')
+        self.calibrate_app_label.text = self.l.get_str('Calibrate')
+        self.update_app_label.text = self.l.get_str('Update')
+        self.maintenance_app_label.text = self.l.get_str('Maintenance')
+        self.system_tools_app_label.text = self.l.get_str('System Tools')
+        self.upgrade_app_label.text = self.l.get_str('Upgrade')
+
         self.welcome_popup_description = (
-            self.format_command(
-                self.l.get_str(
-                    "Use the arrows to go through the menu, and select an app to get started."
-                )
-            )
-            + "\n\n"
-            + self.format_command(
-                self.l.get_str(
-                    "If this is your first time, make sure you use the Wifi, Maintenance, "
-                    + "and Calibrate apps to set up SmartBench."
-                )
-                .replace(self.l.get_str("Wifi"), self.l.get_bold("Wifi"))
-                .replace(self.l.get_str("Maintenance"), self.l.get_bold("Maintenance"))
-                .replace(self.l.get_str("Calibrate"), self.l.get_bold("Calibrate"))
-            )
-            + "\n\n"
-            + self.format_command(self.l.get_str("For more help, please visit:"))
-            + "\n"
-            + "[b]https://www.yetitool.com/support[/b]"
-            + "\n"
+                self.format_command(
+                    self.l.get_str('Use the arrows to go through the menu, and select an app to get started.')
+                ) + '\n\n' +
+                self.format_command(
+                    ((self.l.get_str('If this is your first time, make sure you use the Wifi, Maintenance, ' +
+                                     'and Calibrate apps to set up SmartBench.'
+                                     ).replace(self.l.get_str('Wifi'), self.l.get_bold('Wifi'))
+                      ).replace(self.l.get_str('Maintenance'), self.l.get_bold('Maintenance'))
+                     ).replace(self.l.get_str('Calibrate'), self.l.get_bold('Calibrate')
+                               )
+                ) + '\n\n' +
+                self.format_command(
+                    self.l.get_str('For more help, please visit:')
+                ) + '\n' +
+                '[b]https://www.yetitool.com/support[/b]' + '\n'
         )
         self.update_message = (
-            self.l.get_str("New software update available for download!")
-            + "\n\n"
-            + self.l.get_str(
-                "Please use the Update app to get the latest version."
-            ).replace(self.l.get_str("Update"), self.l.get_bold("Update"))
+                self.l.get_str('New software update available for download!') + '\n\n' +
+                self.l.get_str(
+                    'Please use the Update app to get the latest version.'
+                ).replace(self.l.get_str('Update'), self.l.get_bold('Update'))
         )
 
     def format_command(self, cmd):
