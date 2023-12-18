5b8be252635e6d94030e62a7d0f8861d09ef83ad
archiepeterjarvis <archiepeterjarvis@gmail.com>
2023-12-18 15:18:34 +0000

    test popup manager

--- src/asmcnc/core_UI/popup_manager.py
+++ src/asmcnc/core_UI/popup_manager.py
@@ -6,30 +6,41 @@ class PopupManager:
     l = None
     m = None
 
+    error_popup = None
+    info_popup = None
+    mini_info_popup = None
+
     def __init__(self, sm, m, l):
         self.sm = sm
         self.m = m
         self.l = l
 
-    error_popup = ErrorPopup(sm=sm, m=m, l=l,
-                             main_string="")
+        self.setup_popups()
+
+    def setup_popups(self):
+        self.error_popup = ErrorPopup(sm=self.sm, m=self.m, l=self.l, main_string="")
+
+        self.info_popup = InfoPopup(
+            sm=self.sm,
+            m=self.m,
+            l=self.l,
+            main_string="",
+            popup_width=400,
+            popup_height=440,
+        )
+
+        self.mini_info_popup = MiniInfoPopup(
+            sm=self.sm, m=self.m, l=self.l, main_string=""
+        )
 
     def show_error_popup(self, main_string):
         self.error_popup.main_label.text = main_string
         self.error_popup.open()
 
-    info_popup = InfoPopup(sm=sm, m=m, l=l,
-                           main_string="",
-                           popup_width=400,
-                           popup_height=440)
-
     def show_info_popup(self, main_string, width):
         self.info_popup.main_label.text = main_string
         self.info_popup.width = width
         self.info_popup.open()
-
-    mini_info_popup = MiniInfoPopup(sm=sm, m=m, l=l,
-                                    main_string="")
 
     def show_mini_info_popup(self, main_string):
         self.mini_info_popup.main_label.text = main_string
