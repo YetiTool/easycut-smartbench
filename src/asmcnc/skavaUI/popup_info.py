790acc74ab4ede233136e901747f2703d732adcc
LettyYeti <lettie.adkins@yetitool.com>
2023-12-18 12:15:46 +0000

    all skavaUI popup_info.PopupError to call new ErrorPopup class

--- src/asmcnc/skavaUI/popup_info.py
+++ src/asmcnc/skavaUI/popup_info.py
@@ -9,6 +9,7 @@ import os
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
+from asmcnc.core_UI.popups import ErrorPopup
 from kivy.properties import StringProperty  # @UnresolvedImport
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.widget import Widget
@@ -687,46 +688,7 @@ class PopupSoftwareRepair(Widget):
 
 class PopupError(Widget):
     def __init__(self, screen_manager, localization, warning_message):
-        self.sm = screen_manager
-        self.l = localization
-
-        description = warning_message
-
-        title_string = self.l.get_str('Error!')
-        ok_string = self.l.get_bold('Ok')
-
-        img = Image(source="./asmcnc/apps/shapeCutter_app/img/error_icon.png", allow_stretch=False)
-        label = Label(size_hint_y=1, text_size=(460, None), halign='center', valign='middle', text=description,
-                      color=[0, 0, 0, 1], padding=[0, 10], markup=True)
-
-        ok_button = Button(text=ok_string, markup=True)
-        ok_button.background_normal = ''
-        ok_button.background_color = [230 / 255., 74 / 255., 25 / 255., 1.]
-
-        btn_layout = BoxLayout(orientation='horizontal', spacing=10, padding=[0, 20, 0, 0])
-        btn_layout.add_widget(ok_button)
-
-        layout_plan = BoxLayout(orientation='vertical', spacing=10, padding=[40, 20, 40, 20])
-        layout_plan.add_widget(img)
-        layout_plan.add_widget(label)
-        layout_plan.add_widget(btn_layout)
-
-        popup = Popup(title=title_string,
-                      title_color=[0, 0, 0, 1],
-                      title_size='20sp',
-                      content=layout_plan,
-                      size_hint=(None, None),
-                      size=(500, 400),
-                      auto_dismiss=True
-                      )
-
-        popup.separator_color = [230 / 255., 74 / 255., 25 / 255., 1.]
-        popup.separator_height = '4dp'
-        popup.background = './asmcnc/apps/shapeCutter_app/img/popup_background.png'
-
-        ok_button.bind(on_press=popup.dismiss)
-
-        popup.open()
+        ErrorPopup(sm=screen_manager, l=localization, main_string=warning_message).open()
 
 
 class PopupWarning(Widget):
