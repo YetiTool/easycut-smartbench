c1cdeabe538a2f820e27d2ed51a26adcef60698c
Marc <marc.schlotzhauer@yetitool.com>
2024-01-16 11:57:35 +0000

    initial call list instead of appending.

--- tests/automated_unit_tests/apps/systemTools/test_system_tools_support.py
+++ tests/automated_unit_tests/apps/systemTools/test_system_tools_support.py
@@ -40,17 +40,16 @@ def test_download_to_usb(mock_os,mock_Clock):
     # fake an active USB:
     sm_st.usb_stick.is_usb_mounted_flag = True
     copy_to_usb(1)
-    calls = []
-    calls.append(call.system('mkdir -p /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src'))
-    calls.append(call.system('cp -r /home/pi/easycut-smartbench/src/sb_values /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src/'))
-    calls.append(call.system('rm /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src/sb_values/saved_grbl_settings_params.txt'))
-    calls.append(call.system('cp -r /home/pi/easycut-smartbench/src/jobCache /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src/'))
-    calls.append(call.system('cp /home/pi/smartbench* /home/pi/easycut-smartbench/transfer_tmp/'))
-    calls.append(call.system('cp /home/pi/multiply.txt /home/pi/easycut-smartbench/transfer_tmp/'))
-    calls.append(call.system('cp /home/pi/plus.txt /home/pi/easycut-smartbench/transfer_tmp/'))
-    calls.append(call.system('sudo tar czf /media/usb/transfer.tar.gz -C /home/pi/easycut-smartbench/transfer_tmp .'))
-    calls.append(call.system('rm -r /home/pi/easycut-smartbench/transfer_tmp'))
-    calls.append(call.path.isfile('/media/usb/transfer.tar.gz'))
+    calls = [call.system('mkdir -p /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src'),
+             call.system('cp -r /home/pi/easycut-smartbench/src/sb_values /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src/'),
+             call.system('rm /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src/sb_values/saved_grbl_settings_params.txt'),
+             call.system('cp -r /home/pi/easycut-smartbench/src/jobCache /home/pi/easycut-smartbench/transfer_tmp/easycut-smartbench/src/'),
+             call.system('cp /home/pi/smartbench* /home/pi/easycut-smartbench/transfer_tmp/'),
+             call.system('cp /home/pi/multiply.txt /home/pi/easycut-smartbench/transfer_tmp/'),
+             call.system('cp /home/pi/plus.txt /home/pi/easycut-smartbench/transfer_tmp/'),
+             call.system('sudo tar czf /media/usb/transfer.tar.gz -C /home/pi/easycut-smartbench/transfer_tmp .'),
+             call.system('rm -r /home/pi/easycut-smartbench/transfer_tmp'),
+             call.path.isfile('/media/usb/transfer.tar.gz')]
     mock_os.assert_has_calls(calls)
 
 @patch('asmcnc.apps.systemTools_app.screen_manager_systemtools.Clock')
