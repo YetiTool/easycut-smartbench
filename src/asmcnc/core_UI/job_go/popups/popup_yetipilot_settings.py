5d16ad3c657b6f971bc6a59f69c016aa934dade2
VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>
2024-01-17 19:48:04 +0000

    Allowed health check image to stretch to scale on C10
    Signed-off-by: VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>

--- src/asmcnc/core_UI/job_go/popups/popup_yetipilot_settings.py
+++ src/asmcnc/core_UI/job_go/popups/popup_yetipilot_settings.py
@@ -89,7 +89,7 @@ Builder.load_string(
             center_x: self.parent.center_x
             y: self.parent.y
             size: self.parent.width, self.parent.height
-            allow_stretch: False
+            allow_stretch: True
 
 
 
