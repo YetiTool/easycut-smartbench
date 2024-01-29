8f36f66217a245dd96c0ccb63173bec1e0c8ae22
VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>
2024-01-29 17:43:49 +0000

    Added popup builder string
    Signed-off-by: VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>

---
+++ src/asmcnc/apps/drywall_cutter_app/material_setup_popup.py
@@ -0,0 +1,284 @@
+from kivy.uix.popup import Popup
+from kivy.lang import Builder
+import math
+from kivy.uix.image import Image
+
+Builder.load_string("""
+<CuttingDepthsPopup>:
+    float_layout:float_layout
+
+    material_graphic:material_graphic
+    material_thickness:material_thickness
+    bottom_offset:bottom_offset
+    total_cut_depth:total_cut_depth
+    depth_per_pass:depth_per_pass
+    auto_pass_checkbox:auto_pass_checkbox
+
+    title_label:title_label
+    material_thickness_label:material_thickness_label
+    bottom_offset_label:bottom_offset_label
+    total_cut_depth_label:total_cut_depth_label
+    auto_pass_label:auto_pass_label
+    depth_per_pass_label:depth_per_pass_label
+    cut_depth_warning:cut_depth_warning
+    pass_depth_warning:pass_depth_warning
+
+    cutter_graphic:cutter_graphic
+    total_cut_depth_arrow:total_cut_depth_arrow
+
+    confirm_button:confirm_button
+
+    auto_dismiss: False
+    size_hint: (None,None)
+    size: (577, 301)
+    title: ''
+    separator_height: 0
+    background: './asmcnc/apps/drywall_cutter_app/img/cutting_depths_popup.png'
+
+    on_touch_down: root.on_touch()
+
+    FloatLayout:
+        id: float_layout
+        size_hint: (None, None)
+        size: (577, 301)
+        pos_hint: {'y': -0.05}
+
+        Label:
+            id: title_label
+            pos_hint: {'x': -0.39, 'y': 0.45}
+            text: 'Cutting depths'
+            font_size: '20sp'
+            color: hex('#F9F9F9')
+
+        Label:
+            id: Z0
+            pos_hint: {'x': 0.0375, 'y': 0.21}
+            text: 'Z0'
+            font_size: '20sp'
+            color: hex('#333333')
+
+        Image:
+            id: material_graphic
+            source: "./asmcnc/apps/drywall_cutter_app/img/material_graphic.png"
+            pos_hint: {'center_x': 0.45}
+            y: self.parent.y
+            size: self.parent.size
+            allow_stretch: True 
+
+        Label:
+            id: material_thickness_label
+            pos_hint: {'x': -0.42, 'y': 0.23}
+            text: ''
+            font_size: '16sp'
+            color: hex('#333333')
+            text_size: (75, None)
+
+        Image:
+            id: material_thickness_dims
+            source: "./asmcnc/apps/drywall_cutter_app/img/material_thickness_dims.png"
+            pos_hint: {'center_x': 0.45}
+            y: self.parent.y
+            size: self.parent.size
+            allow_stretch: True
+
+        TextInput:
+            id: material_thickness
+            pos_hint: {'x': 0.165, 'y': 0.68}
+            padding_y: [self.height / 2.0 - (self.line_height / 2.0) * len(self._lines), 0]
+            halign: 'center'
+            size_hint: (.12, .1)
+            text_size: self.size
+            font_size: '16sp'
+            markup: True
+            multiline: False
+            text: ''
+            input_filter: 'float'
+
+        Label:
+            id: bottom_offset_label
+            pos_hint: {'x': -0.42, 'y': -0.075}
+            text: ''
+            font_size: '16sp'
+            color: hex('#333333')
+            text_size: (75, None)
+
+        Image:
+            id: bottom_offset_graphic
+            source: "./asmcnc/apps/drywall_cutter_app/img/bottom_offset_graphic.png"
+            pos_hint: {'center_x': 0.45}
+            y: self.parent.y
+            size: self.parent.size
+            allow_stretch: True
+
+        TextInput:
+            id: bottom_offset
+            pos_hint: {'x': 0.165, 'y': 0.37}
+            padding_y: [self.height / 2.0 - (self.line_height / 2.0) * len(self._lines), 0]
+            halign: 'center'
+            size_hint: (.12, .1)
+            text_size: self.size
+            font_size: '16sp'
+            markup: True
+            multiline: False
+            text: ''
+            input_filter: 'float' 
+
+        Image:
+            id: total_cut_depth_dims
+            source: "./asmcnc/apps/drywall_cutter_app/img/total_cut_depth_dims.png"
+            pos_hint: {'center_x': 0.45}
+            y: self.parent.y
+            size: self.parent.size
+            allow_stretch: True
+
+        Image:
+            id: total_cut_depth_arrow
+            source: "./asmcnc/apps/drywall_cutter_app/img/total_cut_depth_bottom_arrow.png"
+            pos_hint: {'center_x': 0.45, 'y': 0.225}
+            y: self.parent.y
+            size: self.parent.size
+            allow_stretch: True
+
+        Label:
+            id: total_cut_depth_label
+            pos_hint: {'x': -0.42, 'y': -0.275}
+            text: ''
+            font_size: '16sp'
+            color: hex('#333333')
+            text_size: (75, None)
+
+        TextInput:
+            id: total_cut_depth
+            pos_hint: {'x': 0.165, 'y': 0.165}
+            padding_y: [self.height / 2.0 - (self.line_height / 2.0) * len(self._lines), 0]
+            halign: 'center'
+            size_hint: (.12, .1)
+            text_size: self.size
+            font_size: '16sp'
+            markup: True
+            multiline: False
+            text: ''
+            disabled: True
+
+        Image:
+            id: cutter_graphic
+            source: "./asmcnc/apps/drywall_cutter_app/img/cutter_graphic.png"
+            pos_hint: {'center_x': 0.45, 'y': 0.225}
+            # y: self.parent.y
+            size: self.parent.size
+            allow_stretch: True 
+
+        Label:
+            id: cut_depth_warning
+            pos_hint: {'x': -0.335, 'y': -0.4}
+            text: ''
+            font_size: '14sp'
+            markup: True
+            color: hex('#FF0000')
+            text_size: (175, None)
+
+        Label:
+            id: pass_depth_warning
+            pos_hint: {'x': 0.2525, 'y': -0.1}
+            text: ''
+            font_size: '14sp'
+            markup: True
+            color: hex('#FF0000')
+            text_size: (150, None) 
+
+        GridLayout:
+            rows: 2
+            cols: 2
+            pos_hint: {'x': 0.6, 'y': -0.2}
+            row_force_default: True
+            col_force_default: True
+            row_default_height: 50
+            col_default_width: 100
+
+
+            Label:
+                id: auto_pass_label
+                text: ''
+                font_size: '16sp'
+                color: hex('#333333')
+                text_size: (75, None)
+            CheckBox:
+                id: auto_pass_checkbox
+                size_hint: (0.3,1)
+                active: True
+                background_checkbox_normal: "./asmcnc/skavaUI/img/checkbox_inactive.png"
+                on_active: root.on_checkbox_active()
+
+            Label:
+                id: depth_per_pass_label
+                text: ''
+                font_size: '16sp'
+                color: hex('#333333')
+                text_size: (75, None)
+
+            TextInput:
+                id: depth_per_pass
+                padding_y: [self.height / 2.0 - (self.line_height / 2.0) * len(self._lines), 0]
+                halign: 'center'
+                size_hint: (0, 0)
+                text_size: self.size
+                font_size: '16sp'
+                markup: True
+                multiline: False
+                text: ''
+                disabled: True
+                input_filter: 'float' 
+
+        BoxLayout:
+            orientation: 'horizontal'
+            pos_hint: {'x': 0.625, 'y': 0.075}
+            size_hint: (None, None)
+            size: (189,23)
+            spacing: 0.025*app.width
+
+            Button:
+                id: cancel_button
+                on_press: root.cancel()
+                size_hint: (None,None)
+                width: dp(83)
+                height: dp(23)
+                background_color: [0,0,0,0]
+                BoxLayout:
+                    padding: 0
+                    size: self.parent.size
+                    pos: self.parent.pos
+                    Image:
+                        source: "./asmcnc/apps/drywall_cutter_app/img/cancel_button.png"
+                        center_x: self.parent.center_x
+                        y: self.parent.y
+                        size: self.parent.width, self.parent.height
+                        allow_stretch: True
+
+            Button:
+                id: confirm_button
+                on_press: root.confirm()
+                size_hint: (None,None)
+                width: dp(86)
+                height: dp(23)
+                background_color: [0,0,0,0]
+                BoxLayout:
+                    padding: 0
+                    size: self.parent.size
+                    pos: self.parent.pos
+                    Image:
+                        source: "./asmcnc/apps/drywall_cutter_app/img/confirm_button.png"
+                        center_x: self.parent.center_x
+                        y: self.parent.y
+                        size: self.parent.width, self.parent.height
+                        allow_stretch: True
+
+""")
+
+
+class CuttingDepthsPopup(Popup):
+
+    def __init__(self, localization, keyboard, dwt_config, **kwargs):
+        super(CuttingDepthsPopup, self).__init__(**kwargs)
+        self.l = localization
+        self.kb = keyboard
+        self.dwt_config = dwt_config
--- src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
+++ src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
@@ -13,6 +13,7 @@ from asmcnc.apps.drywall_cutter_app import widget_drywall_shape_display
 from asmcnc.apps.drywall_cutter_app.config import config_loader
 from asmcnc.apps.drywall_cutter_app import screen_config_filechooser
 from asmcnc.apps.drywall_cutter_app import screen_config_filesaver
+from asmcnc.apps.drywall_cutter_app import material_setup_popup
 
 
 class ImageButton(ButtonBehavior, Image):
@@ -159,11 +160,13 @@ class DrywallCutterScreen(Screen):
         self.sm = kwargs['screen_manager']
         self.m = kwargs['machine']
         self.l = kwargs['localization']
+        self.kb = kwargs['keyboard']
 
         # XY move widget
         self.xy_move_widget = widget_xy_move_drywall.XYMoveDrywall(machine=self.m, screen_manager=self.sm)
         self.xy_move_container.add_widget(self.xy_move_widget)
 
+        self.materials_popup = material_setup_popup.CuttingDepthsPopup(self.l, self.kb, self.dwt_config)
         self.drywall_shape_display_widget = widget_drywall_shape_display.DrywallShapeDisplay(machine=self.m, screen_manager=self.sm, dwt_config=self.dwt_config)
         self.shape_display_container.add_widget(self.drywall_shape_display_widget)
 
@@ -219,7 +222,7 @@ class DrywallCutterScreen(Screen):
         self.dwt_config.on_parameter_change('toolpath_offset', self.cut_offset_selection.text)
 
     def material_setup(self):
-        pass
+        self.materials_popup.open()
 
     def stop(self):
         popup_info.PopupStop(self.m, self.sm, self.l)
