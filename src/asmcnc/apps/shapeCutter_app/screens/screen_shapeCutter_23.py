234d91c8481c4d92504819a7ca0cf14ea044c5c8
DennisKiselev <kiselev.dennis@gmail.com>
2024-01-30 16:58:20 +0000

    Scale screen 23 warning popup

--- src/asmcnc/apps/shapeCutter_app/screens/screen_shapeCutter_23.py
+++ src/asmcnc/apps/shapeCutter_app/screens/screen_shapeCutter_23.py
@@ -11,6 +11,7 @@ from kivy.properties import StringProperty, ObjectProperty
 from kivy.clock import Clock
 from asmcnc.apps.shapeCutter_app.screens import popup_info
 from asmcnc.apps.shapeCutter_app.screens import popup_input_error
+from asmcnc.core_UI.popups import WarningPopup
 
 Builder.load_string(
     """
@@ -650,7 +651,15 @@ class ShapeCutter23ScreenClass(Screen):
 """
                             + "Please re-enter your parameters."
                         )
-                    popup_input_error.PopupInputError(self.shapecutter_sm, description)
+                    WarningPopup(sm=self.shapecutter_sm, m=self.m, l=self.m.l,
+                                 main_string=description,
+                                 popup_width=400,
+                                 popup_height=380,
+                                 main_label_size_delta=40,
+                                 button_layout_padding=[50,25,50,0],
+                                 main_label_h_align='left',
+                                 main_layout_padding=[50,20,50,20],
+                                 main_label_padding=[20,20]).open()
                     return False
             self.shapecutter_sm.next_screen()
         else:
