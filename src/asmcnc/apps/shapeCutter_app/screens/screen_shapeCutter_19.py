b7a1eecaabe18eed224d843c4681fd503d643d9f
DennisKiselev <kiselev.dennis@gmail.com>
2024-01-30 10:52:21 +0000

    Scale screen 19 info popup

--- src/asmcnc/apps/shapeCutter_app/screens/screen_shapeCutter_19.py
+++ src/asmcnc/apps/shapeCutter_app/screens/screen_shapeCutter_19.py
@@ -9,6 +9,7 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.metrics import MetricsBase
 from kivy.properties import StringProperty, ObjectProperty
 from asmcnc.apps.shapeCutter_app.screens import popup_info
+from asmcnc.core_UI.popups import InfoPopup
 
 Builder.load_string(
     """
@@ -324,7 +325,7 @@ class ShapeCutter19ScreenClass(Screen):
         info = """Ensure your tool is sharp and has no obvious damage.
 
 Ensure your collet size matches your shank diameter."""
-        popup_info.PopupInfo(self.shapecutter_sm, info)
+        InfoPopup(sm=self.shapecutter_sm, m=self.m, l=self.m.l, main_string=info, popup_width=500, popup_height=400, main_label_size_delta=140).open()
 
     def go_back(self):
         self.shapecutter_sm.previous_screen()
