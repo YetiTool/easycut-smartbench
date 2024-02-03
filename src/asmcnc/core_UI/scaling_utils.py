17ab823afc4347721419d7960e8e42d53c0b709a
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-02-03 17:53:46 +0000

    add doc strings

--- src/asmcnc/core_UI/scaling_utils.py
+++ src/asmcnc/core_UI/scaling_utils.py
@@ -5,10 +5,20 @@ Height = Window.height if Window.height in [768, 480] else 480
 
 
 def is_screen_big():
-    return Window.height == 800
+    """
+    Check whether the screen is Console XL (1280x768) or Console (800x480)
+
+    :return: True if the screen is 1280x768, False if the screen is 800x480
+    """
+    return Height == 768
 
 
 def get_scaled_width(width):
+    """
+    Returns the scaled width of the given width, relative to the current console screen size
+    :param width: The width in pixels, based on a 800x480 screen
+    :return: The scaled width
+    """
     if width is None:
         return None
     if width is 0:
@@ -17,6 +27,11 @@ def get_scaled_width(width):
 
 
 def get_scaled_height(height):
+    """
+    Returns the scaled height of the given height, relative to the current console screen size
+    :param height: The height in pixels, based on a 800x480 screen
+    :return: The scaled height
+    """
     if height is None:
         return None
     if height is 0:
@@ -25,6 +40,14 @@ def get_scaled_height(height):
 
 
 def get_scaled_tuple(tup, orientation="horizontal"):
+    """
+    Returns the scaled tuple of the given tuple, relative to the current console screen size
+    This function is helpful for scaling the padding, margin, border, and pos properties of widgets
+
+    :param tup: The tuple to be scaled
+    :param orientation: The orientation of the layout (horizontal or vertical)
+    :return: The scaled tuple
+    """
     if tup is None:
         return None
 
@@ -41,6 +64,11 @@ def get_scaled_tuple(tup, orientation="horizontal"):
 
 
 def get_scaled_sp(sp_str):
+    """
+    Takes a string of a sp font size, such as "20sp", and returns the scaled sp font size
+    :param sp_str: The sp font size to be scaled
+    :return: The scaled sp font size
+    """
     if sp_str is None:
         return None
     return str(float(sp_str[:-2]) / 800.0 * Width) + "sp"
