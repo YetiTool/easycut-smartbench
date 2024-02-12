38ac44d7968c94b4a9d5fc320edf43ae92eac873
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-02-12 17:55:33 +0000

    disable debug

--- src/asmcnc/apps/drywall_cutter_app/config/config_loader.py
+++ src/asmcnc/apps/drywall_cutter_app/config/config_loader.py
@@ -9,7 +9,7 @@ cutters_dir = os.path.join(current_dir, 'cutters')
 temp_dir = os.path.join(current_dir, 'temp')
 
 TEMP_CONFIG_PATH = os.path.join(temp_dir, 'temp_config')
-DEBUG_MODE = True
+DEBUG_MODE = False
 
 
 def debug(func):
