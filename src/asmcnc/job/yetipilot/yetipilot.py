92fbc4a0c13d6a256e7f44fb35609c70274ee6f0
Benji Pestell <benji.pestell@yetitool.com>
2024-02-13 15:44:13 +0000

    Restore old logic

--- src/asmcnc/job/yetipilot/yetipilot.py
+++ src/asmcnc/job/yetipilot/yetipilot.py
@@ -144,10 +144,9 @@ class YetiPilot(object):
 
     def get_speed_adjustment_percentage(self):
         last_gcode_rpm = self.jd.grbl_mode_tracker[0][2]
-        last_spindle_rpm = self.m.correct_rpm(last_gcode_rpm)
 
-        if abs(last_spindle_rpm - self.target_spindle_speed) > 100:
-            return ((self.target_spindle_speed - last_spindle_rpm) / last_spindle_rpm) * 100
+        if abs(last_gcode_rpm - self.target_spindle_speed) > 100:
+            return ((self.target_spindle_speed - last_gcode_rpm) / last_gcode_rpm) * 100
         return 0
 
     def start_feed_too_low_check(self):
