50e0f056931d95daa4f07c0a12002a4c280eb160
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-02-15 17:29:09 +0000

    clean up start_up_sequence_manager.py

--- src/asmcnc/apps/start_up_sequence/start_up_sequence_manager.py
+++ src/asmcnc/apps/start_up_sequence/start_up_sequence_manager.py
@@ -1,213 +1,228 @@
 import os
 
-from asmcnc.apps.start_up_sequence.screens import \
-screen_reboot_to_apply_settings, \
-screen_release_notes, \
-screen_pro_plus_safety, \
-screen_starting_smartbench, \
-screen_safety_warning
-
-from asmcnc.apps.start_up_sequence.warranty_app import screen_manager_warranty
 from asmcnc.apps.start_up_sequence.data_consent_app import screen_manager_data_consent
+from asmcnc.apps.start_up_sequence.screens import (
+    screen_reboot_to_apply_settings,
+    screen_release_notes,
+    screen_pro_plus_safety,
+    screen_starting_smartbench,
+    screen_safety_warning
+)
+from asmcnc.apps.start_up_sequence.warranty_app import screen_manager_warranty
 from asmcnc.apps.start_up_sequence.welcome_to_smartbench_app import screen_manager_welcome_to_smartbench
-from asmcnc.apps.start_up_sequence.welcome_to_smartbench_app import screen_manager_welcome_to_smartbench
+
+# Get the "smartbench_activation_code" path (above easycut-smartbench)
+WARRANTY_ACTIVATION_CODE_PATH = os.path.join(os.path.dirname(os.path.dirname(os.getcwd())),
+                                             "smartbench_activation_code.txt")
 
 
 class StartUpSequence(object):
+    screen_sequence = []
+    seq_step = 0
+    reboot_in_sequence = False
 
-	screen_sequence = []
-	seq_step = 0
-	reboot_in_sequence = False
+    welcome_sm = None
+    release_notes_screen = None
+    data_consent_sm = None
+    pro_plus_safety_screen = None
+    starting_smartbench_screen = None
+    warranty_sm = None
+    reboot_to_apply_settings_screen = None
+    safety_screen = None
 
-	welcome_sm = None
-	release_notes_screen = None
-	data_consent_sm = None
-	pro_plus_safety_screen = None
-	starting_smartbench_screen = None
-	warranty_sm = None
-	reboot_to_apply_settings_screen = None
-	safety_screen = None
+    def __init__(self, app_manager, screen_manager, machine, settings, localization, keyboard, job, database,
+                 config_check, version):
 
-	def __init__(self, app_manager, screen_manager, machine, settings, localization, keyboard, job, database, config_check, version):
+        self.am = app_manager
+        self.sm = screen_manager
+        self.m = machine
+        self.set = settings
+        self.jd = job
+        self.l = localization
+        self.kb = keyboard
+        self.db = database
+        self.cc = config_check
+        self.v = version
 
-		self.am = app_manager
-		self.sm = screen_manager
-		self.m = machine
-		self.set = settings
-		self.jd = job
-		self.l = localization
-		self.kb = keyboard
-		self.db = database
-		self.cc = config_check
-		self.v = version
+        self.set_up_sequence()
+        self.start_sequence()
 
-		self.set_up_sequence()
-		self.start_sequence()
+    ## SCREEN SEQUENCE
 
-	## SCREEN SEQUENCE
+    def set_up_sequence(self):
 
-	def set_up_sequence(self):
+        if self.cc:
 
-		if self.cc:
+            if self.welcome_user():
+                self.prep_welcome_app()
 
-			if self.welcome_user():
-				self.prep_welcome_app()
+            if self.show_release_notes():
+                self.prep_release_notes_screen()
 
-			if self.show_release_notes():
-				self.prep_release_notes_screen()
+            if self.show_user_data_consent():
+                self.prep_data_consent_app()
 
-			if self.show_user_data_consent():
-				self.prep_data_consent_app()
+            if self.show_warranty_app():
+                self.prep_warranty_app()
 
-			if self.show_warranty_app():
-				self.prep_warranty_app()
+            if self.show_user_pro_plus_safety():
+                self.prep_user_pro_plus_safety()
 
-			if self.show_user_pro_plus_safety():
-				self.prep_user_pro_plus_safety()
+        if self.reboot_in_sequence:
+            self.prep_reboot_to_apply_settings_screen()
 
-		if self.reboot_in_sequence:		
-			self.prep_reboot_to_apply_settings_screen()
+        else:
+            self.prep_starting_smartbench_screen()
+            self.prep_safety_screen()
 
-		else:
-			self.prep_starting_smartbench_screen()
-			self.prep_safety_screen()
+    ## BASIC SEQUENCE NAVIGATION FUNCTIONS
 
-	## BASIC SEQUENCE NAVIGATION FUNCTIONS
+    def start_sequence(self):
 
-	def start_sequence(self):
+        self.seq_step = 0
+        self.sm.current = self.screen_sequence[self.seq_step]
 
-		self.seq_step = 0
-		self.sm.current = self.screen_sequence[self.seq_step]
+    def next_in_sequence(self):
 
-	def next_in_sequence(self):
+        self.seq_step += 1
+        self.sm.current = self.screen_sequence[self.seq_step]
 
-		self.seq_step +=1
-		self.sm.current = self.screen_sequence[self.seq_step]
+    def prev_in_sequence(self):
 
-	def prev_in_sequence(self):
+        if self.seq_step > 0:
+            self.seq_step -= 1
+            self.sm.current = self.screen_sequence[self.seq_step]
 
-		if self.seq_step > 0:
-			self.seq_step -=1
-			self.sm.current = self.screen_sequence[self.seq_step]
+    def add_screen_to_sequence(self, screen_name):
+        if screen_name not in self.screen_sequence:
+            self.screen_sequence.append(screen_name)
 
-	def add_screen_to_sequence(self, screen_name):
-		if screen_name not in self.screen_sequence:
-			self.screen_sequence.append(screen_name)
+    ## FUNCTIONS TO CHECK WHICH SCREENS TO SHOW
 
-	## FUNCTIONS TO CHECK WHICH SCREENS TO SHOW
+    def welcome_user(self):
+        flag = (os.popen('grep "show_user_welcome_app" config.txt').read())
 
-	def welcome_user(self):
-		flag = (os.popen('grep "show_user_welcome_app" config.txt').read())
+        if ('True' in flag):
+            self.reboot_in_sequence = True
+            return True
+        else:
+            return False
 
-		if ('True' in flag): 
-			self.reboot_in_sequence = True
-			return True
-		else: return False
+    def show_release_notes(self):
+        pc_alert = (os.popen('grep "power_cycle_alert=True" config.txt').read())
+        if "True" in pc_alert:
+            return True
+        else:
+            return False
 
-	def show_release_notes(self):
-		pc_alert = (os.popen('grep "power_cycle_alert=True" config.txt').read())
-		if "True" in pc_alert: return True
-		else: return False
+    def show_user_data_consent(self):
+        data_consent = (os.popen('grep "user_has_seen_privacy_notice" config.txt').read())
+        if ('False' in data_consent) or (not data_consent):
+            return True
+        else:
+            return False
 
-	def show_user_data_consent(self):
-		data_consent = (os.popen('grep "user_has_seen_privacy_notice" config.txt').read())
-		if ('False' in data_consent) or (not data_consent): return True
-		else: return False
+    def show_warranty_app(self):
+        return os.path.isfile(WARRANTY_ACTIVATION_CODE_PATH)
 
-	def show_warranty_app(self):
-		if os.path.isfile("/home/pi/smartbench_activation_code.txt"): return True
-		else: return False
+    def show_user_pro_plus_safety(self):
+        pro_plus_safety = (os.popen('grep "user_has_seen_pro_plus_safety" config.txt').read())
+        if ('False' in pro_plus_safety or not pro_plus_safety) and os.path.exists(self.m.theateam_path):
+            return True
+        else:
+            return False
 
-	def show_user_pro_plus_safety(self):
-		pro_plus_safety = (os.popen('grep "user_has_seen_pro_plus_safety" config.txt').read())
-		if ('False' in pro_plus_safety or not pro_plus_safety) and os.path.exists(self.m.theateam_path): return True
-		else: return False
+    ## FUNCTIONS TO PREP APPS AND SCREENS
 
-	## FUNCTIONS TO PREP APPS AND SCREENS
+    def prep_welcome_app(self):
+        if not self.welcome_sm:
+            self.welcome_sm = screen_manager_welcome_to_smartbench.ScreenManagerWelcomeToSmartBench(self, self.sm,
+                                                                                                    self.l)
 
-	def prep_welcome_app(self):
-		if not self.welcome_sm:
-			self.welcome_sm = screen_manager_welcome_to_smartbench.ScreenManagerWelcomeToSmartBench(self, self.sm, self.l)
+    def prep_release_notes_screen(self):
+        if not self.release_notes_screen:
+            self.release_notes_screen = screen_release_notes.ReleaseNotesScreen(name='release_notes',
+                                                                                start_sequence=self,
+                                                                                screen_manager=self.sm,
+                                                                                localization=self.l, version=self.v)
+            self.sm.add_widget(self.release_notes_screen)
 
-	def prep_release_notes_screen(self):
-		if not self.release_notes_screen:
-			self.release_notes_screen = screen_release_notes.ReleaseNotesScreen(name = 'release_notes', start_sequence = self, screen_manager = self.sm, localization = self.l, version = self.v)
-			self.sm.add_widget(self.release_notes_screen)
+        if 'release_notes' not in self.screen_sequence:
+            self.screen_sequence.append('release_notes')
 
-		if 'release_notes' not in self.screen_sequence:
-			self.screen_sequence.append('release_notes')
+    def prep_data_consent_app(self):
+        if not self.data_consent_sm:
+            self.data_consent_sm = screen_manager_data_consent.ScreenManagerDataConsent(self, self.sm, self.l)
 
-	def prep_data_consent_app(self):
-		if not self.data_consent_sm: 
-			self.data_consent_sm = screen_manager_data_consent.ScreenManagerDataConsent(self, self.sm, self.l)
+    def prep_warranty_app(self):
+        if not self.warranty_sm:
+            self.warranty_sm = screen_manager_warranty.ScreenManagerWarranty(self, self.sm, self.m, self.l, self.kb)
 
-	def prep_warranty_app(self):
-		if not self.warranty_sm:
-			self.warranty_sm = screen_manager_warranty.ScreenManagerWarranty(self, self.sm, self.m, self.l, self.kb)
+    def prep_user_pro_plus_safety(self):
+        if not self.pro_plus_safety_screen:
+            self.pro_plus_safety_screen = screen_pro_plus_safety.ProPlusSafetyScreen(name='pro_plus_safety',
+                                                                                     start_sequence=self,
+                                                                                     screen_manager=self.sm,
+                                                                                     localization=self.l)
+            self.sm.add_widget(self.pro_plus_safety_screen)
 
-	def prep_user_pro_plus_safety(self):
-		if not self.pro_plus_safety_screen:
-			self.pro_plus_safety_screen = screen_pro_plus_safety.ProPlusSafetyScreen(name='pro_plus_safety', start_sequence = self, screen_manager = self.sm, localization = self.l)
-			self.sm.add_widget(self.pro_plus_safety_screen)
+        if 'pro_plus_safety' not in self.screen_sequence:
+            self.screen_sequence.append('pro_plus_safety')
 
-		if 'pro_plus_safety' not in self.screen_sequence:
-			self.screen_sequence.append('pro_plus_safety')
+    def prep_reboot_to_apply_settings_screen(self):
+        if not self.reboot_to_apply_settings_screen:
+            self.reboot_to_apply_settings_screen = screen_reboot_to_apply_settings.ApplySettingsScreen(
+                name='reboot_apply_settings', start_sequence=self, screen_manager=self.sm, machine=self.m,
+                localization=self.l)
+            self.sm.add_widget(self.reboot_to_apply_settings_screen)
 
-	def prep_reboot_to_apply_settings_screen(self):
-		if not self.reboot_to_apply_settings_screen:    		
-			self.reboot_to_apply_settings_screen = screen_reboot_to_apply_settings.ApplySettingsScreen(name = 'reboot_apply_settings', start_sequence = self, screen_manager = self.sm, machine = self.m, localization = self.l)
-			self.sm.add_widget(self.reboot_to_apply_settings_screen)
+        if 'reboot_apply_settings' not in self.screen_sequence:
+            self.screen_sequence.append('reboot_apply_settings')
 
-		if 'reboot_apply_settings' not in self.screen_sequence:
-			self.screen_sequence.append('reboot_apply_settings')
+    def prep_starting_smartbench_screen(self):
+        if not self.starting_smartbench_screen:
+            self.starting_smartbench_screen = screen_starting_smartbench.StartingSmartBenchScreen(
+                name='starting_smartbench', start_sequence=self, screen_manager=self.sm, machine=self.m,
+                settings=self.set, database=self.db, localization=self.l)
+            self.sm.add_widget(self.starting_smartbench_screen)
 
-	def prep_starting_smartbench_screen(self):
-		if not self.starting_smartbench_screen:
-			self.starting_smartbench_screen = screen_starting_smartbench.StartingSmartBenchScreen(name = 'starting_smartbench', start_sequence = self, screen_manager = self.sm, machine =self.m, settings = self.set, database = self.db, localization = self.l)
-			self.sm.add_widget(self.starting_smartbench_screen)
+        if 'starting_smartbench' not in self.screen_sequence:
+            self.screen_sequence.append('starting_smartbench')
 
-		if 'starting_smartbench' not in self.screen_sequence:
-			self.screen_sequence.append('starting_smartbench')
+    def prep_safety_screen(self):
+        if not self.safety_screen:
+            self.safety_screen = screen_safety_warning.SafetyScreen(name='safety', start_sequence=self,
+                                                                    screen_manager=self.sm, machine=self.m,
+                                                                    localization=self.l)
+            self.sm.add_widget(self.safety_screen)
 
-	def prep_safety_screen(self):
-		if not self.safety_screen:    		
-			self.safety_screen = screen_safety_warning.SafetyScreen(name = 'safety', start_sequence = self, screen_manager = self.sm, machine = self.m, localization = self.l)
-			self.sm.add_widget(self.safety_screen)
+        if 'safety' not in self.screen_sequence:
+            self.screen_sequence.append('safety')
 
-		if 'safety' not in self.screen_sequence:
-			self.screen_sequence.append('safety')
+    ## END OF SEQUENCE FUNCTIONS
 
+    def exit_sequence(self, user_has_confirmed):
 
-	## END OF SEQUENCE FUNCTIONS
+        if self.sm.current != 'alarmScreen' and self.sm.current != 'errorScreen' and self.sm.current != 'door':
+            if user_has_confirmed:
+                [self.destroy_screen(i) for i in self.screen_sequence]
+                self.__del__()
 
-	def exit_sequence(self, user_has_confirmed):
+    def update_check_config_flag(self):
 
-		if self.sm.current != 'alarmScreen' and self.sm.current != 'errorScreen' and self.sm.current != 'door':
-			if user_has_confirmed:
+        if self.cc:
+            os.system('sudo sed -i "s/check_config=True/check_config=False/" config.txt')
 
-				[self.destroy_screen(i) for i in self.screen_sequence]
-				self.__del__()
+    def destroy_screen(self, screen_name):
 
+        if self.sm.has_screen(screen_name):
 
-	def update_check_config_flag(self):
+            try:
+                self.sm.remove_widget(self.sm.get_screen(screen_name))
+                print(screen_name + ' deleted')
 
-		if self.cc:
-			os.system('sudo sed -i "s/check_config=True/check_config=False/" config.txt')
+            except:
+                pass
 
-
-	def destroy_screen(self, screen_name):
-
-		if self.sm.has_screen(screen_name):
-
-			try:
-				self.sm.remove_widget(self.sm.get_screen(screen_name))
-				print(screen_name + ' deleted')
-
-			except: pass
-
-
-	def __del__(self):
-		print('End of startup sequence')
-
-
-
+    def __del__(self):
+        print('End of startup sequence')
