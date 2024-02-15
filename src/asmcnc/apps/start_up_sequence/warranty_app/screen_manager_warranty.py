b5039a57d0175b2ca1508b64a1f57702765da17e
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-02-15 17:21:22 +0000

    clean up screens, add basic logic to start with

--- src/asmcnc/apps/start_up_sequence/warranty_app/screen_manager_warranty.py
+++ src/asmcnc/apps/start_up_sequence/warranty_app/screen_manager_warranty.py
@@ -4,16 +4,14 @@ import sys, os
 from asmcnc.skavaUI import popup_info
 
 from asmcnc.apps.start_up_sequence.warranty_app.screens import \
-screen_warranty_registration_1, \
-screen_warranty_registration_2, \
-screen_warranty_registration_3, \
-screen_warranty_registration_4, \
-screen_warranty_registration_5
+    screen_warranty_registration_1, \
+    screen_warranty_registration_2, \
+    screen_warranty_registration_3, \
+    screen_warranty_registration_4, \
+    screen_warranty_registration_5
 
 
 class ScreenManagerWarranty(object):
-
-
 
     def __init__(self, start_sequence, screen_manager, machine, localization, keyboard):
 
@@ -25,37 +23,22 @@ class ScreenManagerWarranty(object):
 
         self.load_warranty_app()
 
+    warranty_screens = {
+        "warranty_1": screen_warranty_registration_1.WarrantyScreen1,
+        "warranty_2": screen_warranty_registration_2.WarrantyScreen2,
+        "warranty_3": screen_warranty_registration_3.WarrantyScreen3,
+        "warranty_4": screen_warranty_registration_4.WarrantyScreen4,
+        "warranty_5": screen_warranty_registration_5.WarrantyScreen5
+    }
+
     def load_warranty_app(self):
-
-        if not self.sm.has_screen('warranty_1'):
-            warranty_registration_1_screen = screen_warranty_registration_1.WarrantyScreen1(name = 'warranty_1', start_sequence = self.start_seq, machine = self.m, localization = self.l)
-            self.sm.add_widget(warranty_registration_1_screen)
-        else:
-            self.sm.get_screen('warranty_1').update_strings()
-
-        if not self.sm.has_screen('warranty_2'):
-            warranty_registration_2_screen = screen_warranty_registration_2.WarrantyScreen2(name = 'warranty_2', start_sequence = self.start_seq, machine = self.m, localization = self.l)
-            self.sm.add_widget(warranty_registration_2_screen)
-        else:
-            self.sm.get_screen('warranty_2').update_strings()
-
-        if not self.sm.has_screen('warranty_3'):
-            warranty_registration_3_screen = screen_warranty_registration_3.WarrantyScreen3(name = 'warranty_3', start_sequence = self.start_seq, machine = self.m, localization = self.l, keyboard = self.kb)
-            self.sm.add_widget(warranty_registration_3_screen)
-        else:
-            self.sm.get_screen('warranty_3').update_strings()
-
-        if not self.sm.has_screen('warranty_4'):
-            warranty_registration_4_screen = screen_warranty_registration_4.WarrantyScreen4(name = 'warranty_4', start_sequence = self.start_seq, machine = self.m, localization = self.l)
-            self.sm.add_widget(warranty_registration_4_screen)
-        else:
-            self.sm.get_screen('warranty_4').update_strings()
-
-        if not self.sm.has_screen('warranty_5'):
-            warranty_registration_5_screen = screen_warranty_registration_5.WarrantyScreen5(name = 'warranty_5', start_sequence = self.start_seq, machine = self.m, localization = self.l)
-            self.sm.add_widget(warranty_registration_5_screen)
-        else:
-            self.sm.get_screen('warranty_5').update_strings()
+        for name, cls in self.warranty_screens.items():
+            if not self.sm.has_screen(name):
+                screen = cls(name=name, start_sequence=self.start_seq, machine=self.m, localization=self.l,
+                             keyboard=self.kb)
+                self.sm.add_widget(screen)
+            else:
+                self.sm.get_screen(name).update_strings()
 
         self.start_seq.add_screen_to_sequence('warranty_1')
         self.start_seq.add_screen_to_sequence('warranty_2')
@@ -66,10 +49,9 @@ class ScreenManagerWarranty(object):
         if not self.start_seq.screen_sequence.index('warranty_1'):
             self.sm.get_screen('warranty_1').prev_screen_button.opacity = 0
 
-
     def open_warranty_app(self):
         self.load_warranty_app()
-        self.sm.current = 'warranty_1' 
+        self.sm.current = 'warranty_1'
 
     def destroy_screen(self, screen_name):
 
--- src/asmcnc/apps/start_up_sequence/warranty_app/screens/screen_warranty_registration_1.py
+++ src/asmcnc/apps/start_up_sequence/warranty_app/screens/screen_warranty_registration_1.py
@@ -1,17 +1,11 @@
 from kivy.core.window import Window
+
 """
 Created on nov 2020
 @author: Ollie
 """
-from kivy.app import App
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.tabbedpanel import TabbedPanel
-from kivy.uix.textinput import TextInput
-from asmcnc.skavaUI import widget_status_bar
-from kivy.properties import StringProperty, ObjectProperty
-from kivy.uix.gridlayout import GridLayout
-import sys, os
+from kivy.uix.screenmanager import Screen
 from asmcnc.apps.start_up_sequence.warranty_app.screens import popup_warranty
 
 Builder.load_string(
@@ -19,236 +13,235 @@ Builder.load_string(
 
 <WarrantyScreen1>:
 
-	title_label : title_label
-	scan_qr_code : scan_qr_code
-	instructions_label : instructions_label
-	cant_use_web_label : cant_use_web_label
-	contact_us_at_support : contact_us_at_support
-	prev_screen_button : prev_screen_button
-	next_button : next_button
+    title_label : title_label
+    scan_qr_code : scan_qr_code
+    instructions_label : instructions_label
+    cant_use_web_label : cant_use_web_label
+    contact_us_at_support : contact_us_at_support
+    prev_screen_button : prev_screen_button
+    next_button : next_button
+    body_container:body_container
 
+    BoxLayout: 
+        size_hint: (None,None)
+        width: dp(1.0*app.width)
+        height: dp(1.0*app.height)
+        orientation: 'vertical'
 
-	BoxLayout: 
-		size_hint: (None,None)
-		width: dp(1.0*app.width)
-		height: dp(1.0*app.height)
-		orientation: 'vertical'
+        canvas:
+            Color:
+                rgba: hex('##e5e5e5')
+            Rectangle:
+                size: self.size
+                pos: self.pos
 
-		canvas:
-			Color:
-				rgba: hex('##e5e5e5')
-			Rectangle:
-				size: self.size
-				pos: self.pos
+        BoxLayout:
+            padding: 0
+            spacing: 0
+            orientation: "vertical"
 
-		BoxLayout:
-			padding: 0
-			spacing: 0
-			orientation: "vertical"
+            # HEADER
+            BoxLayout:
+                padding: 0
+                spacing: 0
+                canvas:
+                    Color:
+                        rgba: hex('#1976d2ff')
+                    Rectangle:
+                        pos: self.pos
+                        size: self.size
+                Label:
+                    id: title_label
+                    size_hint: (None,None)
+                    height: dp(0.125*app.height)
+                    width: dp(1.0*app.width)
+                    text: "SmartBench Warranty Registration"
+                    color: hex('#f9f9f9ff')
+                    font_size: dp(0.0375*app.width)
+                    halign: "center"
+                    valign: "bottom"
+                    markup: True
+                    
+            # BODY
+            BoxLayout:
+                id: body_container
+                size_hint: (None,None)
+                width: dp(1.0*app.width)
+                height: dp(0.620833333333*app.height)
+                orientation: 'vertical'
 
-			# HEADER
-			BoxLayout:
-				padding: 0
-				spacing: 0
-				canvas:
-					Color:
-						rgba: hex('#1976d2ff')
-					Rectangle:
-						pos: self.pos
-						size: self.size
-				Label:
-					id: title_label
-					size_hint: (None,None)
-					height: dp(0.125*app.height)
-					width: dp(1.0*app.width)
-					text: "SmartBench Warranty Registration"
-					color: hex('#f9f9f9ff')
-					# color: hex('#333333ff') #grey
-					font_size: dp(0.0375*app.width)
-					halign: "center"
-					valign: "bottom"
-					markup: True
-				   
-			# BODY
-			BoxLayout:
-				size_hint: (None,None)
-				width: dp(1.0*app.width)
-				height: dp(0.620833333333*app.height)
-				orientation: 'vertical'
+                Button:
+                    font_size: str(0.01875 * app.width) + 'sp'
+                    size_hint_x: None
+                    width: dp(0.065*app.width)
+                    background_color: hex('##e5e5e5')
+                    background_normal: ''
+                    on_press: root.go_to_factory_settings()
 
+                Label:
+                    id: scan_qr_code
+                    font_size: str(0.0375*app.width) + 'sp'
+                    text_size: self.size
+                    valign: 'bottom'
+                    halign: 'center'
+                    markup: 'true'
+                    color: hex('#333333ff')
 
-				Button:
-				    font_size: str(0.01875 * app.width) + 'sp'
-					size_hint_x: None
-					width: dp(0.065*app.width)
-					background_color: hex('##e5e5e5')
-					background_normal: ''
-					on_press: root.go_to_factory_settings()
+                BoxLayout:
+                    orientation: 'vertical'
+                    width: dp(1.0*app.width)
+                    height: dp(0.416666666667*app.height)
+                    padding:[dp(0.025)*app.width, dp(0.0416666666667)*app.height, dp(0.025)*app.width, 0]
+                    size_hint: (None,None)
+                    spacing: 0
 
-				Label:
-					id: scan_qr_code
-					font_size: str(0.0375*app.width) + 'sp'
-					text_size: self.size
-					valign: 'bottom'
-					halign: 'center'
-					markup: 'true'
-					color: hex('#333333ff')
+                    Label:
+                        id: instructions_label
+                        size_hint_y: 0.3
+                        font_size: str(0.025*app.width) + 'sp'
+                        # text: "[color=333333ff]To submit your details and receive your activation code, go to[/color]"
+                        text_size: self.size
+                        valign: 'middle'
+                        halign: 'center'
+                        markup: True
+                        color: hex('#333333ff')
 
-				BoxLayout:
-					orientation: 'vertical'
-					width: dp(1.0*app.width)
-					height: dp(0.416666666667*app.height)
-					padding:[dp(0.025)*app.width, dp(0.0416666666667)*app.height, dp(0.025)*app.width, 0]
-					size_hint: (None,None)
-					spacing: 0
+                    BoxLayout:
+                        orientation: 'horizontal'
+                        width: dp(1.0*app.width)
+                        height: dp(0.275*app.height)
+                        # padding: [20, 0]
+                        size_hint: (None,None)
+                        spacing: 0
 
-					Label:
-						id: instructions_label
-						size_hint_y: 0.3
-						font_size: str(0.025*app.width) + 'sp'
-						# text: "[color=333333ff]To submit your details and receive your activation code, go to[/color]"
-						text_size: self.size
-						valign: 'middle'
-						halign: 'center'
-						markup: True
-						color: hex('#333333ff')
+                        BoxLayout:
+                            padding:[dp(0.0125)*app.width, 0, 0, 0]
+                            width: dp(0.2025*app.width)
+                            height: dp(0.275*app.height)
+                            size_hint: (None,None)
+                            Image:
+                                source: "./asmcnc/apps/start_up_sequence/warranty_app/img/registration-qr-code.png"
+                                center_x: self.parent.center_x
+                                y: self.parent.y
+                                size: self.parent.width, self.parent.height
+                                allow_stretch: True
 
-					BoxLayout:
-						orientation: 'horizontal'
-						width: dp(1.0*app.width)
-						height: dp(0.275*app.height)
-						# padding: [20, 0]
-						size_hint: (None,None)
-						spacing: 0
+                        BoxLayout:
+                            orientation: 'vertical'
+                            width: dp(0.7475*app.width)
+                            height: dp(0.275*app.height)
+                            padding:[0, 0, 0, 0]
+                            size_hint: (None,None)
 
-						BoxLayout:
-							padding:[dp(0.0125)*app.width, 0, 0, 0]
-							width: dp(0.2025*app.width)
-							height: dp(0.275*app.height)
-							size_hint: (None,None)
-							Image:
-								source: "./asmcnc/apps/start_up_sequence/warranty_app/img/registration-qr-code.png"
-								center_x: self.parent.center_x
-								y: self.parent.y
-								size: self.parent.width, self.parent.height
-								allow_stretch: True
+                            Label:
+                                size_hint_y: 0.4
+                                font_size: str(0.0275*app.width) + 'sp'
+                                text: "[color=333333ff]https://www.yetitool.com/support/Register-Your-Product[/color]"
+                                text_size: self.size
+                                valign: 'middle'
+                                halign: 'left'
+                                markup: 'true'
+                                multiline: True
+                                color: hex('#333333ff')
 
-						BoxLayout:
-							orientation: 'vertical'
-							width: dp(0.7475*app.width)
-							height: dp(0.275*app.height)
-							padding:[0, 0, 0, 0]
-							size_hint: (None,None)
+                            Label:
+                                id: cant_use_web_label
+                                size_hint_y: 0.3
+                                font_size: str(0.025*app.width) + 'sp'
+                                # text: "[color=333333ff]Can't use the web form?"
+                                text_size: self.size
+                                valign: 'bottom'
+                                halign: 'left'
+                                markup: 'true'
+                                color: hex('#333333ff')
 
-							Label:
-								size_hint_y: 0.4
-								font_size: str(0.0275*app.width) + 'sp'
-								text: "[color=333333ff]https://www.yetitool.com/support/Register-Your-Product[/color]"
-								text_size: self.size
-								valign: 'middle'
-								halign: 'left'
-								markup: 'true'
-								multiline: True
-								color: hex('#333333ff')
-							
-							Label:
-								id: cant_use_web_label
-								size_hint_y: 0.3
-								font_size: str(0.025*app.width) + 'sp'
-								# text: "[color=333333ff]Can't use the web form?"
-								text_size: self.size
-								valign: 'bottom'
-								halign: 'left'
-								markup: 'true'
-								color: hex('#333333ff')
+                            Label:
+                                id: contact_us_at_support
+                                size_hint_y: 0.3
+                                font_size: str(0.025*app.width) + 'sp'
+                                # text: "[color=333333ff]Contact us at https://www.yetitool.com/support[/color]"
+                                text_size: self.size
+                                valign: 'middle'
+                                halign: 'left'
+                                markup: 'true'
+                                color: hex('#333333ff')
 
-							Label:
-								id: contact_us_at_support
-								size_hint_y: 0.3
-								font_size: str(0.025*app.width) + 'sp'
-								# text: "[color=333333ff]Contact us at https://www.yetitool.com/support[/color]"
-								text_size: self.size
-								valign: 'middle'
-								halign: 'left'
-								markup: 'true'
-								color: hex('#333333ff')
+            # FOOTER
+            BoxLayout: 
+                padding:[dp(0.0125)*app.width, 0, dp(0.0125)*app.width, dp(0.0208333333333)*app.height]
+                size_hint: (None, None)
+                height: dp(0.254166666667*app.height)
+                width: dp(1.0*app.width)
+                orientation: 'horizontal'
 
-			# FOOTER
-			BoxLayout: 
-				padding:[dp(0.0125)*app.width, 0, dp(0.0125)*app.width, dp(0.0208333333333)*app.height]
-				size_hint: (None, None)
-				height: dp(0.254166666667*app.height)
-				width: dp(1.0*app.width)
-				orientation: 'horizontal'
+                BoxLayout: 
+                    size_hint: (None, None)
+                    height: dp(0.254166666667*app.height)
+                    width: dp(0.305625*app.width)
+                    padding:[0, 0, dp(0.230625)*app.width, 0]
+                    Button:
+                        font_size: str(0.01875 * app.width) + 'sp'
+                        id: prev_screen_button
+                        size_hint: (None,None)
+                        height: dp(0.108333333333*app.height)
+                        width: dp(0.075*app.width)
+                        background_color: hex('#F4433600')
+                        center: self.parent.center
+                        pos: self.parent.pos
+                        on_press: root.prev_screen()
+                        opacity: 1
+                        BoxLayout:
+                            padding: 0
+                            size: self.parent.size
+                            pos: self.parent.pos
+                            Image:
+                                source: "./asmcnc/apps/systemTools_app/img/back_to_menu.png"
+                                center_x: self.parent.center_x
+                                y: self.parent.y
+                                size: self.parent.width, self.parent.height
+                                allow_stretch: True
 
-				BoxLayout: 
-					size_hint: (None, None)
-					height: dp(0.254166666667*app.height)
-					width: dp(0.305625*app.width)
-					padding:[0, 0, dp(0.230625)*app.width, 0]
-					Button:
-					    font_size: str(0.01875 * app.width) + 'sp'
-						id: prev_screen_button
-						size_hint: (None,None)
-						height: dp(0.108333333333*app.height)
-						width: dp(0.075*app.width)
-						background_color: hex('#F4433600')
-						center: self.parent.center
-						pos: self.parent.pos
-						on_press: root.prev_screen()
-						opacity: 1
-						BoxLayout:
-							padding: 0
-							size: self.parent.size
-							pos: self.parent.pos
-							Image:
-								source: "./asmcnc/apps/systemTools_app/img/back_to_menu.png"
-								center_x: self.parent.center_x
-								y: self.parent.y
-								size: self.parent.width, self.parent.height
-								allow_stretch: True
+                BoxLayout: 
+                    size_hint: (None, None)
+                    height: dp(0.254166666667*app.height)
+                    width: dp(0.36375*app.width)
+                    padding:[0, 0, 0, dp(0.0666666666667)*app.height]
+                    Button:
+                        id: next_button
+                        background_normal: "./asmcnc/skavaUI/img/next.png"
+                        background_down: "./asmcnc/skavaUI/img/next.png"
+                        border: [dp(14.5)]*4
+                        size_hint: (None,None)
+                        width: dp(0.36375*app.width)
+                        height: dp(0.164583333333*app.height)
+                        on_press: root.next_screen()
+                        text: 'Next...'
+                        font_size: str(0.0375*app.width) + 'sp'
+                        color: hex('#f9f9f9ff')
+                        markup: True
+                        center: self.parent.center
+                        pos: self.parent.pos
 
-				BoxLayout: 
-					size_hint: (None, None)
-					height: dp(0.254166666667*app.height)
-					width: dp(0.36375*app.width)
-					padding:[0, 0, 0, dp(0.0666666666667)*app.height]
-					Button:
-						id: next_button
-						background_normal: "./asmcnc/skavaUI/img/next.png"
-						background_down: "./asmcnc/skavaUI/img/next.png"
-						border: [dp(14.5)]*4
-						size_hint: (None,None)
-						width: dp(0.36375*app.width)
-						height: dp(0.164583333333*app.height)
-						on_press: root.next_screen()
-						text: 'Next...'
-						font_size: str(0.0375*app.width) + 'sp'
-						color: hex('#f9f9f9ff')
-						markup: True
-						center: self.parent.center
-						pos: self.parent.pos
+                BoxLayout: 
+                    size_hint: (None, None)
+                    height: dp(0.254166666667*app.height)
+                    width: dp(0.305625*app.width)
+                    padding:[dp(0.240625)*app.width, 0, 0, 0]
 
-				BoxLayout: 
-					size_hint: (None, None)
-					height: dp(0.254166666667*app.height)
-					width: dp(0.305625*app.width)
-					padding:[dp(0.240625)*app.width, 0, 0, 0]
-
-					Button:
-					    font_size: str(0.01875 * app.width) + 'sp'
-						size_hint: (None,None)
-						height: dp(0.108333333333*app.height)
-						width: dp(0.065*app.width)
-						background_color: hex('##e5e5e5')
-						background_normal: ''
-						on_press: root.quit_to_console()
-
-"""
-)
+                    Button:
+                        font_size: str(0.01875 * app.width) + 'sp'
+                        size_hint: (None,None)
+                        height: dp(0.108333333333*app.height)
+                        width: dp(0.065*app.width)
+                        background_color: hex('##e5e5e5')
+                        background_normal: ''
+                        on_press: root.quit_to_console()
+""")
 
 
 class WarrantyScreen1(Screen):
+    MACHINE_IS_DWT = True
+
     def __init__(self, **kwargs):
         super(WarrantyScreen1, self).__init__(**kwargs)
         self.start_seq = kwargs["start_sequence"]
@@ -261,6 +254,11 @@ class WarrantyScreen1(Screen):
 
     def prev_screen(self):
         self.start_seq.prev_in_sequence()
+
+    def on_pre_enter(self, *args):
+        if self.MACHINE_IS_DWT:
+            for child in self.body_container.children:
+                self.body_container.remove_widget(child)
 
     def update_strings(self):
         self.title_label.text = self.l.get_str("SmartBench Warranty Registration")
@@ -275,7 +273,7 @@ class WarrantyScreen1(Screen):
         self.next_button.text = self.l.get_str("Next") + "..."
         self.update_contact_us_font_sizes()
 
-    def update_contact_us_font_sizes(self): # Update both labels together to make it look nicer
+    def update_contact_us_font_sizes(self):  # Update both labels together to make it look nicer
         if self.l.get_text_length(self.contact_us_at_support.text) > 70:
             self.cant_use_web_label.font_size = 0.02125 * Window.width
             self.contact_us_at_support.font_size = 0.02125 * Window.width
