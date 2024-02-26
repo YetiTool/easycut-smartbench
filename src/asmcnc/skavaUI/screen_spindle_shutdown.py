f09d46c616632579d3c6336a36db2a7cc0ce1b1b
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-02-26 14:54:36 +0000

    Wait 0.5s before raising so machine is actually resumed

--- src/asmcnc/skavaUI/screen_spindle_shutdown.py
+++ src/asmcnc/skavaUI/screen_spindle_shutdown.py
@@ -113,7 +113,7 @@ class SpindleShutdownScreen(Screen):
     def on_enter(self):
         log("Pausing job...")
         self.m._grbl_resume()  # resume so I can move the Z axis, refactor from protected method ideally
-        self.m.raise_z_axis_for_collet_access()
+        Clock.schedule_once(lambda dt: self.m.raise_z_axis_for_collet_access(), 0.5)
         if self.reason_for_pause == "spindle_overload":
             # Job paused due to overload, send event
             self.db.send_event(
