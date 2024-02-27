76c654ea22c2d232e7634cd71a39950aee065563
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-02-27 23:48:37 +0000

    test

--- src/asmcnc/skavaUI/widget_common_move.py
+++ src/asmcnc/skavaUI/widget_common_move.py
@@ -103,7 +103,7 @@ class CommonMove(Widget):
             background_down="./asmcnc/skavaUI/img/spindle_off.png",
             on_press=self.set_spindle,
         )
-        self.spindle_blinker = BlinkingWidget()
+        self.spindle_blinker = BlinkingWidget(padding=[10, 10])
         self.spindle_blinker.bind(pos=self.update_spindle_button)
         self.spindle_blinker.add_widget(self.spindle_button)
         self.vacuum_spindle_container.add_widget(self.spindle_blinker)
