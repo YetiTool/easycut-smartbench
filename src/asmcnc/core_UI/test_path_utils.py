b396534c27db98f075f6d72b4fed366723d2c91c
Benji Pestell <benji.pestell@yetitool.com>
2024-03-06 11:46:04 +0000

    Fix unit tests

--- src/asmcnc/core_UI/test_path_utils.py
+++ src/asmcnc/core_UI/test_path_utils.py
@@ -3,29 +3,23 @@ from path_utils import get_path
 
 class TestPathUtils(unittest.TestCase):
 
-    def setUp(self):
-        self.root = "/c:/Users/benji/Documents/"
-
     def test_get_path_files_only(self):
         target_dir = "router_machine.py"
-        result = get_path(target_dir, files_only=True)
-        self.assertEqual(result, self.root + "/easycut-smartbench/src/asmcnc/comms/router_machine.py")
+        expected_result = "/easycut-smartbench/src/asmcnc/comms/router_machine.py"
+        result = get_path(target_dir, files_only=True).replace("\\", "/")
+        self.assertIn(expected_result, result)        
 
     def test_get_path_folders_only(self):
         target_dir = "skavaUI"
-        result = get_path(target_dir, folders_only=True)
-        self.assertEqual(result, "/c:/Users/benji/Documents/easycut-smartbench/src/asmcnc/skavaUI")
+        expected_result = "/easycut-smartbench/src/asmcnc/skavaUI"
+        result = get_path(target_dir, folders_only=True).replace("\\", "/")
+        self.assertIn(expected_result, result) 
 
     def test_get_path_first_result_only(self):
-        target_dir = "test_dir"
-        result = get_path(target_dir, first_result_only=True)
-        self.assertEqual(result, "/c:/Users/benji/Documents/easycut-smartbench/src/asmcnc/core_UI/test_dir")
-
-    def test_get_path_multiple_paths(self):
-        target_dir = "test_dir"
-        result = get_path(target_dir)
-        self.assertIsInstance(result, list)
-        self.assertIn("/c:/Users/benji/Documents/easycut-smartbench/src/asmcnc/core_UI/test_dir", result)
+        target_dir = "img"
+        expected_result = "easycut-smartbench/src/asmcnc/apps/drywall_cutter_app/img"
+        result = get_path(target_dir, first_result_only=True).replace("\\", "/")
+        self.assertIn(expected_result, result) 
 
     def test_get_path_not_found(self):
         target_dir = "nonexistent_dir"
