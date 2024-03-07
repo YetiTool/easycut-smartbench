72dd76a574942d0f50f232d2962e93554752541b
Benji Pestell <benji.pestell@yetitool.com>
2024-03-07 12:30:56 +0000

    remove redudant replace function

--- tests/test_path_utils.py
+++ tests/test_path_utils.py
@@ -10,25 +10,25 @@ class TestPathUtils(unittest.TestCase):
     def test_get_path_files_only(self):
         target_dir = "router_machine.py"
         expected_result = "/easycut-smartbench/src/asmcnc/comms/router_machine.py"
-        result = get_path(target_dir, files_only=True).replace("\\", "/")
+        result = get_path(target_dir, files_only=True)
         self.assertIn(expected_result, result)
 
     def test_get_path_folders_only(self):
         target_dir = "skavaUI"
         expected_result = "/easycut-smartbench/src/asmcnc/skavaUI"
-        result = get_path(target_dir, folders_only=True).replace("\\", "/")
+        result = get_path(target_dir, folders_only=True)
         self.assertIn(expected_result, result) 
 
     def test_get_path_first_result_only(self):
         target_dir = "img"
         expected_result = "easycut-smartbench/src/asmcnc/apps/drywall_cutter_app/img"
-        result = get_path(target_dir, first_result_only=True).replace("\\", "/")
+        result = get_path(target_dir, first_result_only=True)
         self.assertIn(expected_result, result) 
 
     def test_directory_in_target(self):
         target_dir = "skavaUI/img"
         expected_result = "easycut-smartbench/src/asmcnc/skavaUI/img"
-        result = get_path(target_dir).replace("\\", "/")
+        result = get_path(target_dir)
         self.assertIn(expected_result, result)
 
     def test_get_path_not_found(self):
