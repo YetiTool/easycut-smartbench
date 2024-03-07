239fb1d2f434ce19922866f1ae2180e1416f1f22
Masc999 <marc.schlotzhauer@yetitool.com>
2024-03-07 22:58:23 +0100

    created unittest for testing events

---
+++ tests/automated_unit_tests/comms/test_serial_connection_events.py
@@ -0,0 +1,50 @@
+import sys
+
+
+from asmcnc.skavaUI.screen_go import GoScreen
+from automated_unit_tests.unit_test_base import UnitTestBase
+from tests import test_utils
+
+sys.path.append('./src')
+
+try:
+    import unittest
+    import pytest
+    from mock import Mock, MagicMock
+    from mock.mock import call, ANY, patch
+
+except:
+    print("Can't import mocking packages, are you on a dev machine?")
+
+from asmcnc.comms import router_machine
+from asmcnc.comms import localization
+
+'''
+######################################
+RUN FROM easycut-smartbench FOLDER WITH: 
+python -m pytest tests/automated_unit_tests/comms/test_serial_connection_events.py
+'''
+
+#test_utils.create_app()
+
+
+class TestSerialConnectionEvents(UnitTestBase):
+    def setUp(self):
+        super(TestSerialConnectionEvents, self).setUp()
+
+        # Create modules
+        self._create_modules()
+
+    @patch.object(GoScreen, 'update_overload_peak')
+    def test_update_overload_peak(self, mock_update_overload_peak):
+        GoScreen(name='go',
+                 screen_manager=self._screen_manager,
+                 machine=self._router_machine_module,
+                 job=self._job_data_module,
+                 app_manager=None,
+                 database=Mock(),
+                 localization=self._localization_module,
+                 yetipilot=Mock())
+        self._router_machine_module.s.dispatch('on_update_overload_peak', 20)
+        mock_update_overload_peak.assert_called_with(ANY, 20)
+
