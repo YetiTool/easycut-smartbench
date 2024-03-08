bc90cd18fd307b95959829af0237f62c5a2529f4
Masc999 <marc.schlotzhauer@yetitool.com>
2024-03-08 10:05:33 +0100

    fixed import in unittest

--- tests/test_path_utils.py
+++ tests/test_path_utils.py
@@ -1,9 +1,8 @@
 import unittest
-import os
 import sys
 
-sys.path.insert(0, '../src/asmcnc/core_UI')
-from path_utils import get_path
+sys.path.append('./src')
+from asmcnc.core_UI.path_utils import get_path
 
 class TestPathUtils(unittest.TestCase):
 
