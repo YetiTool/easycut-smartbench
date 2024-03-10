67fd3f0f7e884abf63737b30222e471599382946
VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>
2024-03-10 11:45:26 +0000

    Added shutdown_handler.json
    Signed-off-by: VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>

--- src/asmcnc/core_UI/console_utils.py
+++ src/asmcnc/core_UI/console_utils.py
@@ -1,7 +1,9 @@
-import os, sys
+import os, sys, json
 
 
 def shutdown():
+    with open('shutdown_handler.json', 'r') as f:
+        shutdown_handler = json.load(f)
     if sys.platform != 'win32' and sys.platform != 'darwin':
         os.system('sudo sed -i "s/correct_shutdown=False/correct_shutdown=True/" config.txt')
         os.system('sudo shutdown -h now')
