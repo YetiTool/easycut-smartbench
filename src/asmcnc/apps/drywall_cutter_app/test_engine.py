480dbbb3ff35d603091b741896d847aa83592fa2
Benji Pestell <benji.pestell@yetitool.com>
2024-03-13 11:36:59 +0000

    Reorder correct_orientation test

--- src/asmcnc/apps/drywall_cutter_app/test_engine.py
+++ src/asmcnc/apps/drywall_cutter_app/test_engine.py
@@ -42,19 +42,6 @@ class EngineTests(unittest.TestCase):
         coordinates = [(0, 0), (0, 100.0), (100.0, 100.0), (100.0, 0), (0, 0)] # TL -> BL -> BR -> TR -> TL, counter-clockwise
         self.assertFalse(self.engine.is_clockwise(coordinates))
 
-    def test_replace_cut_depth_and_z_safe_distance(self):
-        gcode_lines = ["G1 X10 Y10 Z10", "G1 X20 Y20 Z-5", "G1 X30 Y30 Z-2"]
-        processing_args= {
-            "gcode_cut_depth": -5,
-            "gcode_z_safe_distance": 10,
-            "new_cut_depth": -8,
-            "new_z_safe_distance": 5
-        }
-
-        expected_output = ["G1 X10 Y10 Z5", "G1 X20 Y20 Z-8", "G1 X30 Y30 Z-2"]
-        output = self.engine.replace_cut_depth_and_z_safe_distance(gcode_lines, **processing_args)
-        self.assertEqual(output, expected_output)
-
     def test_correct_orientation(self):
         # Case 1
         coordinates = [(0, 0), (0, 100.0), (100.0, 100.0), (100.0, 0), (0, 0)] # TL -> BL -> BR -> TR -> TL, counter-clockwise
@@ -66,6 +53,19 @@ class EngineTests(unittest.TestCase):
         coordinates = [(0, 0), (100.0, 0), (100.0, 100.0), (0, 100.0), (0, 0)] # BL -> BR -> TR -> TL -> BL, clockwise
         expected_output = [(0, 0), (100.0, 0), (100.0, 100.0), (0, 100.0), (0, 0)] # BL -> BR -> TR -> TL -> BL, clockwise
         output = self.engine.correct_orientation(coordinates, self.engine.is_clockwise(coordinates))
+        self.assertEqual(output, expected_output)
+
+    def test_replace_cut_depth_and_z_safe_distance(self):
+        gcode_lines = ["G1 X10 Y10 Z10", "G1 X20 Y20 Z-5", "G1 X30 Y30 Z-2"]
+        processing_args= {
+            "gcode_cut_depth": -5,
+            "gcode_z_safe_distance": 10,
+            "new_cut_depth": -8,
+            "new_z_safe_distance": 5
+        }
+
+        expected_output = ["G1 X10 Y10 Z5", "G1 X20 Y20 Z-8", "G1 X30 Y30 Z-2"]
+        output = self.engine.replace_cut_depth_and_z_safe_distance(gcode_lines, **processing_args)
         self.assertEqual(output, expected_output)
 
 if __name__ == '__main__':
