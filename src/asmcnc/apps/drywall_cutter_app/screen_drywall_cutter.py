a942ea82086f0bc50b756f6bbebdff53663d0235
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-03-27 13:46:55 +0000

    fix

--- src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
+++ src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
@@ -216,9 +216,9 @@ class DrywallCutterScreen(Screen):
         self.dwt_config.bind(active_config=self.on_load_config)
         self.m.bind(datum_position=self.set_datum_position)
 
-    def set_datum_position(self, instance, value):
-        self.dwt_config.on_parameter_change('datum_position.x', value[0])
-        self.dwt_config.on_parameter_change('datum_position.y', value[1])
+    def set_datum_position(self, *args):
+        self.dwt_config.on_parameter_change('datum_position.x', self.m.datum_position[0])
+        self.dwt_config.on_parameter_change('datum_position.y', self.m.datum_position[1])
 
     def on_pre_enter(self):
         self.apply_active_config()
