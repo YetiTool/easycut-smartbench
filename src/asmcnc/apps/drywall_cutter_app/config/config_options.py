0ff6aaed5ebabb0467edccd0c1b3160f0c705b9a
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-03-27 02:15:32 +0000

    Base setup

--- src/asmcnc/apps/drywall_cutter_app/config/config_options.py
+++ src/asmcnc/apps/drywall_cutter_app/config/config_options.py
@@ -1,31 +1,81 @@
+"""Module to define the shapes and their properties."""
+import json
+
 from enum import Enum
-
-"""This module contains the enums for the config options."""
-
-
-class Rotation(Enum):
-    """Rotation enum."""
-    HORIZONTAL = "horizontal"
-    VERTICAL = "vertical"
-
-
-class Shape(Enum):
-    """Shape enum."""
-    RECTANGLE = "rectangle"
-    CIRCLE = "circle"
-    LINE = "line"
-    SQUARE = "square"
-    GEBERIT = "geberit"
 
 
 class ToolpathOffset(Enum):
-    """Toolpath offset enum."""
+    """Enum to define the toolpath offset options."""
     INSIDE = "inside"
     OUTSIDE = "outside"
     ON = "on"
 
 
-class Unit(Enum):
-    """Unit enum."""
-    INCH = "inch"
-    MM = "mm"
+class Shape(object):
+    """Class to define the shapes and their properties."""
+
+    name = ""
+
+    x = 0.0
+    y = 0.0
+    r = 0.0
+
+    rotatable = False
+
+    allowable_toolpath_offsets = []
+
+    def __init__(self, x, y, r, *args, **kwargs):
+        super(Shape, self).__init__(*args, **kwargs)
+        self.x = x
+        self.y = y
+        self.r = r
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "dimensions": {
+                "x": self.x,
+                "y": self.y,
+                "r": self.r
+            }
+        }
+
+
+class Rectangle(Shape):
+    """Class to define the rectangle shape and its properties."""
+    name = "rectangle"
+
+    rotatable = True
+
+    allowable_toolpath_offsets = [ToolpathOffset.INSIDE, ToolpathOffset.OUTSIDE, ToolpathOffset.ON]
+
+
+class Circle(Shape):
+    """Class to define the circle shape and its properties."""
+    name = "circle"
+
+    allowable_toolpath_offsets = [ToolpathOffset.INSIDE, ToolpathOffset.OUTSIDE, ToolpathOffset.ON]
+
+
+class ConfigTest(object):
+    def __init__(self, shape, units):
+        self.shape = shape
+        self.units = units
+
+    def to_dict(self):
+        return {
+            "shape": self.shape.to_dict(),
+            "units": self.units
+        }
+
+
+if __name__ == "__main__":
+    rectangle = Rectangle(10, 20, 30)
+    circle = Circle(5, 10, 15)
+    config = ConfigTest(rectangle, "mm")
+
+    print(rectangle.to_dict())
+    print(circle.to_dict())
+    print(config.to_dict())
+
+    print(json.dumps(config.to_dict(), indent=4))
