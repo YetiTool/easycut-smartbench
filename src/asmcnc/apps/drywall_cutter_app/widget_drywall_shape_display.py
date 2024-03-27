26c3470aa57bc5584e63320039a8ec743fcc437d
archiepeterjarvis <archiepeterjarvis@gmail.com>
2024-03-27 01:40:57 +0000

    Remove log

--- src/asmcnc/apps/drywall_cutter_app/widget_drywall_shape_display.py
+++ src/asmcnc/apps/drywall_cutter_app/widget_drywall_shape_display.py
@@ -759,7 +759,5 @@ class DrywallShapeDisplay(Widget):
         else:
             self.l_input_validation_label.opacity = 0
 
-
     def on_config_name_change(self, instance, value):
-        Logger.debug("Config name changed to: " + value)
         self.config_name_label.text = "New Config" if value == "temp_config.json" else value
