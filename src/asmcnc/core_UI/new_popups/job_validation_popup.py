c91094720b4598603369f5de5bbd537b153831ff
Archie Jarvis <archiepeterjarvis@gmail.com>
2024-03-30 15:35:08 +0000

    Bold button

--- src/asmcnc/core_UI/new_popups/job_validation_popup.py
+++ src/asmcnc/core_UI/new_popups/job_validation_popup.py
@@ -26,9 +26,9 @@ class JobValidationPopup(PopupBase):
         self.root_layout.add_widget(body)
 
         button_layout = BoxLayout(size_hint_y=0.15)
-        button_layout.add_widget(Button(text=localisation.get_str("Ok"), on_press=self.dismiss,
+        button_layout.add_widget(Button(text=localisation.get_bold("Ok"), on_press=self.dismiss,
                                         background_color=[230 / 255., 74 / 255., 25 / 255., 1.], background_normal="",
-                                        font_size=scaling_utils.get_scaled_sp("15sp")))
+                                        font_size=scaling_utils.get_scaled_sp("15sp"), markup=True))
         self.root_layout.add_widget(button_layout)
 
 
