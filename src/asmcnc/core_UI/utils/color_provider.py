aeba414ad9a1d09f9bebfa04d662d1a8e6527172
Archie Jarvis <archiepeterjarvis@gmail.com>
2024-04-03 22:05:35 +0100

    Color provider update

--- src/asmcnc/core_UI/utils/color_provider.py
+++ src/asmcnc/core_UI/utils/color_provider.py
@@ -1,67 +1,77 @@
+from typing import Dict, Tuple
+
+# A dictionary that contains the colors that can be used in the application.
+# Add more colors where required.
+Colours = {
+    "white": (1.0, 1.0, 1.0, 1.0),
+    "black": (0.0, 0.0, 0.0, 1.0),
+    "green": (76 / 255., 175 / 255., 80 / 255., 1.0),
+    "red": (230 / 255., 74 / 255., 25 / 255., 1.0),
+}  # type: Dict[str, Tuple[float, float, float, float]]
+
 
 class ColorProvider(object):
-    """Class for providing colors for the software. This class is a singleton.
+    """A provider for colors that can be used in the application.
 
-    * Usage in Python code:
-    color_provider = ColorProvider()
-    color_provider.get_color("white")
-
-    * Usage in Kivy Builder:
-    background_color: app.color_provider.get_color("button_green")
-
-    Attributes:
-        _instance (ColorProvider): The singleton instance of the ColorProvider class.
-        DEFINED_COLORS (dict): A dictionary containing predefined colors as RGBA values.
-
-    Methods:
-        get_color(color_name): Get a color from the color provider.
-        get_color_hex(color_name): Get a color from the color provider as a hex string.
-        get_markup_color_string(color_name, text): Get a markup string for a color.
-
+    Methods
+    -------
+    get_rgba(color_name)
+        Get the color by name in rgba format.
+    get_color_hex(color_name)
+        Get the color by name in hex format.
+    get_color(color_name)
+        Get the Color object by name.
+    get_markup_color_string(text, color_name)
+        Get a markup string that can be used in labels.
     """
 
-    _instance = None
+    @staticmethod
+    def get_rgba(color_name):
+        """
+        Get the color by name in rgba format. The color is represented as a tuple of floats (r, g, b, a).
+        Use this method when making labels/widgets e.g. Label(color=ColorProvider.get_rgba("green")).
 
-    DEFINED_COLORS = {
-        "white": [1, 1, 1, 1],
-        "black": [0, 0, 0, 1],
-        "button_green": [76 / 255., 175 / 255., 80 / 255., 1.0],
-        "button_red": [230 / 255., 74 / 255., 25 / 255., 1.0],
-    }  # STORE COLORS AS RGBA VALUES (0-1 range as per Kivy)
-
-    def __new__(cls):
-        """Create a new instance of the ColorProvider if one doesn't already exist."""
-        if cls._instance is None:
-            cls._instance = super(ColorProvider, cls).__new__(cls)
-            cls._instance._initialized = False
-        return cls._instance
-
-    def get_color(self, color_name):
-        """Get a color from the color provider.
-        :param color_name: The name of the color to get.
-        :return: The color as a list of RGBA values."""
+        :param color_name: The name of the color.
+        :return: The color in rgba format.
+        :raises KeyError: If the color name is not found in the color provider.
+        """
         try:
-            return self.DEFINED_COLORS[color_name.lower()]
+            rgba = Colours[color_name.lower()]
         except KeyError:
             raise KeyError("Color name not found in the color provider.")
 
-    def get_color_hex(self, color_name):
-        """Get a color from the color provider as a hex string.
-        :param color_name: The name of the color to get.
-        :return: The color as a hex string."""
-        color = self.get_color(color_name)
+        return rgba
+
+    @staticmethod
+    def get_color_hex(color_name):
+        """
+        Get the color by name in hex format.
+        Avoid using this method if you don't need to. Use get_rgba or get_markup_color_string instead.
+
+        :param color_name: The name of the color.
+        :return: The color in hex format.
+        :raises KeyError: If the color name is not found in the color provider.
+        """
+        try:
+            color = Colours[color_name.lower()]
+        except KeyError:
+            raise KeyError("Color name not found in the color provider.")
+
         return "#{:02x}{:02x}{:02x}".format(int(color[0] * 255), int(color[1] * 255), int(color[2] * 255))
 
-    def get_markup_color_string(self, color_name, text):
-        """Get a markup string for a color in the format: [color=hex]text[/color]
-        :param color_name: The name of the color to get.
-        :param text: The text to apply the color to.
-        :return: The text with the color applied."""
-        return "[color={}]{}[/color]".format(self.get_color_hex(color_name), text)
+    @staticmethod
+    def get_markup_color_string(text, color_name):
+        """
+        Get a markup string that can be used in labels. It is in the format [color=hex]text[/color].
 
+        :param text: The text to be colored.
+        :param color_name: The name of the color.
+        :return: The markup string.
+        :raises KeyError: If the color name is not found in the color provider.
+        """
+        try:
+            color_hex = ColorProvider.get_color_hex(color_name)
+        except KeyError:
+            raise KeyError("Color name not found in the color provider.")
 
-if __name__ == "__main__":
-    color_provider = ColorProvider()
-    print(color_provider.get_color("button_green"))
-    print(color_provider.get_color_hex("button_green"))
-    print(color_provider.get_markup_color_string("button_green", "Hello, World!"))
+        return "[color={}]{}[/color]".format(color_hex, text)
