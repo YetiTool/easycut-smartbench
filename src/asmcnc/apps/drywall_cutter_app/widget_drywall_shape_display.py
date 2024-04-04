ae9e7a2482d4a9fa7275f1ceae3558e2101ce156
Benji Pestell <benji.pestell@yetitool.com>
2024-04-04 09:26:22 +0100

    Remove unused variables

--- src/asmcnc/apps/drywall_cutter_app/widget_drywall_shape_display.py
+++ src/asmcnc/apps/drywall_cutter_app/widget_drywall_shape_display.py
@@ -584,7 +584,6 @@ class DrywallShapeDisplay(Widget):
         # Get current x/y values & shape clearances
         current_shape = self.dwt_config.active_config.shape_type.lower()
         current_x, current_y = self.cs.drywall_tec_coordinates.get_x(), self.cs.drywall_tec_coordinates.get_y()
-        machine_x, machine_y = self.cs.machine_coordinates.get_x(), self.cs.machine_coordinates.get_y()
 
         tool_offset_value = self.tool_offset_value()
         x_min_clearance, y_min_clearance, x_max_clearance, y_max_clearance = self.get_x_y_clearances(current_shape, self.x_coord, self.y_coord, tool_offset_value)
