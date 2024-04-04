c917421cbf45cc18516c2636653763de7fc05463
Archie Jarvis <archiepeterjarvis@gmail.com>
2024-04-04 11:40:56 +0100

    Use list instead of tuple for compatibility with Kivy, rename functions, simplify get_hex, add documentation links

--- tests/examples/color_provider/color_provider_examples.py
+++ tests/examples/color_provider/color_provider_examples.py
@@ -1,46 +1,21 @@
-from kivy.base import runTouchApp
-from kivy.graphics import Color, Rectangle
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.carousel import Carousel
-from kivy.uix.label import Label
+from kivy.app import App
+from kivy.lang import Builder
 
-from asmcnc.core_UI.utils import color_provider
+kv = """
+#:import color_provider asmcnc.core_UI.utils.color_provider
 
-layout = BoxLayout(
-    orientation="vertical",
-    size_hint=(1, 1),
-)
-
-
-def update_size(instance, value):
-    instance.canvas.before.clear()
-    with instance.canvas.before:
-        Color(rgba=color_provider.get_rgba("primary"))
-        Rectangle(size=instance.size, pos=instance.pos)
+BoxLayout:
+    Label:
+        text: "Hello, World!"
+        color: color_provider.get_rgba("green")
+        font_size: 20
+"""
 
 
-layout.bind(size=update_size)
+class TestApp(App):
+    def build(self):
+        return Builder.load_string(kv)
 
-carousel = Carousel(
-    direction="right",
-    loop=True,
-    size_hint=(1, 1),
-)
 
-for color_name in color_provider.Colours:
-    color = color_provider.get_rgba(color_name)
-    color_hex = color_provider.get_color_hex(color_name)
-    markup_color_string = color_provider.get_markup_color_string(color_name, color_name)
-
-    print(color, color_hex, markup_color_string)
-
-    carousel.add_widget(Label(
-        text=markup_color_string,
-        color=color,
-        font_size=20,
-        markup=True
-    ))
-
-layout.add_widget(carousel)
-
-runTouchApp(layout)
+if __name__ == "__main__":
+    TestApp().run()
