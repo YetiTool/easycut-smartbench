f55d8f36aabc602238769e00df43f93eb192d683
Benji Pestell <benji.pestell@yetitool.com>
2024-04-08 12:05:24 +0100

    Fix maths

--- src/asmcnc/comms/coordinate_system.py
+++ src/asmcnc/comms/coordinate_system.py
@@ -235,9 +235,9 @@ class CoordinateSystem(object):
             move_command = "G0 G53"
 
             if dwl_x is not None:
-                move_command += " X{}".format(-(self.dwt_coordinates.x_delta - self.dwt_coordinates.m.laser_offset_x_value))
+                move_command += " X{}".format(-self.dwt_coordinates.x_delta - self.dwt_coordinates.m.laser_offset_x_value)
             if dwl_y is not None:
-                move_command += " Y{}".format(-(self.dwt_coordinates.y_delta - self.dwt_coordinates.m.laser_offset_y_value))
+                move_command += " Y{}".format(-self.dwt_coordinates.y_delta - self.dwt_coordinates.m.laser_offset_y_value)
             if dwl_z is not None:
                 move_command += " Z{}".format(self.get_mz_from_dwlz(dwl_z))
 
