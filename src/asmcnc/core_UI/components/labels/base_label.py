850c3dffbbf932b2a9553c0531c14545701faf72
Masc999 <marc.schlotzhauer@yetitool.com>
2024-04-15 14:32:21 +0200

    LabelBase now has localization

--- src/asmcnc/core_UI/components/labels/base_label.py
+++ src/asmcnc/core_UI/components/labels/base_label.py
@@ -1,7 +1,12 @@
+from kivy.app import App
 from kivy.uix.label import Label
 
 from asmcnc.core_UI.hoverable import HoverBehavior
 
 
 class LabelBase(Label, HoverBehavior):
-    pass
+    def __init__(self, **kwargs):
+        super(LabelBase, self).__init__(**kwargs)
+        # get Localization instance from App
+        self.l = App.get_running_app().l
+
