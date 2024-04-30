154bb32e51cc0446081bb0daa55edaa40035bfb2
Archie Jarvis <archiepeterjarvis@gmail.com>
2024-04-30 18:35:40 +0100

    try no sleep

--- src/asmcnc/comms/serial_connection.py
+++ src/asmcnc/comms/serial_connection.py
@@ -182,7 +182,7 @@ class SerialConnection(EventDispatcher):
 
             ser.write(WAKE_UP_CMD.encode())
 
-            time.sleep(0.5)
+            # time.sleep(0.5)
 
             response = ser.read(ser.inWaiting()).decode()
 
