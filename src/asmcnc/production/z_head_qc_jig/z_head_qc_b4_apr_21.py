b19a832d9f32a641ef73cd909d4c9f66f4a5ca06
VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>
2024-05-07 12:37:45 +0100

    Updated setting description
    Signed-off-by: VladPavelescu <126763092+VladPavelescu@users.noreply.github.com>

--- src/asmcnc/production/z_head_qc_jig/z_head_qc_b4_apr_21.py
+++ src/asmcnc/production/z_head_qc_jig/z_head_qc_b4_apr_21.py
@@ -457,7 +457,7 @@ class ZHeadQCWarrantyBeforeApr21(Screen):
         self.probe()
 
     # def dust_shoe_switch(self):
-    #     if self.m.s.dust_shoe_cover_on:
+    #     if self.m.s.dustshoe_is_closed:
     #         self.dust_shoe_check.source = "./asmcnc/skavaUI/img/file_select_select.png"
     #     else:
     #         self.dust_shoe_check.source = "./asmcnc/skavaUI/img/checkbox_inactive.png"
