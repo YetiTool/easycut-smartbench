62d114a6bcdf0c645545bcedb1249360e92f71f5
Archie Jarvis <archiepeterjarvis@gmail.com>
2024-05-29 16:12:29 +0100

    60FPS animations

--- src/asmcnc/core_UI/components/widgets/blinking_widget.py
+++ src/asmcnc/core_UI/components/widgets/blinking_widget.py
@@ -40,8 +40,8 @@ class BlinkingWidget(Widget):
         super(BlinkingWidget, self).__init__(**kwargs)
 
         self.animation = (
-            Animation(bg_color=YELLOW, duration=0.5)
-            + Animation(bg_color=TRANSPARENT_YELLOW, duration=0.5)
+            Animation(bg_color=YELLOW, duration=0.5, s=1.0/60.0)
+            + Animation(bg_color=TRANSPARENT_YELLOW, duration=0.5, s=1.0/60.0)
         )
         self.animation.repeat = True
 
