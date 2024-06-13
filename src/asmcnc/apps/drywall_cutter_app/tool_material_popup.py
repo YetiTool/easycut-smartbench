04d8971c6456f902eb7f0971ac1b27fea8983004
Marc <marc.schlotzhauer@yetitool.com>
2024-06-13 14:19:55 +0200

    Logger instead of print

--- src/asmcnc/apps/drywall_cutter_app/tool_material_popup.py
+++ src/asmcnc/apps/drywall_cutter_app/tool_material_popup.py
@@ -157,10 +157,10 @@ class ToolMaterialPopup(Popup):
     def on_tool_change(self, instance, value):
         if value != '':
             self.confirm_button.disabled = False
-            Logger.debug("Tool changed to ", value)
+            Logger.debug("Tool changed to " + value)
 
     def on_material_change(self, instance, value):
-        print("Material changed to ", value)
+        Logger.debug("Material changed to " + value)
         self.tool_dropdown.values = self.profile_db.get_tool_names(self.profile_db.get_material_id(value))
         self.tool_dropdown.text = ''
         self.confirm_button.disabled = True
