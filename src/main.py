1ac460d47760135cd84b600b3b25a024751ddb15
LettyYeti <lettie.adkins@yetitool.com>
2024-06-17 14:26:26 +0100

    add release notes and update version

--- src/main.py
+++ src/main.py
@@ -104,7 +104,7 @@ from asmcnc.skavaUI import screen_homing_decision
 Cmport = 'COM3'
 
 # Current version active/working on
-initial_version = 'v2.9.0'
+initial_version = 'v2.9.1'
 
 config_flag = False
 
