a75a9b08a5ee2c490e7e4e43c86d394a1f9e0e56
Archie Jarvis <archiepeterjarvis@gmail.com>
2024-07-02 22:12:39 +0100

    Enforce z datum reminder on startup, material change, cutter change or material thickness change

--- src/asmcnc/apps/drywall_cutter_app/config/config_loader.py
+++ src/asmcnc/apps/drywall_cutter_app/config/config_loader.py
@@ -104,6 +104,8 @@ class DWTConfig(EventDispatcher):
     active_cutter = ObjectProperty(config_classes.Cutter.default())  # type: config_classes.Cutter
     active_profile = ObjectProperty(config_classes.Profile.default())  # type: config_classes.Profile
 
+    show_z_height_reminder = True
+
     def __init__(self, screen_drywall_cutter=None, *args, **kwargs):
         super(DWTConfig, self).__init__(*args, **kwargs)
         self.screen_drywall_cutter = screen_drywall_cutter
@@ -424,9 +426,13 @@ class DWTConfig(EventDispatcher):
 
             setattr(self.active_config, parameter_name, parameter_value)
 
+        if parameter_name == "material" or parameter_name.split(".")[-1] == "material_thickness":
+            self.show_z_height_reminder = True
+
         if parameter_name == "cutter_type":
             self.load_cutter(parameter_value)
             self.load_new_profile()
+            self.show_z_height_reminder = True
 
         # update screen, check bumpers and so on:
         if not (self.active_config.shape_type == 'geberit' and self.active_cutter.dimensions.tool_diameter is None):
