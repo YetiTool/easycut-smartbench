11f83eeb050fa67a10d3c0e7e20c21fcbcacdd56
DennisKiselev <kiselev.dennis@gmail.com>
2024-07-02 15:18:20 +0100

    Replace unit switch with mm label

--- src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
+++ src/asmcnc/apps/drywall_cutter_app/screen_drywall_cutter.py
@@ -635,7 +635,7 @@ class DrywallCutterScreen(Screen):
         self.drywall_shape_display_widget.x_input.text = str(self.dwt_config.active_config.canvas_shape_dims.x)
         self.drywall_shape_display_widget.y_input.text = str(self.dwt_config.active_config.canvas_shape_dims.y)
 
-        self.drywall_shape_display_widget.unit_switch.active = self.dwt_config.active_config.units == 'mm'
+        # self.drywall_shape_display_widget.unit_switch.active = self.dwt_config.active_config.units == 'mm'
 
         # Vlad set your text inputs here:
 
