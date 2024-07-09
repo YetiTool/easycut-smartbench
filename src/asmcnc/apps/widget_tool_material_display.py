2debe411aeb31afd4632626496025844569cbf9b
Benji Pestell <benji.pestell@yetitool.com>
2024-07-09 15:13:52 +0100

    Correct background colour in widget_tool_material_display.py

--- src/asmcnc/apps/widget_tool_material_display.py
+++ src/asmcnc/apps/widget_tool_material_display.py
@@ -1,9 +1,16 @@
 from kivy.metrics import dp
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
+from kivy.graphics import Color, Rectangle
+
+import kivy.utils
 
 from asmcnc.core_UI import scaling_utils
 from asmcnc.core_UI.utils import color_provider
+
+
+def hex_to_rgba(hex_color):
+    return kivy.utils.get_color_from_hex(hex_color)
 
 
 class ToolMaterialDisplayWidget(BoxLayout):
@@ -17,6 +24,13 @@ class ToolMaterialDisplayWidget(BoxLayout):
         self.material_text = ""
         self.tool_text = ""
 
+        # Set the background color
+        rgba_color = hex_to_rgba("#f9f9f9ff")
+        with self.canvas.before:
+            Color(*rgba_color)
+            self.rect = Rectangle(size=self.size, pos=self.pos)
+            self.bind(pos=self.update_rect, size=self.update_rect)
+
         self.tool_material_label = Label(text="", halign="left", valign="top",
                                          font_size=scaling_utils.get_scaled_sp("13sp"),
                                          color=color_provider.get_rgba("black"), center_y=self.center_y)
@@ -27,6 +41,10 @@ class ToolMaterialDisplayWidget(BoxLayout):
         config.bind(active_profile=self.on_active_profile)
 
         self.update_tool_material_label()
+
+    def update_rect(self, *args):
+        self.rect.pos = self.pos
+        self.rect.size = self.size
 
     def on_active_profile(self, *args):
         self.material_text = self.config.active_profile.material.description
